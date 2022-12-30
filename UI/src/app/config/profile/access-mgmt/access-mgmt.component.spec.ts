faa0dc0feabd783b523ec56e05b4322e5619ecf2
HinPublicis <hinshah@publicisgroupe.net>
2022-12-30 18:57:56 +0530

    added unit tests for access mgmt component

--- UI/src/app/config/profile/access-mgmt/access-mgmt.component.spec.ts
+++ UI/src/app/config/profile/access-mgmt/access-mgmt.component.spec.ts
@@ -34,13 +34,13 @@ import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccessMgmtComponent } from './access-mgmt.component';
-
 describe('AccessMgmtComponent', () => {
   let component: AccessMgmtComponent;
   let fixture: ComponentFixture<AccessMgmtComponent>;
   let httpService: HttpService;
   let httpMock;
   let messageService;
+  let confirmationService;
   const baseUrl = environment.baseUrl;  // Servers Env
 
   const fakeUserData = require('../../../../test/resource/fakeUserData.json');
@@ -49,6 +49,8 @@ describe('AccessMgmtComponent', () => {
   const fakeServiceInputValid = require('../../../../test/resource/fakeUserDataChangeValid.json');
   const fakeFilterByProjectData = require('../../../../test/resource/fakeFilterByProjectData.json');
   const fakeFilterByRoleData = require('../../../../test/resource/fakeFilterByRoleData.json');
+  const accessNodes = require('../../../../test/resource/fakeAccessNodes.json')
+  const fakeDeleteAccess = require('../../../../test/resource/fakeDeleteAccessRequest.json');
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AccessMgmtComponent],
@@ -79,6 +81,7 @@ describe('AccessMgmtComponent', () => {
     httpService = TestBed.inject(HttpService);
     httpMock = TestBed.inject(HttpTestingController);
     messageService = TestBed.inject(MessageService);
+    confirmationService = TestBed.inject(ConfirmationService);
     // fixture.detectChanges();
   });
 
@@ -108,9 +111,9 @@ describe('AccessMgmtComponent', () => {
     httpMock.match(baseUrl + '/api/roles')[0].flush(fakeRolesData);
     if (component.rolesData.success) {
       fakeRolesData.data = fakeRolesData.data.map((role) => ({
-          label: role.roleName,
-          value: role.roleName
-        }));
+        label: role.roleName,
+        value: role.roleName
+      }));
       expect(component.roleList).toEqual(fakeRolesData.data);
     } else {
       // component.messageService.add({ severity: 'error', summary: 'Error in fetching roles. Please try after some time.' });
@@ -133,17 +136,149 @@ describe('AccessMgmtComponent', () => {
     done();
   });
 
-  it('should filter by project', ()=>{
+  it('should filter by project', () => {
     component.searchProject = 'KnowHOW';
     component.filterByProject();
     component.users = fakeFilterByProjectData;
     expect(component.users[0].projectsAccess[0].accessNodes[0].accessItems[0].itemName).toContain('KnowHOW');
   });
 
-  it('should filter by role', ()=>{
+  it('should filter by role', () => {
     component.searchRole = 'ROLE_SUPERADMIN';
     component.filterByRole();
     component.users = fakeFilterByRoleData;
     expect(component.users[0].projectsAccess[0].role).toContain('ROLE_SUPERADMIN');
   });
+
+  it('should show dialog to add', () => {
+    const projectsArr = accessNodes;
+    const projectAccess = [
+      {
+        "role": "ROLE_PROJECT_VIEWER",
+        "accessNodes": accessNodes
+      }
+    ]
+    const index = 0;
+    component.projectFilter = {
+      resetDropdowns: true,
+      clearFilters: () => { }
+    }
+    component.showDialogToAdd(projectsArr, projectAccess, index);
+    expect(component.selectedProjects.length).toEqual(projectsArr.length);
+  })
+
+  it('should save dialog', () => {
+    component.addedProjectsOrNodes = accessNodes;
+    component.selectedProjectAccess = [
+      {
+        "role": "ROLE_PROJECT_VIEWER",
+        "accessNodes": accessNodes
+      }
+    ]
+    component.saveDialog();
+    expect(component.selectedProjectAccess[0].accessNodes.length).toEqual(component.selectedProjects.length);
+  })
+
+  /**confirm  */
+  it('should display message on role change', () => {
+    const event = {
+      "value": "ROLE_SUPERADMIN"
+    };
+    const index = 0;
+    const access = [
+      {
+        "role": "ROLE_SUPERADMIN",
+        "accessNodes": accessNodes
+      }
+    ];
+    component.submitValidationMessage = `A row for ROLE_SUPERADMIN already exists, please add accesses there`;
+    component.onRoleChange(event, index, access);
+    expect(component.submitValidationMessage).toContain(event.value);
+  })
+
+  it('should check access deletion status', () => {
+    const isSuperAdmin = false;
+    const response = fakeDeleteAccess;
+    component.accessDeletionStatus(response, isSuperAdmin);
+    if (response.success) {
+      // message = response.data;
+      spyOn(component, 'getUsers');
+    }
+    expect(component.accessConfirm).toBe(false);
+  });
+
+  it('should show project on tooltip on mouseenter', () => {
+    const event = {
+      'pageX': '805',
+      'pageY': '393'
+    }
+    const item = {
+      "itemId": "6375c3d6b8336258af26e2d9",
+      "itemName": "FASTREPLAT"
+    };
+    const node = {
+      "accessLevel": "project",
+      "accessItems": accessNodes
+    };
+    component.allProjectsData = [
+      {
+          "id": "6375c3d6b8336258af26e2d9",
+          "projectName": "FASTREPLAT",
+          "createdAt": "2022-11-17T06:37:53",
+          "kanban": false,
+          "hierarchy": [
+              {
+                  "hierarchyLevel": {
+                      "level": 1,
+                      "hierarchyLevelId": "bu",
+                      "hierarchyLevelName": "BU"
+                  },
+                  "value": "International"
+              },
+          ],
+          "isKanban": false
+      }
+    ]
+    component.mouseEnter(event, item, node);
+    expect(component.top).toEqual(event['pageY']+'px');
+    expect(component.left).toEqual(event['pageX']+'px');
+    expect(component.showToolTip).toBe(true);
+    expect(component.toolTipHtml).toContain('Project');
+  })
+
+  it('should not show project on tooltip on mouseenter', () => {
+    const event = {
+      'pageX': '805',
+      'pageY': '393'
+    }
+    const item = {
+      "itemId": "6375c3d6b8336258af26e2d9",
+      "itemName": "Test"
+    };
+    const node = {
+      "accessLevel": "hierarchy1",
+      "accessItems": accessNodes
+    };
+    component.allProjectsData = [
+      {
+          "id": "6375c3d6b8336258af26e2d9",
+          "projectName": "FASTREPLAT",
+          "createdAt": "2022-11-17T06:37:53",
+          "kanban": false,
+          "hierarchy": [
+              {
+                  "hierarchyLevel": {
+                      "level": 1,
+                      "hierarchyLevelId": "hierarchy1",
+                      "hierarchyLevelName": "Hierarchy One"
+                  },
+                  "value": "Test"
+              },
+          ],
+          "isKanban": false
+      }
+    ]
+    component.mouseEnter(event, item, node);
+    expect(component.toolTipHtml).not.toContain('Project');
+  })
 });
