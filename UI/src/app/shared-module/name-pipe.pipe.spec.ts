88e750ec726233bc6eef90be3462db06ead50236
Brahmanand <bnm.amethi@gmail.com>
2023-01-04 16:29:16 +0530

    review comment fixed

--- UI/src/app/shared-module/name-pipe.pipe.spec.ts
+++ UI/src/app/shared-module/name-pipe.pipe.spec.ts
@@ -7,7 +7,7 @@ describe('Pipe: Default', () => {
     pipe = new NamePipePipe();
   });
 
-  it('When passing as blank - if case', () => {
+  it('Corrent value is returning', () => {
     const hierarchyData = [
       {
         level: 1,
@@ -48,48 +48,6 @@ describe('Pipe: Default', () => {
     ];
     localStorage.setItem('hierarchyData', JSON.stringify(hierarchyData));
     expect(pipe.transform('')).toBe('');
-  });
-
-  it('When passing value - else case', () => {
-    const hierarchyData = [
-      {
-        level: 1,
-        hierarchyLevelId: 'hierarchyLevelOne',
-        hierarchyLevelName: 'Level One',
-        suggestions: [
-          { name: 'Sample One', code: 'Sample One' },
-          { name: 'Sample Two', code: 'Sample Two' },
-          { name: 'T1', code: 'T1' },
-        ],
-        value: '',
-        required: true,
-      },
-      {
-        level: 2,
-        hierarchyLevelId: 'hierarchyLevelTwo',
-        hierarchyLevelName: 'Level Two',
-        suggestions: [
-          { name: 'Sample Four', code: 'Sample Four' },
-          { name: 'Sample Three', code: 'Sample Three' },
-          { name: 'T2', code: 'T2' },
-        ],
-        value: '',
-        required: true,
-      },
-      {
-        level: 3,
-        hierarchyLevelId: 'hierarchyLevelThree',
-        hierarchyLevelName: 'Level Three',
-        suggestions: [
-          { name: 'Sample Five', code: 'Sample Five' },
-          { name: 'Sample Six', code: 'Sample Six' },
-          { name: 'T3', code: 'T3' },
-        ],
-        value: '',
-        required: true,
-      },
-    ];
-    localStorage.setItem('hierarchyData', JSON.stringify(hierarchyData));
     expect(pipe.transform('Project')).toBe('Project');
   });
 });
