a77c0d13327385ee1be897e0b9b0917ebf9cb995
satchatt <satarupa.chatterjee@publicissapient.com>
2023-01-06 19:17:53 +0530

    20811 development completed.Waiting for code review

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/JiraToolConfigServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/JiraToolConfigServiceImpl.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.publicissapient.kpidashboard.common.model.ToolCredential;
-import com.publicissapient.kpidashboard.common.service.ToolCredentialProvider;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang.StringUtils;
 import org.bson.types.ObjectId;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,10 +22,18 @@ import com.publicissapient.kpidashboard.apis.jira.model.BoardDetailsDTO;
 import com.publicissapient.kpidashboard.apis.jira.model.BoardRequestDTO;
 import com.publicissapient.kpidashboard.apis.jira.model.JiraBoardListResponse;
 import com.publicissapient.kpidashboard.apis.util.RestAPIUtils;
+import com.publicissapient.kpidashboard.common.model.ToolCredential;
+import com.publicissapient.kpidashboard.common.model.application.AssigneeRoles;
+import com.publicissapient.kpidashboard.common.model.application.ProjectAssignee;
+import com.publicissapient.kpidashboard.common.model.application.ProjectBasicConfig;
+import com.publicissapient.kpidashboard.common.model.application.ProjectToolConfig;
+import com.publicissapient.kpidashboard.common.model.application.dto.ProjectAssigneeDTO;
 import com.publicissapient.kpidashboard.common.model.connection.Connection;
+import com.publicissapient.kpidashboard.common.repository.application.ProjectBasicConfigRepository;
+import com.publicissapient.kpidashboard.common.repository.application.ProjectToolConfigRepository;
 import com.publicissapient.kpidashboard.common.repository.connection.ConnectionRepository;
-
-import lombok.extern.slf4j.Slf4j;
+import com.publicissapient.kpidashboard.common.repository.rbac.ProjectAssigneeRepository;
+import com.publicissapient.kpidashboard.common.service.ToolCredentialProvider;
 
 /**
  * class for jira tool config fetch data api
@@ -37,7 +46,7 @@ import lombok.extern.slf4j.Slf4j;
 public class JiraToolConfigServiceImpl {
 
 	private static final String RESOURCE_JIRA_BOARD_ENDPOINT = "/rest/agile/1.0/board?projectKeyOrId=%s&startAt=%d&type=%s";
-
+	private static final String RESOURCE_JIRA_ASSINGEE_ENDPOINT = "user/assignable/search?project=";
 	final ObjectMapper mapper = new ObjectMapper();
 
 	@Autowired
@@ -51,6 +60,13 @@ public class JiraToolConfigServiceImpl {
 
 	@Autowired
 	private ToolCredentialProvider toolCredentialProvider;
+
+	@Autowired
+	private ProjectToolConfigRepository projectToolConfigRepository;
+	@Autowired
+	private ProjectAssigneeRepository projectAssigneeRepository;
+	@Autowired
+	private ProjectBasicConfigRepository projectBasicConfigRepository;
 
 	public List<BoardDetailsDTO> getJiraBoardDetailsList(BoardRequestDTO boardRequestDTO) {
 
@@ -74,8 +90,8 @@ public class JiraToolConfigServiceImpl {
 		boolean isLast = false;
 		do {
 			try {
-				String url = String.format(new StringBuilder(baseUrl).append(RESOURCE_JIRA_BOARD_ENDPOINT).toString(),
-						boardRequestDTO.getProjectKey(), nextPageIndex, boardRequestDTO.getBoardType());
+				String url = String.format(baseUrl + RESOURCE_JIRA_BOARD_ENDPOINT, boardRequestDTO.getProjectKey(),
+						nextPageIndex, boardRequestDTO.getBoardType());
 
 				ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
 
@@ -114,18 +130,130 @@ public class JiraToolConfigServiceImpl {
 		String username = "";
 		String password = "";
 
-		if (connection.isVault()){
-			ToolCredential credential = toolCredentialProvider.findCredential(connection.getUsername() == null ? null : connection.getUsername().trim());
-			if (credential != null){
+		if (connection.isVault()) {
+			ToolCredential credential = toolCredentialProvider
+					.findCredential(connection.getUsername() == null ? null : connection.getUsername().trim());
+			if (credential != null) {
 				username = credential.getUsername();
 				password = credential.getPassword();
 			}
 		} else {
 			username = connection.getUsername() == null ? null : connection.getUsername().trim();
-			password = connection.getPassword() == null ? null
-					: restAPIUtils.decryptPassword(connection.getPassword());
+			password = connection.getPassword() == null ? null : restAPIUtils.decryptPassword(connection.getPassword());
 		}
 		HttpHeaders headers = restAPIUtils.getHeaders(username, password);
 		return new HttpEntity<>(headers);
+	}
+
+	public ProjectAssigneeDTO getProjectAssigneeDetails(String projectConfigId) {
+		ProjectAssigneeDTO projectAssigneeDTO=new ProjectAssigneeDTO();
+		List<AssigneeRoles> assigneeDetailsResponseList=new ArrayList<>();
+		Optional<ProjectBasicConfig>  basicConfig  = projectBasicConfigRepository.findById(new ObjectId(projectConfigId));
+		if(basicConfig.isPresent()) {
+			ProjectBasicConfig projectBasicConfig = basicConfig.get();
+			List<ProjectToolConfig> projectToolConfigs = projectToolConfigRepository
+					.findByToolNameAndBasicProjectConfigId("Jira", new ObjectId(projectConfigId));
+
+			projectToolConfigs.stream().forEach(projectToolConfig -> {
+
+				Optional<Connection> optConnection = connectionRepository.findById(projectToolConfig.getConnectionId());
+				if (optConnection.isPresent()) {
+					Connection connection = optConnection.get();
+					String baseUrl = connection.getBaseUrl() == null ? null : connection.getBaseUrl().trim();
+					String endPoint = connection.getApiEndPoint() == null ? null : connection.getApiEndPoint().trim();
+					String projectKey = projectToolConfig.getProjectKey().trim();
+					getApiCreationAndCall(projectConfigId, assigneeDetailsResponseList, projectToolConfig, connection, baseUrl, endPoint, projectKey);
+				}
+			});
+
+			projectAssigneeDTO.setBasicProjectConfigId(new ObjectId(projectConfigId));
+			projectAssigneeDTO.setAssigneeRoles(assigneeDetailsResponseList);
+			projectAssigneeDTO.setProjectName(projectBasicConfig.getProjectName());
+		}
+		return projectAssigneeDTO;
+	}
+
+	private void getApiCreationAndCall(String projectConfigId, List<AssigneeRoles> assigneeDetailsResponseList, ProjectToolConfig projectToolConfig, Connection connection, String baseUrl, String endPoint, String projectKey) {
+		String url = createApiUrl(baseUrl, endPoint);
+		if (StringUtils.isNotEmpty(url)) {
+			url = url + RESOURCE_JIRA_ASSINGEE_ENDPOINT + projectKey;
+			HttpEntity<?> httpEntity = getHttpEntity(connection);
+			List<AssigneeRoles> assigneeRolesList = fetchAssigneeDetailsRestAPICall(projectToolConfig,
+					httpEntity, url);
+			getAssigneeRoleCheck(projectConfigId, assigneeRolesList);
+			assigneeDetailsResponseList.addAll(assigneeRolesList);
+
+		}
+	}
+
+	private List<AssigneeRoles> getAssigneeRoleCheck(String projectConfigId, List<AssigneeRoles> assigneeRolesList) {
+
+		ProjectAssignee projectAssignee = projectAssigneeRepository
+				.findByBasicProjectConfigId(new ObjectId(projectConfigId));
+		if (null != projectAssignee) {
+			List<AssigneeRoles> existingRoles = projectAssignee.getAssigneeRoles();
+			for (AssigneeRoles roles : existingRoles) {
+				for (AssigneeRoles assigneeRoles : assigneeRolesList) {
+					if (roles.getName().equals(assigneeRoles.getName())
+							&& roles.getDisplayName().equals(assigneeRoles.getDisplayName())) {
+						assigneeRoles.setRoles(roles.getRoles());
+					}
+				}
+			}
+		}
+		return assigneeRolesList;
+	}
+
+	private List<AssigneeRoles> fetchAssigneeDetailsRestAPICall(ProjectToolConfig toolConfig, HttpEntity<?> httpEntity,
+			String url) {
+
+
+		List<AssigneeRoles> assigneeRoles = new ArrayList<>();
+
+			try {
+				ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+				if (response.getStatusCode() == HttpStatus.OK) {
+					log.info(response.getBody());
+					assigneeRoles = convertListFromArray(response, assigneeRoles);
+				} else {
+					String statusCode = response.getStatusCode().toString();
+					log.error("Error while fetching BoardList from {}. with status {}", url, statusCode);
+				}
+
+			} catch (Exception exception) {
+				log.error("Error while fetching assignees for projectKey Id {}:  {}", toolConfig.getProjectKey(),
+						exception.getMessage());
+			}
+
+		return assigneeRoles;
+	}
+
+	public List<AssigneeRoles> convertListFromArray(ResponseEntity<String> response, List<AssigneeRoles> list) {
+		JSONParser jsonParser = new JSONParser();
+		JSONArray jsonArray = null;
+		try {
+			jsonArray = (JSONArray) jsonParser.parse(response.getBody());
+
+			for (Object obj : jsonArray) {
+				AssigneeRoles assigneeRole = new AssigneeRoles();
+				JSONObject jsonObject = (JSONObject) obj;
+				if (jsonObject != null) {
+					assigneeRole.setName(jsonObject.get("name").toString());
+					assigneeRole.setDisplayName(jsonObject.get("displayName").toString());
+					list.add(assigneeRole);
+				}
+			}
+		} catch (ParseException e) {
+			log.error("error while fetching assignee details {}", e.getMessage());
+		}
+		return list;
+	}
+
+	private String createApiUrl(String baseUrl, String endPoint) {
+
+		if (StringUtils.isNotEmpty(baseUrl) && StringUtils.isNotEmpty(endPoint)) {
+			return baseUrl.endsWith("/") ? baseUrl.concat(endPoint) : baseUrl.concat("/").concat(endPoint);
+		}
+		return null;
 	}
 }
