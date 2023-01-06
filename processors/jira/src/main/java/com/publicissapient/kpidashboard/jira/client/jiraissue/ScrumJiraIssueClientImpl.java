b3a5ba109505ffe2636e6fddb3e8a65c4f89b4e1
aksshriv1 <akshat.shrivastav@publicisgroupe.net>
2023-01-06 11:10:59 +0530

    Now Quality KPI's will also see subtask deffect

--- processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/client/jiraissue/ScrumJiraIssueClientImpl.java
+++ processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/client/jiraissue/ScrumJiraIssueClientImpl.java
@@ -72,6 +72,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.text.StringEscapeUtils;
 import org.bson.types.ObjectId;
 import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.joda.time.DateTime;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
@@ -423,7 +424,7 @@ public class ScrumJiraIssueClientImpl extends JiraIssueClient {// NOPMD
 				// Set additional filters
 				setAdditionalFilters(jiraIssue, issue, projectConfig);
 
-				setStoryLinkWithDefect(issue, jiraIssue);
+				setStoryLinkWithDefect(issue, jiraIssue,fields);
 
 				// ADD QA identification field to feature
 				setQADefectIdentificationField(fieldMapping, issue, jiraIssue, fields);
@@ -818,10 +819,11 @@ public class ScrumJiraIssueClientImpl extends JiraIssueClient {// NOPMD
 	 * @param issue
 	 * @param jiraIssue
 	 */
-	private void setStoryLinkWithDefect(Issue issue, JiraIssue jiraIssue) {
-		if (NormalizedJira.DEFECT_TYPE.getValue().equalsIgnoreCase(jiraIssue.getTypeName())
-				|| NormalizedJira.TEST_TYPE.getValue().equalsIgnoreCase(jiraIssue.getTypeName())) {
+	private void setStoryLinkWithDefect(Issue issue, JiraIssue jiraIssue, Map<String, IssueField> fields) {
+		if (NormalizedJira.DEFECT_TYPE.getValue().equalsIgnoreCase(jiraIssue.getTypeName())) {
 			Set<String> defectStorySet = new HashSet<>();
+			String parentKey = null;
+
 			for (IssueLink issueLink : issue.getIssueLinks()) {
 				if (CollectionUtils.isNotEmpty(jiraProcessorConfig.getExcludeLinks())
 						&& jiraProcessorConfig.getExcludeLinks().stream()
@@ -829,6 +831,21 @@ public class ScrumJiraIssueClientImpl extends JiraIssueClient {// NOPMD
 					break;
 				}
 				defectStorySet.add(issueLink.getTargetIssueKey());
+			}
+
+			if (issue.getIssueType().isSubtask()) {
+				if (MapUtils.isNotEmpty(fields)) {
+					try {
+						parentKey = ((JSONObject) fields.get(JiraConstants.PARENT).getValue()).get(JiraConstants.KEY)
+								.toString();
+						defectStorySet.add(parentKey);
+					} catch (JSONException e) {
+						log.error(
+								"JIRA Processor | Error while parsing parent value as JSONObject or converting JSONObject to string",
+								e);
+					}
+
+				}
 			}
 			jiraIssue.setDefectStoryID(defectStorySet);
 		}
