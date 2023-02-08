a4f833c3205a8885ec56d38968d8161ca034becf
hirbabar <hirbabar@publicisgroupe.net>
2023-02-08 21:14:30 +0530

    DTS-21018 - Secure expose API generate token

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/pushdata/controller/ExposeAPIController.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/pushdata/controller/ExposeAPIController.java
@@ -1,10 +1,7 @@
 package com.publicissapient.kpidashboard.apis.pushdata.controller;
 
-import com.publicissapient.kpidashboard.apis.model.ServiceResponse;
-import com.publicissapient.kpidashboard.apis.pushdata.model.dto.ExposeAPITokenRequestDTO;
-import com.publicissapient.kpidashboard.apis.pushdata.service.AuthExposeAPIService;
-import com.publicissapient.kpidashboard.common.util.EncryptionException;
-import lombok.extern.slf4j.Slf4j;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import com.publicissapient.kpidashboard.apis.model.ServiceResponse;
+import com.publicissapient.kpidashboard.apis.pushdata.model.dto.ExposeAPITokenRequestDTO;
+import com.publicissapient.kpidashboard.apis.pushdata.service.AuthExposeAPIService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Validated
 @RestController
@@ -30,7 +31,7 @@ public class ExposeAPIController {
 	@PreAuthorize("hasPermission(null, 'SAVE_PROJECT_TOOL')")
 	@RequestMapping(value = "/generateToken", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE) // NOSONAR
 	public ResponseEntity<ServiceResponse> generateAndSaveToken(
-			@RequestBody @Valid ExposeAPITokenRequestDTO exposeAPITokenRequestDTO) throws EncryptionException {
+			@RequestBody @Valid ExposeAPITokenRequestDTO exposeAPITokenRequestDTO) {
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(authExposeAPIService.generateAndSaveToken(exposeAPITokenRequestDTO));
 	}
