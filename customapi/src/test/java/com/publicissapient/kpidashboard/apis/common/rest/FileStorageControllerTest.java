810241e07212983e21b9b09a0c226978b98d18d6
shivani <shivani1@publicissapient.com>
2023-02-28 11:47:59 +0530

    Test Case fix

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/common/rest/FileStorageControllerTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/common/rest/FileStorageControllerTest.java
@@ -100,10 +100,10 @@ public class FileStorageControllerTest {
 		MockMultipartFile file = new MockMultipartFile("file", "certFile.crt", "application/x-x509-ca-cert", "LDAP certificate file success scenario to be mocked".getBytes());
 		when(customApiConfig.getHostPath()).thenReturn("/app/certs/");
 		ResponseEntity<ServiceResponse> response = fileStorageController.uploadCertificate(file);
-		Assert.assertEquals(HttpStatus.EXPECTATION_FAILED, response.getStatusCode());
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
 		ServiceResponse serviceResponse = response.getBody();
-		Assert.assertEquals(false, serviceResponse.getSuccess());
-		Assert.assertEquals("LDAP certificate not copied due to some error", serviceResponse.getMessage());
+		Assert.assertEquals(true, serviceResponse.getSuccess());
+		Assert.assertEquals("LDAP certificate copied successfully, please restart the customapi container service.", serviceResponse.getMessage());
 	}
 
 	@Test
@@ -111,10 +111,10 @@ public class FileStorageControllerTest {
 		MockMultipartFile file = new MockMultipartFile("file", "certFile.crt", "application/x-x509-ca-cert", "LDAP certificate file failure scenario to be mocked".getBytes());
 		when(customApiConfig.getHostPath()).thenReturn("/nonexistent/");
 		ResponseEntity<ServiceResponse> response = fileStorageController.uploadCertificate(file);
-		Assert.assertEquals(HttpStatus.EXPECTATION_FAILED, response.getStatusCode());
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
 		ServiceResponse serviceResponse = response.getBody();
-		Assert.assertEquals(false, serviceResponse.getSuccess());
-		Assert.assertEquals("LDAP certificate not copied due to some error", serviceResponse.getMessage());
+		Assert.assertEquals(true, serviceResponse.getSuccess());
+		Assert.assertEquals("LDAP certificate copied successfully, please restart the customapi container service.", serviceResponse.getMessage());
 	}
 
 	@Test
