7a355a82a4070a4e972f688f4778d611e0f3c7d2
hirbabar <hirbabar@publicisgroupe.net>
2023-03-14 22:29:02 +0530

    resolve conflicts

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/model/IterationKpiModalValue.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/model/IterationKpiModalValue.java
@@ -84,13 +84,13 @@ public class IterationKpiModalValue implements Serializable {
 	private String potentialDelay;
 	@JsonProperty("Issue Priority")
 	private String issuePriority;
-	@JsonProperty("Delay(in days)")
-	private String delayInDays;
-	@JsonProperty("Actual Start Date")
-	private String actualStartDate;
 	@JsonProperty("Linked Stories")
 	private Map<String, String> linkedStories;
 	@JsonProperty("Linked Stories Size")
 	private String linkedStoriesSize;
+	@JsonProperty("Delay(in days)")
+	private String delayInDays;
+	@JsonProperty("Actual Start Date")
+	private String actualStartDate;
 
 }
