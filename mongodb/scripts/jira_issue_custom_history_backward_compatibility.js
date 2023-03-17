26c7ba7868aefe1e6fab5f7acfb222304e34b6b2
Harsh Gupta <harsh.gupta@publicissapient.com>
2023-03-17 18:08:35 +0530

    backward compactibility

---
+++ mongodb/scripts/jira_issue_custom_history_backward_compatibility.js
@@ -0,0 +1,57 @@
+print("Start : backward compatibility script for jira issue custom history");
+
+function hasStorySprintDetails(historyObject){
+    return historyObject.storySprintDetails && historyObject.storySprintDetails.length !== 0;
+}
+
+var jiraIssueHistorys = db.jira_issue_custom_history.find({})
+
+jiraIssueHistorys.forEach(function(historyObject){
+    if(hasStorySprintDetails(historyObject))
+    {
+        var storySprintDetails = historyObject.storySprintDetails;
+        var prevChangedTo="";
+        var changedLogs = [];
+        var storyID = historyObject.storyID;
+        storySprintDetails.forEach(function(obj){
+            var changedTo = obj.fromStatus;
+            var updatedOn = obj.activityDate;
+            changedLogs.push({"changedFrom":prevChangedTo,changedTo,updatedOn});
+            prevChangedTo=changedTo;
+        });
+
+         var result = db.jira_issue_custom_history.updateOne({"storyID": storyID},
+        				{
+        $set: {
+            'statusChangeLog':changedLogs
+        },
+        $unset: {
+            	"storySprintDetails":""
+        }
+    });
+        print(result);
+
+    }
+
+});
+
+print("End : backward compatibility script for jira issue custom history");
+
+/*
+Query to fetch count of object with StorySprintDetails
+
+db.jira_issue_custom_history.find({"storySprintDetails":{$exists: true, $not: {$size: 0}}})
+   .projection({})
+   .sort({_id:-1})
+   .count()
+*/
+
+/*
+Query to fetch count of object with StatusChangeLog
+
+db.jira_issue_custom_history.find({"statusChangeLog":{$exists: true, $not: {$size: 0}}})
+   .projection({})
+   .sort({_id:-1})
+   .count()
+*/
+
