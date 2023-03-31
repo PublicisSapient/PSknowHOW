90c595bba4d16e723d4360419ac07f79763277d4
brahmanand1 <44412993+brahmanand1@users.noreply.github.com>
2023-03-31 16:16:31 +0530

    move tracelog calls

--- UI/src/app/dashboard/filter/filter.component.ts
+++ UI/src/app/dashboard/filter/filter.component.ts
@@ -1047,7 +1047,6 @@ export class FilterComponent implements OnInit {
          localStorage.setItem('filter',selectedProject);
         this.filterForm?.get('selectedSprintValue')?.setValue('');
         this.selectedProjectData = this.trendLineValueList.find(x => x.nodeId === selectedProject);
-        this.getProcessorsTraceLogsForProject(this?.selectedProjectData['basicProjectConfigId']);
         this.filteredAddFilters['sprint'] = [];
         if (this.additionalFiltersDdn && this.additionalFiltersDdn['sprint']) {
           this.filteredAddFilters['sprint'] = [...this.additionalFiltersDdn['sprint']?.filter((x) =>x['parentId']?.includes(selectedProject))];
@@ -1081,6 +1080,7 @@ export class FilterComponent implements OnInit {
         this.selectedFilterArray = [];
         this.selectedFilterArray.push(selectedSprint);
         this.createFilterApplyData();
+        this.getProcessorsTraceLogsForProject(this?.selectedProjectData['basicProjectConfigId']);
         this.service.select(this.masterData,this.filterData,this.filterApplyData,this.selectedTab);
       } else {
         if (type == 1) {
