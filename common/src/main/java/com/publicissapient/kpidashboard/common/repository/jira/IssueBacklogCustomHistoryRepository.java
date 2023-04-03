36020ede1d98aa0e2c0fa1d704b76044805a4d5e
Harsh Gupta <harsh.gupta@publicissapient.com>
2023-04-03 18:20:51 +0530

    initial commit

---
+++ common/src/main/java/com/publicissapient/kpidashboard/common/repository/jira/IssueBacklogCustomHistoryRepository.java
@@ -0,0 +1,48 @@
+package com.publicissapient.kpidashboard.common.repository.jira;
+
+import com.publicissapient.kpidashboard.common.model.jira.IssueBacklogCustomHistory;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IssueBacklogCustomHistoryRepository extends CrudRepository<IssueBacklogCustomHistory, String>,
+        QuerydslPredicateExecutor<IssueBacklogCustomHistory>, JiraIssueHistoryCustomQueryRepository {
+
+    /**
+     * Find by story id list.
+     *
+     * @param storyID              the story id
+     * @param basicProjectConfigId basicProjectConfigId
+     * @return the list
+     */
+    List<IssueBacklogCustomHistory> findByStoryIDAndBasicProjectConfigId(String storyID, String basicProjectConfigId);
+
+    /**
+     * Find by story id in list.
+     *
+     * @param storyList the story list
+     * @return the list
+     */
+    List<IssueBacklogCustomHistory> findByStoryIDIn(List<String> storyList);
+
+    /**
+     * Deletes all documents that matches with given projectID.
+     *
+     * @param projectID String projectID
+     */
+    void deleteByBasicProjectConfigId(String projectID);
+
+    /**
+     * Find by story id list.
+     *
+     * @param storyID              the story id
+     * @param basicProjectConfigId basicProjectConfigId
+     * @return the list
+     */
+    List<IssueBacklogCustomHistory> findByStoryIDInAndBasicProjectConfigIdIn(List<String> storyID,
+                                                                          List<String> basicProjectConfigId);
+
+}
