55a0a1b4b6ae163e600e56fa994833644bcf9cb4
purushottam08 <purushottam.gupta@publicissapient.com>
2023-04-06 12:34:41 +0530

    DTS-21784 KnowHOW| Refactor Jira Processor code and make it error free

---
+++ common/src/main/java/com/publicissapient/kpidashboard/common/model/jira/JiraHistoryChangeLog.java
@@ -0,0 +1,17 @@
+package com.publicissapient.kpidashboard.common.model.jira;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JiraHistoryChangeLog {
+	private String changedFrom;
+	private String changedTo;
+	private LocalDateTime updatedOn;
+}
