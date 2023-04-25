ea4c2dea6047255f8bd15a3c14a218243e36d94d
narsingh9 <narender.singhnegi@publicissapient.com>
2023-04-25 17:09:18 +0530

    decryption logic implemented

---
+++ processors/jiratest/src/main/java/com/publicissapient/kpidashboard/jiratest/config/MongoConfig.java
@@ -0,0 +1,48 @@
+package com.publicissapient.kpidashboard.apis.config;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.publicissapient.kpidashboard.common.service.AesEncryptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private int port;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Value("${spring.data.mongodb.username}")
+    private String user;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
+    @Autowired
+    private AesEncryptionService aesEncryptionService;
+
+    @Autowired
+    private JiraTestProcessorConfig jiraTestProcessorConfig;
+
+    @Bean
+    public MongoClient mongoClient() {
+        password = aesEncryptionService.decrypt(password, jiraTestProcessorConfig.getAesEncryptionKey());
+        MongoCredential credential = MongoCredential.createCredential(user, database, password.toCharArray());
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder ->
+                        builder.hosts(Collections.singletonList(new ServerAddress(host, port))))
+                .credential(credential)
+                .build();
+        return MongoClients.create(settings);
+    }
+}
