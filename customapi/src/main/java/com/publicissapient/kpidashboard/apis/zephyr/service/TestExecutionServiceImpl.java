535acb3d31fe2d22d826dc76a376c75eff175633
rahgupta34 <rahgupta34@publicisgroupe.net>
2023-05-10 18:04:59 +0530

    reverted not needed change

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/zephyr/service/TestExecutionServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/zephyr/service/TestExecutionServiceImpl.java
@@ -110,7 +110,7 @@ public class TestExecutionServiceImpl extends ZephyrKPIService<Double, List<Obje
 	}
 
 	@Override
-	public Map<String, Object> fetchKPIDataFromDb(List<Node> leafNodeList, String startsdfDate, String endczxcDate,
+	public Map<String, Object> fetchKPIDataFromDb(List<Node> leafNodeList, String startDate, String endDate,
 			KpiRequest kpiRequest) {
 
 		Map<String, Object> resultListMap = new HashMap<>();
