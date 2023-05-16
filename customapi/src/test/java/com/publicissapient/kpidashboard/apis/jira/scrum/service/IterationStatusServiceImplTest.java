13e8646c8378edce8c9bfd4219117c977bb78dec
pankaj5090 <pankaj5090@gmail.com>
2023-05-16 12:35:21 +0530

    remove double semicolon

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/IterationStatusServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/IterationStatusServiceImplTest.java
@@ -36,7 +36,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
