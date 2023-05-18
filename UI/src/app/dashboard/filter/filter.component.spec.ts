195111ad6acf146f6d9ae6f83a1465e83e4c0df6
HinPublicis <hinshah@publicisgroupe.net>
2023-05-18 16:39:56 +0530

    removed force it

--- UI/src/app/dashboard/filter/filter.component.spec.ts
+++ UI/src/app/dashboard/filter/filter.component.spec.ts
@@ -830,7 +830,7 @@ describe('FilterComponent', () => {
     expect(spy).toHaveBeenCalled();
   });
 
-  fit('should get format date based on iteration/milestone', () => {
+  it('should get format date based on iteration/milestone', () => {
     component.selectedTab = 'iteration';
     component.filteredAddFilters = {
       sprint: [
