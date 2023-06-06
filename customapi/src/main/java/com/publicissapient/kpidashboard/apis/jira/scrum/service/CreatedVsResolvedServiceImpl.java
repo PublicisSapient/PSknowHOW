6ed90d963f74097cdad8766d62a939e57aff5a59
kunal.kamble@publicissapient.com <kunkambl@publicisgroupe.net>
2023-06-06 11:08:22 +0530

    major bug fix

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/CreatedVsResolvedServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/CreatedVsResolvedServiceImpl.java
@@ -96,7 +96,6 @@ public class CreatedVsResolvedServiceImpl extends JiraKPIService<Double, List<Ob
 	private static final String PROJECT_WISE_CLOSED_STORY_STATUS = "projectWiseClosedStoryStatus";
 	private static final String TAGGED_DEFECTS_CREATED_AFTER_SPRINT = "Added Defects";
 	private static final String TAGGED_DEFECTS = "Total Defects";
-	private static final String CLOSED = "CLOSED";
 	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
 	@Autowired
@@ -475,8 +474,13 @@ public class CreatedVsResolvedServiceImpl extends JiraKPIService<Double, List<Ob
 		return calculateKpiValueForDouble(valueList, kpiName);
 	}
 
-	
-
+	/**
+	 *
+	 * @param allSubTasks
+	 * @param sprintDetails
+	 * @param subTaskHistory
+	 * @return sprint sub task bugs
+	 */
 	public List<JiraIssue> getTotalSubTasks(List<JiraIssue> allSubTasks, SprintDetails sprintDetails,
 			List<JiraIssueCustomHistory> subTaskHistory) {
 		LocalDateTime sprintEndDate = sprintDetails.getCompleteDate() != null
