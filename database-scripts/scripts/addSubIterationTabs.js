9601be16582a0019e6430056a423cb41d3198a74
shivani <shivani1@publicissapient.com>
2023-06-16 20:20:05 +0530

    add_sub_itertaion_issue_fix

--- database-scripts/scripts/addSubIterationTabs.js
+++ database-scripts/scripts/addSubIterationTabs.js
@@ -31,13 +31,15 @@ db.user_board_config.find({}).forEach(function(use_board) {
  boardScrum.forEach(function(scrum){
      if(scrum.boardName == "Iteration"){
            scrum.kpis.forEach(function(kpi){
-                if (isIterationReviewKpi(kpi.kpiId) && kpi.subCategoryBoard == undefined)
+                if (isIterationReviewKpi(kpi.kpiId) && kpi.subCategoryBoard == undefined){}
                         print("kpi_id Iteration Review" + kpi.kpiId);
                         kpi.subCategoryBoard = "Iteration Review";
+                        }
 
-                if (isIterationProgressKpi(kpi.kpiId) && kpi.subCategoryBoard == undefined)
+                if (isIterationProgressKpi(kpi.kpiId) && kpi.subCategoryBoard == undefined){
                     print("kpi_id Iteration Progress" + kpi.kpiId);
                     kpi.subCategoryBoard = "Iteration Progress";
+                    }
             });
      }
  });
