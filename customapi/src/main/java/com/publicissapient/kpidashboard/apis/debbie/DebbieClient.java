5688128957c294aaa2063943378edb1bcafc6eb3
kunal.kamble@publicissapient.com <kunkambl@publicisgroupe.net>
2023-06-26 18:04:27 +0530

    initial debbie commit

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/debbie/DebbieClient.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/debbie/DebbieClient.java
@@ -3,6 +3,7 @@ package com.publicissapient.kpidashboard.apis.debbie;
 import com.google.gson.Gson;
 import com.publicissapient.kpidashboard.apis.config.CustomApiConfig;
 import com.publicissapient.kpidashboard.apis.debbie.model.DebbieConfig;
+import com.publicissapient.kpidashboard.apis.debbie.model.RepoActivity;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -12,11 +13,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 public class DebbieClient {
 
     private RestTemplate restTemplate;
-    private static final String DEBBIE_ENROLL_URL = "api/beta/repositories/";
-    private static final String DEBBIE_TRIGGER_SCAN_URL = "metric/%s/trigger-scan";
+    private static final String DEBBIE_ENROLL_URL = "/beta/repositories/";
+    private static final String DEBBIE_TRIGGER_SCAN_URL = "/metric/%s/trigger-scan";
+
+    private static final String REPO_ACTIVITY_URL = "/metric/{project_code}/repo-activity/";
     private static final String X_API_KEY = "X-API-Key";
     private HttpHeaders httpHeaders;
 
@@ -47,6 +52,13 @@ public class DebbieClient {
 		return response.getStatusCode().value();
 
 	}
+
+//    public RepoActivity repoActivityCall(String projectKey) {
+//        setHttpHeaders();
+//        String repoActivityUrl = String.format(REPO_ACTIVITY_URL, projectKey);
+//        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+//        ResponseEntity<List<RepoActivity>> response = restTemplate.getForEntity(customApiConfig.getDebbieURL() + repoActivityUrl, entity, RepoActivity.class);
+//    }
 
     public void setHttpHeaders() {
         httpHeaders.add(X_API_KEY, customApiConfig.getDebbieAPIKey());
