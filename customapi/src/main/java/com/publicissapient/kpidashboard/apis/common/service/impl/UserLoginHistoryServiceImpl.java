a34439e9cdfa1e66dd72127d6b100eb5288af7b7
aksshriv1 <akshat.shrivastav@publicissapient.com>
2023-07-20 00:02:51 +0530

    Review Comments fixes

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/common/service/impl/UserLoginHistoryServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/common/service/impl/UserLoginHistoryServiceImpl.java
@@ -1,6 +1,25 @@
+/*******************************************************************************
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package com.publicissapient.kpidashboard.apis.common.service.impl;
 
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,6 +38,12 @@ public class UserLoginHistoryServiceImpl implements UserLoginHistoryService {
 	@Autowired
 	private UserLoginHistoryRepository userLoginHistoryRepository;
 
+	/**
+	 *
+	 * @param userInfo
+	 * @param status
+	 * @return
+	 */
 	@Override
 	public UsersLoginHistory createUserLoginHistoryInfo(UserInfo userInfo, String status) {
 		UsersLoginHistory usersLoginHistoryInfo = new UsersLoginHistory();
@@ -27,7 +52,9 @@ public class UserLoginHistoryServiceImpl implements UserLoginHistoryService {
 		usersLoginHistoryInfo.setEmailId(userInfo.getEmailAddress());
 		usersLoginHistoryInfo.setLoginType(userInfo.getAuthType().toString());
 		usersLoginHistoryInfo.setStatus(status);
-		usersLoginHistoryInfo.setDateAndTime(String.valueOf(Calendar.getInstance().getTime()));
+		LocalDateTime localDateTime = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		usersLoginHistoryInfo.setDateTime(localDateTime.format(formatter));
 		return userLoginHistoryRepository.save(usersLoginHistoryInfo);
 	}
 }
