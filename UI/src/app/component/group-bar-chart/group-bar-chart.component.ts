4230a3ae199228c558893b4e28dcba4c7feba2e6
brahmanand1 <44412993+brahmanand1@users.noreply.github.com>
2023-07-24 18:15:08 +0530

    css issue fixed

--- UI/src/app/component/group-bar-chart/group-bar-chart.component.ts
+++ UI/src/app/component/group-bar-chart/group-bar-chart.component.ts
@@ -342,7 +342,7 @@ export class GroupBarChartComponent implements OnChanges {
     })
 
     legendDiv.html(htmlString)
-      .style('left', 40 + 'px')
+      .style('left', 25 + 'px')
       .style('bottom', 20 + 'px')
       .style('top', y[0] + 30 + 'px');
   }
