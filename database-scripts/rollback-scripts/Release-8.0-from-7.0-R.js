1d7788dbf4f237f74929bef1272dde6efa3e4831
purushottam08 <purushottam.gupta@publicissapient.com>
2023-08-10 10:58:40 +0530

    rollback added

--- database-scripts/rollback-scripts/Release-8.0-from-7.0-R.js
+++ database-scripts/rollback-scripts/Release-8.0-from-7.0-R.js
@@ -117,6 +117,22 @@ db.getCollection('metadata_identifier').updateMany(
    }}
 );
 
+db.action_policy_rule.updateOne(
+{
+    "name": "Fetch Sprint"
+},
+{
+$set: {
+        "name": "Fetch Sprint",
+        "roleAllowed": "",
+        "description": "super admin and project admin can run active sprint fetch",
+        "roleActionCheck": "action == 'TRIGGER_SPRINT_FETCH'",
+        "condition": "subject.authorities.contains('ROLE_SUPERADMIN') || subject.authorities.contains('ROLE_PROJECT_ADMIN')",
+        "createdDate": new Date(),
+        "lastModifiedDate": new Date(),
+        "isDeleted": false
+    }
+});
 //dropping field_mapping_structure table
 db.getCollection('field_mapping_structure').drop();
 
