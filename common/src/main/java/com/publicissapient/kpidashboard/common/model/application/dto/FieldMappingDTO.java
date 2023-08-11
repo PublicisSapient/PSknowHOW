74e0b265ef523610a3efcb424b3b48f6e0593a0d
shivani <shivani1@publicissapient.com>
2023-08-11 17:10:29 +0530

    Fieldmapping resolver

--- common/src/main/java/com/publicissapient/kpidashboard/common/model/application/dto/FieldMappingDTO.java
+++ common/src/main/java/com/publicissapient/kpidashboard/common/model/application/dto/FieldMappingDTO.java
@@ -104,7 +104,6 @@ public class FieldMappingDTO extends BasicModel {
 	private String jiraDefectRejectionStatusKPI151;
 	private String jiraDefectRejectionStatusAVR;
 	private String jiraDefectRejectionStatusKPI28;
-	private String jiraDefectRejectionStatusKPI34;
 	private String jiraDefectRejectionStatusKPI37;
 	private String jiraDefectRejectionStatusKPI35;
 	private String jiraDefectRejectionStatusKPI82;
@@ -123,7 +122,6 @@ public class FieldMappingDTO extends BasicModel {
 	private List<String> jiraDefectRemovalStatus;
 	private List<String> jiraDefectRemovalStatusKPI34;
 	private List<String> jiraDefectRemovalIssueType;
-	private List<String> jiraDefectRemovalIssueTypeKPI34;
 	// Added for Defect Reopen Rate KPI.
 	private List<String> jiraDefectClosedStatus;
 	private List<String> jiraDefectClosedStatusKPI137;
@@ -216,7 +214,6 @@ private String jiraLiveStatusKPI127;
 	private List<String> resolutionTypeForRejection;
 	private List<String> resolutionTypeForRejectionAVR;
 	private List<String> resolutionTypeForRejectionKPI28;
-	private List<String> resolutionTypeForRejectionKPI34;
 	private List<String> resolutionTypeForRejectionKPI37;
 	private List<String> resolutionTypeForRejectionKPI35;
 	private List<String> resolutionTypeForRejectionKPI82;
