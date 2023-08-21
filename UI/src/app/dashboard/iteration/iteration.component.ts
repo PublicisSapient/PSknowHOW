8e46d02c2ae10fe7fd46d421283c19a467509076
Bhagyashree Shettigar <bhashett1@publicisgroupe.net>
2023-08-21 18:02:17 +0530

    fixed DTS-27804 bug

--- UI/src/app/dashboard/iteration/iteration.component.ts
+++ UI/src/app/dashboard/iteration/iteration.component.ts
@@ -916,7 +916,7 @@ export class IterationComponent implements OnInit, OnDestroy {
     this.kpiChartData[event.kpiDetail?.kpiId] = [];
     const currentKPIGroup = this.helperService.groupKpiFromMaster('Jira', false, this.masterData, this.filterApplyData, this.filterData, {}, event?.kpiDetail?.groupId,'Iteration');
     if (currentKPIGroup?.kpiList?.length > 0) {
-        this.postJiraKpi(this.kpiJira, 'jira');
+        this.postJiraKpi(currentKPIGroup, 'jira');
     }
   }
 
