b7cf2b91af1efe46c1041006bf915449320eae08
HinPublicis <hinshah@publicisgroupe.net>
2023-09-06 11:43:24 +0530

    commented required fake json

--- UI/src/app/dashboard/backlog/backlog.component.ts
+++ UI/src/app/dashboard/backlog/backlog.component.ts
@@ -234,9 +234,9 @@ export class BacklogComponent implements OnInit, OnDestroy{
                 });
             }
           }
-          if(this.jiraKpiData && Object.keys(this.jiraKpiData)?.length>0 && this.jiraKpiData?.hasOwnProperty('kpi138')){
-            this.jiraKpiData['kpi138'] = require('../../../test/resource/fakeBacklogReadinessKpi.json');
-          }
+          // if(this.jiraKpiData && Object.keys(this.jiraKpiData)?.length>0 && this.jiraKpiData?.hasOwnProperty('kpi138')){
+          //   this.jiraKpiData['kpi138'] = require('../../../test/resource/fakeBacklogReadinessKpi.json');
+          // }
           this.jiraKpiData = Object.assign({}, this.jiraKpiData, localVariable);
           this.createAllKpiArray(this.jiraKpiData);
         } else {
