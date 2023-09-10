45904c3f6742dabbb0cb6103c1dfa281741085d7
HinPublicis <hinshah@publicisgroupe.net>
2023-09-10 18:28:52 +0530

    group bar chart

--- UI/src/app/component/groupedstackchart/groupstackchart.component.ts
+++ UI/src/app/component/groupedstackchart/groupstackchart.component.ts
@@ -55,7 +55,7 @@ export class GroupstackchartComponent implements OnChanges {
     // only run when property "data" changed
     if (changes['data']) {
       this.dataPoints = this.data.length;
-      // this.dataLength = Object.keys(this.data[0]?.value)?.length;
+      this.dataLength = this.dataPoints; //Object.keys(this.data[0]?.value)?.length;
       this.elem = this.viewContainerRef.element.nativeElement;
       if (!changes['data'].firstChange) {
         this.draw('update');
@@ -72,31 +72,34 @@ export class GroupstackchartComponent implements OnChanges {
     d3.select(elem).select('svg').remove();
     d3.select(elem).select('.tooltip').remove();
     d3.select(elem).select('.legend').remove();
-    d3.select(elem).select('.d3-legend').remove();
+    // console.log("it", d3.select(elem).select('#d3-legend').select('div'));
+
+    // d3.select(elem).select('#d3-legend').select('div').remove();
 
     d3.select(elem).select('#verticalSVG').select('svg').remove();
     d3.select(elem).select('#horizontalSVG').select('svg').remove();
-    d3.select(elem).select('#svgLegend').select('svg').remove();
-    d3.select(elem).select('#legendIndicator').select('svg').remove();
+    d3.select(elem).select('#svgLegend').select('div').remove();
+    // d3.select(elem).select('#legendIndicator').select('svg').remove();
     d3.select(elem).select('#xCaptionContainer').select('text').remove();
 
     const data = this.formatData();
-    const thresholdValue = this.thresholdValue;
-    const barWidth = 20;
+
+    // const thresholdValue = this.thresholdValue;
+    // const barWidth = 20;
     // let width = this.dataLength * barWidth * 8;
-    const width = this.dataPoints <= 5 ? document.getElementById('groupstackchart').offsetWidth - 70 : this.dataPoints * barWidth * 4;
+    const width = this.dataPoints <= 5 ? document.getElementById('groupstackchart').offsetWidth - 70 : this.dataPoints * 20 * 4;
     // let spacingVariable = width > 1500 ? 145 : width > 1000 ? 120 : width > 600 ? 70 : 50;
-    const spacingVariable = 50;
-    const height = 225;
+    // const spacingVariable = 20;
+    const height = 190;
     const margin = 50;
     const marginLeft = 40;
     const marginTop = 35;
-    const marginBottom = 60;
-    const marginRight = 100;
+    // const marginBottom = 60;
+    // const marginRight = 100;
 
     const svgX = d3.select(elem).select('#horizontalSVG').append('svg')
       .attr('width', width)
-      .attr('height', (height + 35) + 'px')
+      .attr('height', (height + 15) + 'px')
       .style('text-align', 'center')
       .append('g')
       .attr('transform', `translate(${0}, ${marginTop})`);
@@ -111,13 +114,13 @@ export class GroupstackchartComponent implements OnChanges {
     /** Add x-axis */
     const x0 = d3.scaleBand()
       .rangeRound([0, width - margin]);
-    if (this.dataLength < 5) {
-      x0.paddingInner(.5);
-    } else if (this.dataPoints == 2) {
-      x0.paddingInner(0.4);
-    } else if (this.dataPoints == 3) {
-      x0.paddingInner(0.2);
-    }
+    // if (this.dataLength < 5) {
+    //   x0.paddingInner(.5);
+    // } else if (this.dataPoints == 2) {
+    //   x0.paddingInner(0.4);
+    // } else if (this.dataPoints == 3) {
+      x0.paddingInner(0.5);
+    // }
 
     const x1 = d3.scaleBand();
 
@@ -146,7 +149,7 @@ export class GroupstackchartComponent implements OnChanges {
     const y1 = d3.scaleBand();
     let z; let stackColorsList;
     // if (this.kpiId != 'kpi125' && this.kpiId != 'kpi127') {
-    stackColorsList = ['#DEB0D2', '#F2C69B', '#B395E2', '#B5E7BE', '#DF9292', '#B5C6E7', '#ff8c00', '#3F51B5', '#aaaaaa'];
+    stackColorsList = ['#DEB0D2', '#F2C69B', '#B395E2', '#B5E7BE', '#DF9292', '#B5C6E7', '#ff8c00', '#3F51B5', '#aaaaaa', '#E7FFAC', '#85E3FF', '#8DA47E'];
     z = d3.scaleOrdinal()
       .range(stackColorsList);
     // } else {
@@ -161,10 +164,10 @@ export class GroupstackchartComponent implements OnChanges {
       return d.group;
     }));
     x1.domain(data.map(function (d, i) {
-      return d.group;
+      return d.xName;
     }))
       .rangeRound([0, x0.bandwidth()]);
-      
+
     // .padding(0.2);// bar width
     const actualTypes = [];
     data.forEach(function (d) {
@@ -176,12 +179,18 @@ export class GroupstackchartComponent implements OnChanges {
     const keys = z.domain();
     let groupData = d3.rollup(data, function (d, i) {
       const d2 = { xName: d[0].xName, group: d[0].group };
-      d.forEach(function (dx) {
+      d2['hoverText'] = {};
+      d.forEach((dx) => {
+        console.log("dx", dx);
+        
         d2[dx.type] = dx.value;
+        for (let key in dx?.hoverText) {
+          d2['hoverText'][key] = dx?.hoverText[key];
+        }
       });
       return d2;
     }, function (d) {
-      return d.xName;
+      return d.group;
     });
 
     // .key(function (d) { return d.xName + d.sprojectName; })
@@ -190,17 +199,29 @@ export class GroupstackchartComponent implements OnChanges {
     groupData = Array.from(groupData).map(function (d) {
       return d[1];
     });
-    console.log(this.kpiId+"groupData", groupData);
-
+    // for(let i = 0; i<groupData?.length; i++){
+    //   for(let j = 0; j < actualTypes.length; j++){
+    //     if(!groupData[i].hasOwnProperty(actualTypes[j])){
+    //       groupData[i][actualTypes[j]] = 0;
+    //     }   
+    //   }
+    // }
+    console.log("groupData", groupData);
+    
     const stackData = stack
       .keys(keys)(groupData);
-
+    console.log("stackData", stackData);
+    
     svgX.append('g')
       .attr('class', 'xAxis')
       .attr('transform', `translate(0, ${y(0)})`)
       .call(d3.axisBottom(x0));
 
-
+    d3.select('.xAxis')
+      .selectAll('.tick text').style('width','70px')
+      // .attr('transform', 'rotate(-10)')
+      .call(this.wrap, 60) // select all the text elements 
+    
     const XCaption = d3
       .select(this.elem).select('#xCaptionContainer').append('text')
       .attr('x', ((document.getElementById('groupstackchart').offsetWidth - 70) / 2) - 24)
@@ -208,20 +229,20 @@ export class GroupstackchartComponent implements OnChanges {
       .attr('transform', 'rotate(0)')
       .text(this.xCaption);
     const xTick = self.dataPoints === 1 ? width > 1600 ? 20 : width > 1500 ? -20 : width > 1000 ? 20 : 10 : 0;
-    
-    svgX
-      .select('.xAxis')
-      .selectAll('.tick text')
-      .attr('x', xTick)
-      .attr('y', 15);
-    svgX
-      .select('.xAxis')
-      .selectAll('line')
-      .attr('x1', xTick)
-      .attr('x2', xTick)
-      .attr('y1', 0)
-      .attr('y2', 10)
-      .style('stroke', '#333333');
+
+    // svgX
+    //   .select('.xAxis')
+    //   .selectAll('.tick text')
+    //   .attr('x', xTick)
+    //   .attr('y', 15);
+    // svgX
+    //   .select('.xAxis')
+    //   .selectAll('line')
+    //   .attr('x1', xTick)
+    //   .attr('x2', xTick)
+    //   .attr('y1', 0)
+    //   .attr('y2', 10)
+    //   .style('stroke', '#333333');
 
     svgY.append('g')
       .attr('class', 'yAxis')
@@ -249,7 +270,6 @@ export class GroupstackchartComponent implements OnChanges {
       .style('fill', 'none')
       .attr('class', 'gridline');
 
-
     const serie = svgX.selectAll('.serie')
       .data(stackData)
       .enter().append('g')
@@ -266,72 +286,73 @@ export class GroupstackchartComponent implements OnChanges {
 
 
     serie.selectAll('rect')
-      .data(function (d) {      
+      .data(function (d) {
         return d;
       })
       .enter().append('rect')
       .attr('class', 'serie-rect1')
       .attr('x', function (d, i) {
-        return self.dataPoints === 1 ? spacingVariable + x1(d.data.group) : x1(d.data.group);;
+        return x0(d.data.group); //self.dataPoints === 1 ? spacingVariable : x1(d.data.group);
       })
       .attr('y', function (d) {
         return y(d[1]);
       })
-      .attr('transform', function (d) {
-        console.log(x0(d?.data?.group));
-        
-        return 'translate(' + x0(d?.data?.xName) + ',0)';
-      })
+      // .attr('transform', function (d) {
+      //   let val = x0(d?.data?.group);
+      //   return 'translate(' + val + ',0)';
+      // })
       .attr('height', function (d) {
-        return Math.abs(y(d[0]) - y(d[1]));
+        return (y(d[0]) - y(d[1]));
       })
-      .attr('width', barWidth)
+      .attr('width', x0.bandwidth())
       .on('click', function (d, i) {
       })
-    // .on('mouseover', function (event, d) {
-    //   const topValue = 75;
-    // if (d.data.hoverValue) {
+      .on('mouseover', function (event, d) {
+        const topValue = 75;
+        if (d.data?.hoverText) {
 
-    //   const circle = event.target;
-    //   const {
-    //     top: yPosition,
-    //     left: xPosition
-    //   } = circle.getBoundingClientRect();
+          const circle = event.target;
+          const {
+            top: yPosition,
+            left: xPosition
+          } = circle.getBoundingClientRect();
 
-    //   div.transition()
-    //     .duration(200)
-    //     .style('display', 'block')
-    //     .style('opacity', .9);
+          div.transition()
+            .duration(200)
+            .style('display', 'block')
+            .style('opacity', .9);
 
-    //   const dataObj = JSON.parse(JSON.stringify(d.data));
-    //   delete dataObj.sSprintName;
-    //   delete dataObj.sprojectName;
-    //   delete dataObj.hoverValue;
-    //   delete dataObj.xName;
+          // const dataObj = JSON.parse(JSON.stringify(d.data));
+          // delete dataObj.group;
+          // // delete dataObj.sprojectName;
+          // delete dataObj.hoverText;
+          // delete dataObj.xName;
 
-    //   let dataString = '';
-    //   for (const hoverData in dataObj) {
-    //     dataString += `${hoverData}` + ' : ' + '<span class=\'toolTipValue\'> ' + `${dataObj[hoverData] + ' ' + (self.unit ? self.unit.toLowerCase() != 'number' ? self.unit : '' : '')} </span>`;
-    //   }
+          let dataString = '';
+          for (let key in d.data?.hoverText) {
+            dataString += `<div class=\'toolTipValue p-d-flex p-align-center\'><div class="stack-key p-mr-1">${key}</div><div>${d.data?.hoverText[key]}</div></div>`;
+          }
 
-    //   div.html(`${d?.data?.sSprintName}` + ' : ' + '<span class=\'toolTipValue\'> ' + `${dataString}` + '</span>')
-    //     .style('left', xPosition + 20 + 'px')
-    //     // .style('top', y(d[0]) - y(d[1]) - topValue + 'px');
-    //     .style('top', yPosition + 20 + 'px')
-    //     .style('position', 'fixed');
 
-    //   for (const hoverData in d.data.hoverValue) {
-    //     div.append('p').html(`${hoverData}` + ' : ' + '<span class=\'toolTipValue\'> ' + `${d.data.hoverValue[hoverData]}` + ' </span>');
-    //   }
-    // }
-    // })
-    // .on('mouseout', function (d) {
-    //   div.transition()
-    //     .duration(500)
-    //     .style('display', 'none')
-    //     .style('opacity', 0);
+          div.html(`${d?.data?.group}` + ' : ' + '<div class=\'toolTip\'> ' + `${dataString}` + '</div>')
+            .style('left', xPosition + 20 + 'px')
+            // .style('top', y(d[0]) - y(d[1]) - topValue + 'px');
+            .style('top', yPosition + 'px')
+            .style('position', 'fixed')
+            .style('align', 'left');
 
-    // });
+          // for (const hoverData in d.data.hoverValue) {
+          // div.append('p').html(`${hoverData}` + ' : ' + '<span class=\'toolTipValue\'> ' + `${d.data.hoverValue[hoverData]}` + ' </span>');
+          // }
+        }
+      })
+      .on('mouseout', function (d) {
+        div.transition()
+          .duration(500)
+          .style('display', 'none')
+          .style('opacity', 0);
+
+      });
     // if (this.kpiId != 'kpi125' && this.kpiId != 'kpi127') {
     //   const serie2 = svgX.selectAll('.serie2')
     //     .data(stackData)
@@ -365,98 +386,97 @@ export class GroupstackchartComponent implements OnChanges {
     // }
 
     /** legend code */
-    const legendDiv = d3.select(this.elem).select('#groupstackchart').append('div');
+    const legendDiv = d3.select(this.elem).select('#svgLegend').append('div');
 
-    const svgLegend = d3
-      .select(this.elem)
-      .select('#svgLegend')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', 50)
-      .append('g');
+    // const svgLegend = d3
+    //   .select(this.elem)
+    //   .select('#svgLegend')
+    //   .append('svg')
+    //   .attr('width', width)
+    //   .attr('height', 50)
+    //   .append('g');
 
-    const legend = svgLegend.selectAll('.d3-legend')
-      .data(actualTypes)
-      .enter()
-      .append('g')
-      .attr('class', 'd3-legend')
-      .attr('transform', function (d, i) {
-        return 'translate(40, 10)';
-      });
+    // const legend = svgLegend.selectAll('.d3-legend')
+    //   .data(actualTypes)
+    //   .enter()
+    //   .append('g')
+    //   .attr('class', 'd3-legend')
+    //   .attr('transform', function (d, i) {
+    //     return 'translate(40, 10)';
+    //   });
 
     if (this.legendType == 'normal') {
-      const topValue = 30;
       legendDiv.transition()
         .duration(200)
         .style('display', 'block')
         .style('opacity', 1)
-        .attr('class', 'p-d-flex p-flex-wrap normal-legend');;
+        .attr('id', 'd3-legend')
+        .attr('class', 'p-d-flex p-flex-wrap normal-legend');
 
       let htmlString = '';
-
-      stackData.forEach((key, i) => {
-        if (stackColorsList[i]) {
-          htmlString += `<div class="legend_item"><div class="legend_color_indicator" style="background-color: ${stackColorsList[i]}"></div> : ${key['key']}</div>`;
+      this.sortAlphabetically(stackData);
+      actualTypes.forEach((key, i) => {
+        if (z(key)) {
+          htmlString += `<div class="legend_item p-d-flex p-align-center"><div class="legend_color_indicator" style="background-color: ${z(key)}"></div> ${key}</div>`;
         }
       });
 
-      legendDiv.html(htmlString)
-        .style('left', 70 + 'px')
-        .style('top', y[0] - topValue + 'px');
+      legendDiv.html(htmlString);
 
-    } else {
-      legendDiv.attr('class', 'legend-tooltip')
-        .style('display', 'none')
-        .style('opacity', 0);
-
-      legend.append('rect')
-        .attr('width', 12)
-        .attr('height', 12)
-        .attr('x', 0)
-        .attr('y', -7)
-        .style('fill', function (d, i) {
-          return '#DF9292';
-        });
-
-      legend.append('text')
-        .attr('x', 18)
-        .attr('y', -6)
-        .attr('dy', '.85em')
-        .style('text-anchor', 'start')
-        .style('font-size', 10)
-        .text(function (d) {
-          return 'Legend';
-        });
-
-      legend
-        .on('mouseover', function () {
-          const topValue = 30;
-
-          legendDiv.transition()
-            .duration(200)
-            .style('display', 'block')
-            .style('opacity', 1);
-
-          let htmlString = '';
-
-          stackData.forEach((key, i) => {
-            if (stackColorsList[i]) {
-              htmlString += `<div class="legend_item"><div class="legend_color_indicator" style="background-color: ${stackColorsList[i]}"></div> : ${key['key']}</div>`;
-            }
-          });
-
-          legendDiv.html(htmlString)
-            .style('left', 70 + 'px')
-            .style('top', y[0] - topValue + 'px');
-        })
-        .on('mouseout', function () {
-          legendDiv.transition()
-            .duration(500)
-            .style('display', 'none')
-            .style('opacity', 0);
-
-        });
     }
+    // else {
+    //   legendDiv.attr('class', 'legend-tooltip')
+    //     .style('display', 'none')
+    //     .style('opacity', 0);
+
+    // legend.append('rect')
+    //   .attr('width', 12)
+    //   .attr('height', 12)
+    //   .attr('x', 0)
+    //   .attr('y', -7)
+    //   .style('fill', function (d, i) {
+    //     return '#DF9292';
+    //   });
+
+    // legend.append('text')
+    //   .attr('x', 18)
+    //   .attr('y', -6)
+    //   .attr('dy', '.85em')
+    //   .style('text-anchor', 'start')
+    //   .style('font-size', 10)
+    //   .text(function (d) {
+    //     return 'Legend';
+    //   });
+
+    // legend
+    //   .on('mouseover', function () {
+    //     const topValue = 30;
+
+    //     legendDiv.transition()
+    //       .duration(200)
+    //       .style('display', 'block')
+    //       .style('opacity', 1);
+
+    //     let htmlString = '';
+
+    //     stackData.forEach((key, i) => {
+    //       if (stackColorsList[i]) {
+    //         htmlString += `<div class="legend_item"><div class="legend_color_indicator" style="background-color: ${stackColorsList[i]}"></div> : ${key['key']}</div>`;
+    //       }
+    //     });
+
+    //     legendDiv.html(htmlString)
+    //       .style('left', 70 + 'px')
+    //       .style('top', y[0] - topValue + 'px');
+    //   })
+    //   .on('mouseout', function () {
+    //     legendDiv.transition()
+    //       .duration(500)
+    //       .style('display', 'none')
+    //       .style('opacity', 0);
+
+    //   });
+    // }
 
     // }
 
@@ -542,7 +562,7 @@ export class GroupstackchartComponent implements OnChanges {
       const targetList = [];
       this.data.forEach((item, index) => {
         // pro.value?.forEach((item, index) => {
-        
+
         // obj['sprojectName'] = pro.data;
         // if (item.hoverValue) {
         //   obj['hoverValue'] = item.hoverValue;
@@ -556,8 +576,15 @@ export class GroupstackchartComponent implements OnChanges {
             const obj = {};
             obj['group'] = item.sSprintName;
             obj['type'] = type;
-            obj['value'] = item.value[type]?.['issueCount'];
-            obj['hoverText'] = '(' + item.value[type]?.['issueCount'] + ', ' + item.value[type]?.['issueStoryPoint'] + ')';
+            obj['value'] = item.value[type][Object.keys(item.value[type])?.[0]];
+            obj['hoverText'] = {};
+            obj['hoverText'][type] = '(';
+            // obj['hoverText'] = type +': ' + '(';
+            let lastEle = Object.keys(item.value[type])?.length - 1;
+            Object.keys(item.value[type])?.forEach((x, i) => {
+              obj['hoverText'][type] += item.value[type][x] + (i != lastEle ? ', ' : '');
+            })
+            obj['hoverText'][type] += ')';
             obj['xName'] = sprintValue;
             targetList.push(obj);
             max = Math.max(max, item.data);
@@ -576,7 +603,7 @@ export class GroupstackchartComponent implements OnChanges {
         // targetList.push(obj);
         // currentSum = currentSum + item.value;
         // }
-        
+
 
         // if (item.xAxisTick) {
         //   this.xCaption = 'Months';
@@ -588,5 +615,36 @@ export class GroupstackchartComponent implements OnChanges {
     }
   }
 
+  sortAlphabetically(objArray) {
+    if (objArray && objArray?.length > 1) {
+      objArray?.sort((a, b) => a.key?.localeCompare(b.data));
+    }
+    return objArray;
+  }
+
+  wrap(text, wrapWidth, yAxisAdjustment = 0) {
+    text.each(function() {
+      var text = d3.select(this),
+          words = text.text().split(/[\s|]+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1, 
+          y = text.attr("y"),
+          dy = parseFloat(text.attr("dy")) - yAxisAdjustment,
+          tspan = text.text(null).append("tspan").attr("x", 30).attr("y", y - 2).attr("dy", `${dy}em`).attr("text-anchor", "end");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > wrapWidth) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", 30).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word).attr("text-anchor", "end");
+        }
+      }
+    });
+    return 0;
+  }
 
 }
