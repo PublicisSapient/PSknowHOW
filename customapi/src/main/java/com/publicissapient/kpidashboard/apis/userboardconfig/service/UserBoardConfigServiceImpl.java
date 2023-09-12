6e91ae1d7205b3ed21d6212193e01551bc0f8c87
aksshriv1 <akshat.shrivastav@publicissapient.com>
2023-09-12 13:47:29 +0530

    File Formating

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/userboardconfig/service/UserBoardConfigServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/userboardconfig/service/UserBoardConfigServiceImpl.java
@@ -162,7 +162,7 @@ public class UserBoardConfigServiceImpl implements UserBoardConfigService {
 	 * @param kpiCategoryList
 	 *            kpiCategoryList
 	 * @param kpiMasterMap
-	 * 	kpiMasterMap
+	 *            kpiMasterMap
 	 * @return kpiCategoryList
 	 */
 	private boolean checkCategories(UserBoardConfigDTO existingUserBoardConfigDTO, List<KpiCategory> kpiCategoryList,
