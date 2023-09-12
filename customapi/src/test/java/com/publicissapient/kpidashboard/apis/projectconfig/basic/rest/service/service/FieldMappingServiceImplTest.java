85982f64db7ff0546ea2096491cf120716380af0
pragauta3 <pramod.gautam@publicissapient.com>
2023-09-12 16:30:49 +0530

    DTS-274423 reformat code

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/projectconfig/basic/rest/service/service/FieldMappingServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/projectconfig/basic/rest/service/service/FieldMappingServiceImplTest.java
@@ -102,7 +102,6 @@ public class FieldMappingServiceImplTest {
 
 		Set<String> configIds = new HashSet<>();
 		configIds.add("5d0533b0ff45ea9c730bb718");
-		//configHelperService.getFieldMappingMap().get(new ObjectId(projectToolConfigId));
 		when(configHelperService.getFieldMappingMap()).thenReturn(map);
 		when(fieldMappingRepository.findByProjectToolConfigId(Mockito.any(ObjectId.class))).thenReturn(fieldMapping);
 		when(projectToolConfigRepository.findById("5d0533b0ff45ea9c730bb718")).thenReturn(projectToolConfig);
