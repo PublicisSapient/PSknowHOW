6c676a5afe1823d3aca965bbc5b493610d1aba7c
shivani <shivani1@publicissapient.com>
2023-09-20 18:37:53 +0530

    DTS-28866_CommitmentReliabilty

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/CommittmentReliabilityServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/CommittmentReliabilityServiceImpl.java
@@ -129,7 +129,7 @@ public class CommittmentReliabilityServiceImpl extends JiraKPIService<Long, List
 		Map<String, List<DataCount>> trendValuesMap = getTrendValuesMap(kpiRequest, nodeWiseKPIValue,
 				KPICode.COMMITMENT_RELIABILITY);
 		Map<String, List<DataCount>> unsortedMap = trendValuesMap.entrySet().stream()
-				.sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
+				.sorted(Map.Entry.comparingByKey())
 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
 		Map<String, Map<String, List<DataCount>>> statusTypeProjectWiseDc = new LinkedHashMap<>();
 		unsortedMap.forEach((statusType, dataCounts) -> {
