a84cdc16ae354989d14db8e22c16c5a2d27dafd3
Shubh Narayan <shubh.narayan@publicissapient.com>
2023-10-13 15:42:57 +0530

    DTS-29139:bug fix date parse

--- common/src/main/java/com/publicissapient/kpidashboard/common/util/DateUtil.java
+++ common/src/main/java/com/publicissapient/kpidashboard/common/util/DateUtil.java
@@ -56,6 +56,7 @@ public class DateUtil {
 	public static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
 	public static final String TIME_FORMAT_WITH_SEC = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
+	public static final String ISO_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
 	public static final String TIME_FORMAT_WITH_SEC_ZONE = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
