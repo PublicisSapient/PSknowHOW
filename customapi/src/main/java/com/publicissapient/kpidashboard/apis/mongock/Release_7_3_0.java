f6341e83a3f79a2bf7905c8b8e854895ab4d9312
hargupta15 <harsh.gupta@publicissapient.com>
2023-10-17 20:58:24 +0530

    7.3.0 release

---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/Release_7_3_0.java
@@ -0,0 +1,88 @@
+package com.publicissapient.kpidashboard.apis.mongock;
+
+import com.publicissapient.kpidashboard.apis.util.MongockUtils;
+import io.mongock.api.annotations.BeforeExecution;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@ChangeUnit(id = "Release_7.3.0", order = "002", author = "hargupta15", runAlways = true, systemVersion = "8.0.0")
+public class Release_7_3_0 {
+
+	private final MongoTemplate mongoTemplate;
+	List<String> kpiIdsToCheck = Arrays.asList("kpi151", "kpi152");
+
+	public Release_7_3_0(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@BeforeExecution
+	private void setUp() {
+		MongockUtils.checkCollectionExists(mongoTemplate, MongockUtils.KPI_COLUMN_CONFIG_COLLECTION);
+		MongockUtils.checkCollectionExists(mongoTemplate, MongockUtils.KPI_MASTER_COLLECTION);
+	}
+
+	@Execution
+	public void changeSet() {
+
+		List<Document> kpiData = getKpiData(kpiIdsToCheck);
+		List<Document> kpiColumnData = getKpiColumnData(kpiIdsToCheck);
+
+		if (kpiColumnData.isEmpty()) {
+			List<Document> kpiColumnConfigs = Arrays.asList(MongockUtils.createKpiColumnConfig("kpi151"),
+					MongockUtils.createKpiColumnConfig("kpi152"));
+			mongoTemplate.insert(kpiColumnConfigs, MongockUtils.KPI_COLUMN_CONFIG_COLLECTION);
+		} else {
+			System.out.println("KPI Column Config data is already present");
+		}
+
+		if (kpiData.isEmpty()) {
+			List<Document> kpiDocuments = new ArrayList<>();
+			kpiDocuments.add(MongockUtils.createKpiDocument("kpi151", "Backlog Count By Status", "Count", 9, "Backlog",
+					"Jira", 10, "Total count of issues in the Backlog with a breakup by Status.", true, false,
+					"dropdown", "chart", false));
+			kpiDocuments.add(MongockUtils.createKpiDocument("kpi152", "Backlog Count By Issue Type", "Count", 10,
+					"Backlog", "Jira", 10, "Total count of issues in the backlog with a breakup by issue type.", true,
+					false, "dropdown", "chart", false));
+
+			mongoTemplate.insert(kpiDocuments, "kpi_master");
+		} else {
+			System.out.println("KPI are already present in Kpi master");
+		}
+
+	}
+
+	@RollbackExecution
+	public void rollback() {
+		rollbackKpiData(kpiIdsToCheck);
+		rollbackKpiColumnData(kpiIdsToCheck);
+	}
+
+	private List<Document> getKpiData(List<String> kpiIdsToCheck) {
+		Query query = Query.query(Criteria.where(MongockUtils.kpiId).in(kpiIdsToCheck));
+		return mongoTemplate.find(query, Document.class, MongockUtils.KPI_MASTER_COLLECTION);
+	}
+
+	private List<Document> getKpiColumnData(List<String> kpiIdsToCheck) {
+		Query query = Query.query(Criteria.where(MongockUtils.kpiId).in(kpiIdsToCheck));
+		return mongoTemplate.find(query, Document.class, MongockUtils.KPI_COLUMN_CONFIG_COLLECTION);
+	}
+
+	private void rollbackKpiData(List<String> kpiIdsToCheck) {
+		Criteria criteria = Criteria.where(MongockUtils.kpiId).in(kpiIdsToCheck);
+		mongoTemplate.remove(Query.query(criteria), Document.class, MongockUtils.KPI_MASTER_COLLECTION);
+	}
+
+	private void rollbackKpiColumnData(List<String> kpiIdsToCheck) {
+		Criteria criteria = Criteria.where(MongockUtils.kpiId).in(kpiIdsToCheck);
+		mongoTemplate.remove(Query.query(criteria), Document.class, MongockUtils.KPI_COLUMN_CONFIG_COLLECTION);
+	}
+}
---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/util/MongockUtils.java
@@ -0,0 +1,48 @@
+package com.publicissapient.kpidashboard.apis.util;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+public final class MongockUtils {
+
+	public static final String ROLES_COLLECTION = "roles";
+	public static final String KPI_MASTER_COLLECTION = "kpi_master";
+	public static final String KPI_COLUMN_CONFIG_COLLECTION = "kpi_column_configs";
+	public static final String kpiId = "kpiId";
+
+	public static void checkCollectionExists(MongoTemplate mongoTemplate, String collectionName) {
+		if (!mongoTemplate.collectionExists(collectionName))
+			mongoTemplate.createCollection(collectionName);
+	}
+
+	public static Document createKpiColumnConfig(String kpiId) {
+		return new Document("basicProjectConfigId", null).append("kpiId", kpiId).append("kpiColumnDetails",
+				Arrays.asList(createColumnDetail("Issue ID", 0), createColumnDetail("Issue Description", 1),
+						createColumnDetail("Issue Type", 2), createColumnDetail("Issue Status", 3),
+						createColumnDetail("Priority", 4), createColumnDetail("Created Date", 5),
+						createColumnDetail("Updated Date", 6), createColumnDetail("Assignee", 7)));
+	}
+
+	private static Document createColumnDetail(String columnName, int order) {
+		return new Document("columnName", columnName).append("order", order).append("isShown", true).append("isDefault",
+				true);
+	}
+
+	public static Document createKpiDocument(String kpiId, String kpiName, String kpiUnit, int defaultOrder,
+			String kpiCategory, String kpiSource, int groupId, String definition, boolean isPositiveTrend,
+			boolean showTrend, String kpiFilter, String boxType, boolean calculateMaturity) {
+		Document kpiDocument = new Document();
+		kpiDocument.append("kpiId", kpiId).append("kpiName", kpiName).append("kpiUnit", kpiUnit)
+				.append("isDeleted", "False").append("defaultOrder", defaultOrder).append("kpiCategory", kpiCategory)
+				.append("kpiSource", kpiSource).append("groupId", groupId).append("thresholdValue", "")
+				.append("kanban", false).append("chartType", "pieChart")
+				.append("kpiInfo", new Document("definition", definition)).append("xAxisLabel", "")
+				.append("yAxisLabel", "").append("isPositiveTrend", isPositiveTrend).append("showTrend", showTrend)
+				.append("isAdditionalFilterSupport", false).append("kpiFilter", kpiFilter).append("boxType", boxType)
+				.append("calculateMaturity", calculateMaturity);
+
+		return kpiDocument;
+	}
+}
