ae0f3d3117ca5185af33bb92bcddfb54e163eeb5
hargupta15 <harsh.gupta@publicissapient.com>
2023-10-18 16:30:31 +0530

    script 1,2,3

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/DDL/AuthenticationAndAuthorization.java
+++
@@ -1,9 +0,0 @@
-package com.publicissapient.kpidashboard.apis.mongock.DDL;
-
-import io.mongock.api.annotations.ChangeUnit;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@ChangeUnit(id = "DDL", order = "001", author = "hargupta15", runAlways = true)
-public class AuthenticationAndAuthorization {
-}
---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/ddl/CreateDatabaseIndexes.java
@@ -0,0 +1,10 @@
+package com.publicissapient.kpidashboard.apis.mongock.ddl;
+
+import io.mongock.api.annotations.ChangeUnit;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ChangeUnit(id = "DDL", order = "002", author = "hargupta15", runAlways = true)
+public class CreateDatabaseIndexes {
+
+}
