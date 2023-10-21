34040e212507de475cd72af9ba76a9706f5576f3
Rishabh Shukla <risshukl0@publicisgroupe.net>
2023-10-21 23:30:35 +0530

    Very minor spacing fix

--- UI/src/app/dashboard/daily-scrum-graph/daily-scrum-graph.component.ts
+++ UI/src/app/dashboard/daily-scrum-graph/daily-scrum-graph.component.ts
@@ -308,7 +308,7 @@ export class DailyScrumGraphComponent implements OnChanges, OnDestroy {
             }
           }
         })
-        .attr('y', (d, i) => issueList.length <= 1 ? swimLaneHeight / 2 + 20 : (y(i + 1) - y(i) - 1) / 2 + 20)
+        .attr('y', (d, i) => issueList.length <= 1 ? swimLaneHeight / 2 + 20 : (y(i + 1) - y(i) - 1) / 2 + 25)
         .style('cursor', 'pointer')
         .text(function (d, i) {
           currentIssue = (JSON.parse(d3.select(this.parentNode.parentNode).attr('parent-data')));
