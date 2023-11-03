533b6ac5d72f5946a34f6e4a51187c4b1b64b497
Shubh Narayan <shubh.narayan@publicissapient.com>
2023-11-03 13:54:21 +0530

    DTS-27274:Mongock upgrade & installation script

---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/Release810Mongock.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.publicissapient.kpidashboard.apis.mongock.upgrade;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+
+@SuppressWarnings("java:S1192")
+@ChangeUnit(id = "dml8.1.0", order = "1000", author = "PSKnowHOW", systemVersion = "8.1.0")
+public class Release810Mongock {
+
+	private final MongoTemplate mongoTemplate;
+
+	public Release810Mongock(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@Execution
+	public void execution() {
+		releaseBurnUpDefaultOrderUpdate();
+		releaseBurnUpFieldMappingInsert();
+	}
+
+	public void releaseBurnUpDefaultOrderUpdate() {
+		Bson filter = Filters.in("kpiId", "kpi150");
+		Bson update = Updates.set("defaultOrder", 1);
+		mongoTemplate.getCollection("kpi_master").updateMany(filter, update);
+	}
+
+	public void releaseBurnUpFieldMappingInsert() {
+
+		Document document = new Document("fieldName", "startDateCountKPI150")
+				.append("fieldLabel",
+						"Count of days from the release start date to calculate closure rate for prediction")
+				.append("fieldType", "number").append("section", "Issue Types Mapping")
+				.append("tooltip", new Document("definition",
+						"If this field is kept blank, then daily closure rate of issues is calculated based on the number of working days between today and the release start date or date when the first issue was added. This configuration allows you to decide from which date the closure rate should be calculated."));
+
+		mongoTemplate.insert(document, "field_mapping_structure");
+
+	}
+
+	@RollbackExecution
+	public void rollback() {
+		releaseBurnUpDefaultOrderRollback();
+		releaseBurnUpFieldMappingRollback();
+	}
+
+	public void releaseBurnUpDefaultOrderRollback() {
+
+		Bson filter = Filters.in("kpiId", "kpi150");
+		Bson rollbackUpdate = Updates.set("defaultOrder", 6);
+		mongoTemplate.getCollection("kpi_master").updateMany(filter, rollbackUpdate);
+
+	}
+
+	public void releaseBurnUpFieldMappingRollback() {
+
+		// Define a filter to remove the inserted document
+		Bson filter = Filters.eq("fieldName", "startDateCountKPI150");
+		// Remove the document from the collection
+		mongoTemplate.getCollection("field_mapping_structure").deleteOne(filter);
+
+	}
+
+}
