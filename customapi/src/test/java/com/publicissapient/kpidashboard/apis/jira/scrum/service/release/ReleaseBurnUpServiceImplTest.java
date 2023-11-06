9f214f886275f8818857d5b7a0368c153c6cc0b0
Shubh Narayan <shubh.narayan@publicissapient.com>
2023-11-06 14:45:47 +0530

    DTS-27274:release progress coverage increase

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/release/ReleaseBurnUpServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/release/ReleaseBurnUpServiceImplTest.java
@@ -107,11 +107,7 @@ public class ReleaseBurnUpServiceImplTest {
 		FieldMapping fieldMapping = fieldMappingDataFactory.getFieldMappings().get(0);
 		JiraIssueCustomHistory history = jiraIssuesCustomHistory.stream().findFirst()
 				.orElse(new JiraIssueCustomHistory());
-		jiraIssuesCustomHistory = jiraIssuesCustomHistory.stream()
-				.filter(j -> j.getBasicProjectConfigId().equalsIgnoreCase(history.getBasicProjectConfigId()))
-				.collect(Collectors.toList());
 		fieldMappingMap.put(fieldMapping.getBasicProjectConfigId(), fieldMapping);
-		fieldMappingMap.put(new ObjectId(history.getBasicProjectConfigId()), fieldMapping);
 		configHelperService.setFieldMappingMap(fieldMappingMap);
 	}
 
