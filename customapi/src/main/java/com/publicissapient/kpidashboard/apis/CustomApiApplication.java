01a36746f7271369b976a4b32d78e3c0ec15558d
Chittauri <98313533+Chittauri@users.noreply.github.com>
2023-11-07 18:06:32 +0530

    Update CustomApiApplication.java
    Signed-off-by: Chittauri <98313533+Chittauri@users.noreply.github.com>

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/CustomApiApplication.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/CustomApiApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014 CapitalOne, LLC.
+ * Copyright 2014 CapitalOne, LLC .
  * Further development Copyright 2022 Sapient Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
