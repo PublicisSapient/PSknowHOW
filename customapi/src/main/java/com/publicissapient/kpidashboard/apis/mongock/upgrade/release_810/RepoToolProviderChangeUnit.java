e7a2c49f4fd736415a9534785cde34c2ebdc799a
kunkambl <kunkambl@publicisgroupe.net>
2023-11-08 19:24:42 +0530

    changed error msg

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_810/RepoToolProviderChangeUnit.java
+++
@@ -1,7 +0,0 @@
-package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_810;
-
-import io.mongock.api.annotations.ChangeUnit;
-
-@ChangeUnit(id = "scope_churn_filters", order = "8106", author = "eswbogol", systemVersion = "8.1.0")
-public class RepoToolProviderChangeUnit {
-}
