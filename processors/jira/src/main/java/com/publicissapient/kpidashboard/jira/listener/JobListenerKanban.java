69165a90abc6e8486864e9e03f3d0ced7a8c7319
purushottam08 <purushottam.gupta@publicissapient.com>
2023-11-16 17:44:11 +0530

    notification_format_fix

--- processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/listener/JobListenerKanban.java
+++ processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/listener/JobListenerKanban.java
@@ -27,11 +27,11 @@ import com.publicissapient.kpidashboard.common.repository.tracelog.ProcessorExec
 import com.publicissapient.kpidashboard.jira.cache.JiraProcessorCacheEvictor;
 import com.publicissapient.kpidashboard.jira.config.JiraProcessorConfig;
 import com.publicissapient.kpidashboard.jira.constant.JiraConstants;
+import com.publicissapient.kpidashboard.jira.service.JiraCommonService;
 import com.publicissapient.kpidashboard.jira.service.NotificationHandler;
 import com.publicissapient.kpidashboard.jira.service.OngoingExecutionsService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.bson.types.ObjectId;
 import org.springframework.batch.core.BatchStatus;
@@ -43,12 +43,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
+
+import static com.publicissapient.kpidashboard.jira.helper.JiraHelper.convertDateToCustomFormat;
 
 /**
  * @author purgupta2
@@ -82,17 +81,11 @@ public class JobListenerKanban extends JobExecutionListenerSupport {
     private ProjectBasicConfigRepository projectBasicConfigRepo;
 
     @Autowired
+    private JiraCommonService jiraCommonService;
+
+    @Autowired
     public JobListenerKanban(@Value("#{jobParameters['projectId']}") String projectId) {
         this.projectId = projectId;
-    }
-
-    public static String convertDateToCustomFormat(long currentTimeMillis) {
-        Date inputDate = new Date(currentTimeMillis);
-        SimpleDateFormat outputFormat = new SimpleDateFormat("MMMM dd, yyyy, EEEE, hh:mm:ss a");
-
-        String outputStr = outputFormat.format(inputDate);
-
-        return outputStr;
     }
 
     @Override
@@ -113,50 +106,34 @@ public class JobListenerKanban extends JobExecutionListenerSupport {
         jiraProcessorCacheEvictor.evictCache(CommonConstant.CACHE_CLEAR_ENDPOINT,
                 CommonConstant.CACHE_ACCOUNT_HIERARCHY_KANBAN);
         jiraProcessorCacheEvictor.evictCache(CommonConstant.CACHE_CLEAR_ENDPOINT, CommonConstant.JIRAKANBAN_KPI_CACHE);
-        // sending notification in case of job failure
-        if (jobExecution.getStatus() == BatchStatus.FAILED) {
-            log.error("job failed : {} for the project : {}", jobExecution.getJobInstance().getJobName(), projectId);
-            Throwable stepFaliureException = null;
-            for (StepExecution stepExecution : jobExecution.getStepExecutions()) {
-                if (stepExecution.getStatus() == BatchStatus.FAILED) {
-                    stepFaliureException = stepExecution.getFailureExceptions().get(0);
-                    break;
+        try {
+            // sending notification in case of job failure
+            if (jobExecution.getStatus() == BatchStatus.FAILED) {
+                log.error("job failed : {} for the project : {}", jobExecution.getJobInstance().getJobName(), projectId);
+                Throwable stepFaliureException = null;
+                for (StepExecution stepExecution : jobExecution.getStepExecutions()) {
+                    if (stepExecution.getStatus() == BatchStatus.FAILED) {
+                        stepFaliureException = stepExecution.getFailureExceptions().get(0);
+                        break;
+                    }
                 }
-            }
-            try {
                 sendNotification(stepFaliureException);
-            } catch (UnknownHostException e) {
-                log.error("AccessRequestController: Server Host name is not bind with Access Request mail ");
+                setExecutionSuccessFalse();
             }
-            setExecutionSuccessFalse();
+        } catch (Exception e) {
+            log.error("An Exception has occured in kanban jobListener", e);
+        } finally {
+            log.info("removing project with basicProjectConfigId {}", projectId);
+            // Mark the execution as completed
+            ongoingExecutionsService.markExecutionAsCompleted(projectId);
         }
-
-        log.info("removing project with basicProjectConfigId {}", projectId);
-        // Mark the execution as completed
-        ongoingExecutionsService.markExecutionAsCompleted(projectId);
-    }
-
-    /**
-     * Gets api host
-     **/
-    public String getApiHost() throws UnknownHostException {
-
-        StringBuilder urlPath = new StringBuilder();
-        if (StringUtils.isNotEmpty(jiraProcessorConfig.getUiHost())) {
-            urlPath.append("https").append(':').append(File.separator + File.separator)
-                    .append(jiraProcessorConfig.getUiHost().trim());
-        } else {
-            throw new UnknownHostException("Api host not found in properties.");
-        }
-
-        return urlPath.toString();
     }
 
     private void sendNotification(Throwable stepFaliureException) throws UnknownHostException {
         FieldMapping fieldMapping = fieldMappingRepository.findByBasicProjectConfigId(new ObjectId(projectId));
         ProjectBasicConfig projectBasicConfig = projectBasicConfigRepo.findById(new ObjectId(projectId)).orElse(null);
-        if (fieldMapping.getNotificationEnabler() && projectBasicConfig != null) {
-            handler.sendEmailToProjectAdmin(convertDateToCustomFormat(System.currentTimeMillis()) + " on " + getApiHost() + " for \"" + projectBasicConfig.getProjectName() + "\"", ExceptionUtils.getStackTrace(stepFaliureException), projectId);
+        if (fieldMapping==null || (fieldMapping.getNotificationEnabler() && projectBasicConfig != null)) {
+            handler.sendEmailToProjectAdmin(convertDateToCustomFormat(System.currentTimeMillis()) + " on " + jiraCommonService.getApiHost() + " for \"" + projectBasicConfig.getProjectName() + "\"", ExceptionUtils.getStackTrace(stepFaliureException), projectId);
         } else {
             log.info("Notification Switch is Off for the project : {}. So No mail is sent to project admin", projectId);
         }
