8978fd5fffe4e96b57dfd3371d9c26e65c01fb59
purushottam08 <purushottam.gupta@publicissapient.com>
2023-11-16 12:57:37 +0530

    bearer_fix

--- processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/service/JiraCommonService.java
+++ processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/service/JiraCommonService.java
@@ -131,7 +131,12 @@ public class JiraCommonService {
                 password = decryptJiraPassword(connectionOptional.map(Connection::getPassword).orElse(null));
             }
         }
-        request.setRequestProperty("Authorization", "Basic " + encodeCredentialsToBase64(username, password)); // NOSONAR
+        if (connectionOptional.isPresent() && connectionOptional.get().isBearerToken()) {
+            String patOAuthToken = decryptJiraPassword(connectionOptional.get().getPatOAuthToken());
+            request.setRequestProperty("Authorization", "Bearer " + patOAuthToken); // NOSONAR
+        } else {
+            request.setRequestProperty("Authorization", "Basic " + encodeCredentialsToBase64(username, password)); // NOSONAR
+        }
         request.connect();
         StringBuilder sb = new StringBuilder();
         try (InputStream in = (InputStream) request.getContent();
