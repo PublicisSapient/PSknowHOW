3089dd1922e860dad9661d1f3c7e0c80cbff9ced
kunkambl <kunkambl@publicisgroupe.net>
2023-11-27 12:42:54 +0530

    added gitlab provider

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_810/RepoToolProviderUrlChange.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_810/RepoToolProviderUrlChange.java
@@ -32,40 +32,40 @@ import java.util.Arrays;
 @ChangeUnit(id = "r_repo_tool_provider_url_change", order = "08110", author = "kunkambl", systemVersion = "8.1.0")
 public class RepoToolProviderUrlChange {
 
-    private final MongoTemplate mongoTemplate;
+	private final MongoTemplate mongoTemplate;
 
-    public RepoToolProviderUrlChange(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
-    }
+	public RepoToolProviderUrlChange(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
 
-    @Execution
-    public void execution() {
-        changeRepoToolProviderTestApiUrlsRollback();
-        changePRSizeMaturityRollback();
-        updateThresholdsRollback();
-        updateKpi162Rollback();
-    }
+	@Execution
+	public void execution() {
+		changeRepoToolProviderTestApiUrlsRollback();
+		changePRSizeMaturityRollback();
+		updateThresholdsRollback();
+		updateKpi162Rollback();
+	}
 
-    public void changeRepoToolProviderTestApiUrlsRollback() {
-        Document filter_bb = new Document("toolName", "bitbucket");
-        Document update_bb = new Document("$set", new Document()
-                .append("testServerApiUrl", "https://api.bitbucket.org/2.0/repositories/")
-                .append("testApiUrl", ""));
+	public void changeRepoToolProviderTestApiUrlsRollback() {
+		Document filter_bb = new Document("toolName", "bitbucket");
+		Document update_bb = new Document("$set", new Document()
+				.append("testServerApiUrl", "https://api.bitbucket.org/2.0/repositories/").append("testApiUrl", ""));
 
-        mongoTemplate.getCollection("repo_tools_provider").updateOne(filter_bb, update_bb);
+		mongoTemplate.getCollection("repo_tools_provider").updateOne(filter_bb, update_bb);
 
-        Document filter = new Document("toolName", "gitlab");
-        Document update = new Document("$set", new Document("testApiUrl", "https://gitlab.com/api/v4/projects/"));
+		Document filter = new Document("toolName", "gitlab");
+		Document update = new Document("$set", new Document()
+				.append("testApiUrl", "https://gitlab.com/api/v4/projects/").append("repoToolProvider", ""));
 
-        mongoTemplate.getCollection("repo_tools_provider").updateOne(filter, update);
-    }
+		mongoTemplate.getCollection("repo_tools_provider").updateOne(filter, update);
+	}
 
-    public void changePRSizeMaturityRollback() {
+	public void changePRSizeMaturityRollback() {
 
-        mongoTemplate.getCollection("kpi_master").updateOne(new Document("kpiId", "kpi162"),
-                new Document("$set", new Document("calculateMaturity", true)));
+		mongoTemplate.getCollection("kpi_master").updateOne(new Document("kpiId", "kpi162"),
+				new Document("$set", new Document("calculateMaturity", true)));
 
-    }
+	}
 
 	public void updateThresholdsRollback() {
 		mongoTemplate.getCollection("kpi_master").updateMany(
@@ -74,53 +74,54 @@ public class RepoToolProviderUrlChange {
 				new Document("$set", new Document("upperThresholdBG", "").append("lowerThresholdBG", "")));
 	}
 
-    public void updateKpi162Rollback() {
-        Document filter = new Document("kpiId", "kpi162");
-        Document update = new Document("$set", new Document("calculateMaturity", true).append("showTrend", true));
-        mongoTemplate.getCollection("kpi_master").updateOne(filter, update);
-    }
+	public void updateKpi162Rollback() {
+		Document filter = new Document("kpiId", "kpi162");
+		Document update = new Document("$set", new Document("calculateMaturity", true).append("showTrend", true));
+		mongoTemplate.getCollection("kpi_master").updateOne(filter, update);
+	}
 
-    @RollbackExecution
-    public void rollback() {
-        changeRepoToolProviderTestApiUrls();
-        changePRSizeMaturity();
-        updateThresholds();
-        updateKpi162();
-    }
+	@RollbackExecution
+	public void rollback() {
+		changeRepoToolProviderTestApiUrls();
+		changePRSizeMaturity();
+		updateThresholds();
+		updateKpi162();
+	}
 
-    public void changeRepoToolProviderTestApiUrls() {
-        // Update for bitbucket tool
-        Document filter_bb = new Document("toolName", "bitbucket");
-        Document update_bb = new Document("$set",
-                new Document().append("testServerApiUrl", "/bitbucket/rest/api/1.0/projects/").append("testApiUrl",
-                        "https://api.bitbucket.org/2.0/workspaces/"));
+	public void changeRepoToolProviderTestApiUrls() {
+		// Update for bitbucket tool
+		Document filter_bb = new Document("toolName", "bitbucket");
+		Document update_bb = new Document("$set",
+				new Document().append("testServerApiUrl", "/bitbucket/rest/api/1.0/projects/").append("testApiUrl",
+						"https://api.bitbucket.org/2.0/workspaces/"));
 
-        mongoTemplate.getCollection("repo_tools_provider").updateOne(filter_bb, update_bb);
-        // Update for gitlab tool
-        Document filter_gl = new Document("toolName", "gitlab");
-        Document update_gl = new Document("$set", new Document().append("testApiUrl", "/api/v4/projects/"));
+		mongoTemplate.getCollection("repo_tools_provider").updateOne(filter_bb, update_bb);
+		// Update for gitlab tool
+		Document filter_gl = new Document("toolName", "gitlab");
+		Document update_gl = new Document("$set",
+				new Document().append("testApiUrl", "/api/v4/projects/").append("repoToolProvider", "gitlab"));
 
-        mongoTemplate.getCollection("repo_tools_provider").updateOne(filter_gl, update_gl);
-    }
+		mongoTemplate.getCollection("repo_tools_provider").updateOne(filter_gl, update_gl);
+	}
 
-    public void changePRSizeMaturity() {
+	public void changePRSizeMaturity() {
 
-        mongoTemplate.getCollection("kpi_master").updateOne(new Document("kpiId", "kpi162"),
-                new Document("$set", new Document("calculateMaturity", false)));
+		mongoTemplate.getCollection("kpi_master").updateOne(new Document("kpiId", "kpi162"),
+				new Document("$set", new Document("calculateMaturity", false)));
 
-    }
+	}
 
-    public void updateThresholds() {
-        mongoTemplate.getCollection("kpi_master").updateMany(
-                new Document("kpiId", new Document("$in", Arrays.asList("kpi160", "kpi158"))),
+	public void updateThresholds() {
+		mongoTemplate.getCollection("kpi_master").updateMany(
+				new Document("kpiId", new Document("$in", Arrays.asList("kpi160", "kpi158"))),
 
-                new Document("$set", new Document("upperThresholdBG", "red").append("lowerThresholdBG", "white")));
-    }
+				new Document("$set", new Document("upperThresholdBG", "red").append("lowerThresholdBG", "white")));
+	}
 
-    public void updateKpi162() {
-        Document filter = new Document("kpiId", "kpi162");
-        Document update = new Document("$set", new Document("calculateMaturity", false).append("showTrend", false));
+	public void updateKpi162() {
+		Document filter = new Document("kpiId", "kpi162");
+		Document update = new Document("$set", new Document("calculateMaturity", false).append("showTrend", false));
 
-        mongoTemplate.getCollection("kpi_master").updateOne(filter, update);
-    }
+		mongoTemplate.getCollection("kpi_master").updateOne(filter, update);
+	}
 }
