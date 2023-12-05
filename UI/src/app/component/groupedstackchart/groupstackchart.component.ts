d442d4eba536e88768aa9d420367eebc57d10d99
brahmanand1 <44412993+brahmanand1@users.noreply.github.com>
2023-12-05 15:02:38 +0530

    aggragation issue fixed

--- UI/src/app/component/groupedstackchart/groupstackchart.component.ts
+++ UI/src/app/component/groupedstackchart/groupstackchart.component.ts
@@ -47,6 +47,7 @@ export class GroupstackchartComponent implements OnChanges {
   dataPoints = 2;
   dataLength = 0;
   @Input() activeTab?: number = 0;
+  @Input() isAggregationStacks // to determine wheather need to aggrigate stacks 
   elemObserver = new ResizeObserver(() => {this.draw()});
   constructor(private viewContainerRef: ViewContainerRef, private service: SharedService) { }
 
@@ -176,14 +177,14 @@ export class GroupstackchartComponent implements OnChanges {
       actualTypes.reverse();
       z.domain(actualTypes);
       const keys = z.domain();
-      let groupData = d3.rollup(data, function (d, i) {
+      let groupData = d3.rollup(data, (d, i) => {
         const d2 = { xName: d[0].xName, group: d[0].group };
         d2['hoverSum'] = 0
         d2['hoverText'] = {};
         d.forEach((dx) => {
           d2[dx.type] = dx.value;
           for (let key in dx?.hoverText) {
-            d2['hoverSum'] += dx?.hoverText[key];
+            d2['hoverSum'] += (this.isAggregationStacks == false) ? (dx?.value) : (dx?.hoverText[key]);
             d2['hoverText'][key] = dx?.hoverText[key];
           }
         });
@@ -372,11 +373,11 @@ export class GroupstackchartComponent implements OnChanges {
         if (typeof (item?.value) === 'object' && Object.keys(item?.value)?.length > 0) {
           const types = Object.keys(item.value);
           // if (types.length >= 1) {
-          types?.forEach(function (type) {
+          types?.forEach(type => {
             const obj = {};
             obj['group'] = item?.sSprintName;
             obj['type'] = type;
-            obj['value'] = item?.value[type];
+            obj['value'] = (this.isAggregationStacks == false) ? (item?.data) : (item?.value[type]);
             obj['hoverText'] = {};
             obj['hoverText'][type] = item?.value[type];
             obj['xName'] = sprintValue;
