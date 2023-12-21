23d44992cb3c8d96573bd3463f85049780c73a17
rapkalya <raparthi.kalyan@publicissapient.com>
2023-12-21 12:50:41 +0530

    reverted back pom version

--- UI/src/environments/environment.docker.ts
+++ UI/src/environments/environment.docker.ts
@@ -20,7 +20,7 @@ export const environment = {
   production: true,
     baseUrl: '',
     SSO_LOGIN: false,
-   CENTRAL_LOGIN_URL: 'https://uat-authnauth.tools.publicis.sapient.com',
+   CENTRAL_LOGIN_URL: '',
    RESOURCE: 'PSKnowHOW',
    AUTHENTICATION_SERVICE: true
 };
