0e028fd019d60e1cf840c14359227e514db7c373
aksshriv1 <akshat.shrivastav@publicissapient.com>
2024-01-05 00:28:47 +0530

    Coverage improve sonar utils

--- processors/sonar/src/test/java/com/publicissapient/kpidashboard/sonar/util/SonarProcessorUtilsTest.java
+++ processors/sonar/src/test/java/com/publicissapient/kpidashboard/sonar/util/SonarProcessorUtilsTest.java
@@ -18,13 +18,18 @@
 
 package com.publicissapient.kpidashboard.sonar.util;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.powermock.api.mockito.PowerMockito.doReturn;
 
-import org.junit.Assert;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -40,6 +45,10 @@ import com.publicissapient.kpidashboard.sonar.data.ProjectToolConnectionFactory;
 @ExtendWith(SpringExtension.class)
 public class SonarProcessorUtilsTest {
 
+	private static final String FORMATTED_DATE = "yyyy-MM-dd'T'HH:mm:ssZ";
+	private static final String AUTHORIZATION = "Authorization";
+	@InjectMocks
+	SonarProcessorUtils sonarProcessorUtils;
 	@Mock
 	private ToolCredentialProvider toolCredentialProvider;
 
@@ -47,14 +56,14 @@ public class SonarProcessorUtilsTest {
 	public void getHeaders_BasicAuthTrue() {
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.add("Authorization", "Basic YWJjOg==");
-		Assert.assertEquals(httpHeaders, SonarProcessorUtils.getHeaders("abc", true));
+		assertEquals(httpHeaders, SonarProcessorUtils.getHeaders("abc", true));
 	}
 
 	@Test
 	public void getHeaders_BasicAuthFalse() {
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.add("Authorization", "Bearer abc");
-		Assert.assertEquals(httpHeaders, SonarProcessorUtils.getHeaders("abc", false));
+		assertEquals(httpHeaders, SonarProcessorUtils.getHeaders("abc", false));
 	}
 
 	@Test
@@ -74,4 +83,70 @@ public class SonarProcessorUtilsTest {
 		ProcessorToolConnection processorToolConnection = toolConnectionFactory.getProcessorToolConnectionList().get(2);
 		SonarUtils.getToolCredentials(toolCredentialProvider, processorToolConnection);
 	}
+
+	@Test
+	void getTimestamp() {
+		// Creating a mock JSON object
+		JSONObject jsonData = new JSONObject();
+		Date currentDate = new Date();
+		jsonData.put("timestamp", new SimpleDateFormat(FORMATTED_DATE, Locale.ROOT).format(currentDate));
+
+		// Testing the method
+		long timestamp = sonarProcessorUtils.getTimestamp(jsonData, "timestamp");
+
+	}
+
+	@Test
+	void convertToString() {
+		// Creating a mock JSON object
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("key", "value");
+
+		// Testing the method
+		String result = sonarProcessorUtils.convertToString(jsonData, "key");
+
+		// Asserting the result
+		assertEquals("value", result);
+	}
+
+	@Test
+	void convertToStringSafe() {
+		// Creating a mock JSON object
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("key", "value");
+
+		// Testing the method
+		String result = sonarProcessorUtils.convertToStringSafe(jsonData, "key");
+
+		// Asserting the result
+		assertEquals("value", result);
+	}
+
+	@Test
+	void getHeaders() {
+		// Testing the method
+		HttpHeaders headers = sonarProcessorUtils.getHeaders("username", "password");
+
+		// Asserting the result
+		assertEquals("Basic dXNlcm5hbWU6cGFzc3dvcmQ=", headers.getFirst(AUTHORIZATION));
+	}
+
+	@Test
+	void getHeadersWithAccessToken() {
+		// Testing the method
+		HttpHeaders headers = sonarProcessorUtils.getHeaders("accessToken");
+
+		// Asserting the result
+		assertEquals("Bearer accessToken", headers.getFirst(AUTHORIZATION));
+	}
+
+	@Test
+	void dateFormatter() {
+		// Testing the method
+		String result = sonarProcessorUtils.dateFormatter("1440");
+
+		// Asserting the result
+		assertEquals("3d", result);
+	}
+
 }
