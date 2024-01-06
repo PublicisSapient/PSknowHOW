a217c6cc735c97dd39259dba26eb2fd0715abcd2
purushottam08 <purushottam.gupta@publicissapient.com>
2024-01-06 06:43:41 +0530

    jira processor testcase

--- processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/constant/JiraConstants.java
+++ processors/jira/src/main/java/com/publicissapient/kpidashboard/jira/constant/JiraConstants.java
@@ -25,8 +25,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public final class JiraConstants {
-	private JiraConstants() {
-	}
 
 	public static final Set<String> ISSUE_FIELD_SET = new HashSet<>();// NOSONAR
 	public static final String STATUS = "status";
