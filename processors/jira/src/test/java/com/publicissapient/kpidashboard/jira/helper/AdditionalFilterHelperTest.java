2bbba61b7e3154eed147ab209049f305a62fa6df
deepaktandn <deepak.tandon@publicissapient.com>
2024-01-16 14:48:20 +0530

    JunitTest Class AdditionalFilterHelper for Jira

--- processors/jira/src/test/java/com/publicissapient/kpidashboard/jira/helper/AdditionalFilterHelperTest.java
+++ processors/jira/src/test/java/com/publicissapient/kpidashboard/jira/helper/AdditionalFilterHelperTest.java
@@ -16,19 +16,25 @@
  *
  ******************************************************************************/
 
-
 package com.publicissapient.kpidashboard.jira.helper;
 
-import static org.apache.commons.collections4.IterableUtils.forEach;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
 
 import com.atlassian.jira.rest.client.api.domain.BasicComponent;
+import com.atlassian.jira.rest.client.api.domain.IssueField;
 import com.publicissapient.kpidashboard.common.model.application.AdditionalFilterCategory;
 import org.bson.types.ObjectId;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -67,6 +73,21 @@ public class AdditionalFilterHelperTest {
     }
 
 	@Test
+	public void getAdditionalFilterOtherTest(){
+
+		when(projectConfig.getBasicProjectConfigId()).thenReturn(ObjectId.get());
+		AdditionalFilterConfig additionalFilterConfig1=getAdditionalFilterConfig("afOne","Labels","",getValueSet());
+		AdditionalFilterConfig additionalFilterConfig2=getAdditionalFilterConfig("","","",getValueSet());
+		when(projectConfig.getFieldMapping()).thenReturn(getFieldMapping(Arrays.asList(additionalFilterConfig1,additionalFilterConfig2)));
+		List<AdditionalFilterCategory> additionalFilterCategories =new ArrayList<>();
+		additionalFilterCategories.add(getAdditionalFilterCategory(1,"afOne","Teams"));
+		additionalFilterCategories.add(getAdditionalFilterCategory(2,"afOne1","Teams1"));
+		when(additionalFilterCategoryService.getAdditionalFilterCategories()).thenReturn(additionalFilterCategories);
+		assertEquals(0,additionalFilterHelper.getAdditionalFilter(issue,projectConfig).size());
+
+	}
+
+	@Test
 	public void getAdditionalFilterLabelsTest(){
 
 		when(projectConfig.getBasicProjectConfigId()).thenReturn(ObjectId.get());
@@ -93,31 +114,34 @@ public class AdditionalFilterHelperTest {
 		additionalFilterCategories.add(getAdditionalFilterCategory(1,"afOne","Teams"));
 		additionalFilterCategories.add(getAdditionalFilterCategory(2,"afOne1","Teams1"));
 		when(additionalFilterCategoryService.getAdditionalFilterCategories()).thenReturn(additionalFilterCategories);
-		List<BasicComponent> componentList= Arrays.asList(new BasicComponent(null, 1234567L,"Component","desc"));
-		Iterable<BasicComponent> components =new Iterable<BasicComponent>() {
-			@Override
-			public Iterator<BasicComponent> iterator() {
-				return componentList.iterator();
-			}
-		};
+		Iterable<BasicComponent> components =getComponents();
 		when(issue.getComponents()).thenReturn(components);
-		assertEquals(0,additionalFilterHelper.getAdditionalFilter(issue,projectConfig).size());
+		assertEquals(1,additionalFilterHelper.getAdditionalFilter(issue,projectConfig).size());
 
 	}
 
 	@Test
-	public void getAdditionalFilterCustomfieldTest(){
+	public void getAdditionalFilterCustomfieldTest() throws JSONException, URISyntaxException {
 
 		when(projectConfig.getBasicProjectConfigId()).thenReturn(ObjectId.get());
 		AdditionalFilterConfig additionalFilterConfig1=getAdditionalFilterConfig("afOne","CustomField","",getValueSet());
+		additionalFilterConfig1.setIdentificationField("id1");
 		AdditionalFilterConfig additionalFilterConfig2=getAdditionalFilterConfig("","","",getValueSet());
 		when(projectConfig.getFieldMapping()).thenReturn(getFieldMapping(Arrays.asList(additionalFilterConfig1,additionalFilterConfig2)));
 		List<AdditionalFilterCategory> additionalFilterCategories =new ArrayList<>();
 		additionalFilterCategories.add(getAdditionalFilterCategory(1,"afOne","Teams"));
 		additionalFilterCategories.add(getAdditionalFilterCategory(2,"afOne1","Teams1"));
 		when(additionalFilterCategoryService.getAdditionalFilterCategories()).thenReturn(additionalFilterCategories);
-
-		assertEquals(0,additionalFilterHelper.getAdditionalFilter(issue,projectConfig).size());
+		Collection<IssueField> issueFields= Arrays.asList(new IssueField("id1","name1","type1",getJSONArray()),
+				new IssueField("id2","name2","type2",getJSONArray()));
+		//Issue issue1=spy(issue);
+		Issue issue1=new Issue("summary",new URI(""),"key",123l,null,null,null,"",
+				null,null,null,null,null,new DateTime(),new DateTime(),new DateTime(),
+				null,null,null,null,issueFields,null,null,
+				null,null,null,null,null,null,null,null,
+				null
+		);
+		assertEquals(1,additionalFilterHelper.getAdditionalFilter(issue1,projectConfig).size());
 
 	}
 
@@ -132,6 +156,55 @@ public class AdditionalFilterHelperTest {
 		additionalFilterHelper.getAdditionalFilter(issue, projectConfig);
 	}
 
+	@Test(expected = InvocationTargetException.class)
+	public void getCustomFieldValuesExceptionTest() throws NoSuchMethodException, InvocationTargetException,
+			IllegalAccessException, URISyntaxException, JSONException {
+
+		// AdditionalFilterHelper
+		// additionalFilterHelper=mock(AdditionalFilterHelper.class);
+		AdditionalFilterConfig additionalFilterConfig = spy(AdditionalFilterConfig.class);
+		additionalFilterConfig.setIdentificationField("id1");
+		Method method = AdditionalFilterHelper.class.getDeclaredMethod("getCustomFieldValues", Issue.class,
+				AdditionalFilterConfig.class);
+		method.setAccessible(true);
+		Collection<IssueField> issueFields = Arrays.asList(
+				new IssueField("id1", "name1", "type1", getJSONObjectException("name1", "27", "77777")),
+				new IssueField("id2", "name2", "type2", getJSONObjectException("name1", "27", "77777")));
+		Issue issue1 = new Issue("summary", new URI(""), "key", 123l, null, null, null, "", null, null, null, null,
+				null, new DateTime(), new DateTime(), new DateTime(), null, null, null, null, issueFields, null, null,
+				null, null, null, null, null, null, null, null, null);
+
+		Set resultSet = (HashSet) method.invoke(additionalFilterHelper, issue1, additionalFilterConfig);
+
+		assertNotNull(resultSet);
+		assertEquals(1, resultSet.size());
+		assertTrue(resultSet.contains("77777"));
+
+	}
+
+	@Test
+	public void getCustomFieldValuesTest() throws NoSuchMethodException, InvocationTargetException,
+			IllegalAccessException, URISyntaxException, JSONException {
+
+		AdditionalFilterConfig additionalFilterConfig = spy(AdditionalFilterConfig.class);
+		// additionalFilterConfig.setIdentificationField("id");
+		Method method = AdditionalFilterHelper.class.getDeclaredMethod("getCustomFieldValues", Issue.class,
+				AdditionalFilterConfig.class);
+		method.setAccessible(true);
+		Collection<IssueField> issueFields = Arrays.asList(new IssueField("id1", "name1", "type1", getJSONArray()),
+				new IssueField("id2", "name2", "type2", getJSONArray()));
+		// Issue issue1=spy(issue);
+		Issue issue1 = new Issue("summary", new URI(""), "key", 123l, null, null, null, "", null, null, null, null,
+				null, new DateTime(), new DateTime(), new DateTime(), null, null, null, null, issueFields, null, null,
+				null, null, null, null, null, null, null, null, null);
+
+		Set resultSet = (HashSet) method.invoke(additionalFilterHelper, issue1, additionalFilterConfig);
+
+		assertNotNull(resultSet);
+		assertEquals(0, resultSet.size());
+
+	}
+
 	AdditionalFilterConfig getAdditionalFilterConfig(String filterId, String identifyFrom, String identificationField,
 			Set<String> valueSet) {
 		AdditionalFilterConfig additionalFilterConfig = new AdditionalFilterConfig();
@@ -143,12 +216,14 @@ public class AdditionalFilterHelperTest {
 
 	}
 
-	Set<String> getValueSet() {
+	private Set<String> getValueSet() {
 		Set<String> valueSet = new HashSet<>();
 		valueSet.add("JAVA");
 		valueSet.add("prod_defect");
 		valueSet.add("UI");
 		valueSet.add("QA_Defect");
+		valueSet.add("Component");
+		valueSet.add("CustomField");
 		return valueSet;
 	}
 
@@ -167,11 +242,53 @@ public class AdditionalFilterHelperTest {
 		return additionalFilterCategory;
 	}
 
-	Set<String> getLabels(String... labelArr) {
+	private Set<String> getLabels(String... labelArr) {
 		Set<String> labels = new HashSet<>();
 		for (String s : labelArr) {
 			labels.add(s);
 		}
 		return labels;
+	}
+
+	private JSONObject getJSONObject(String name, String age, String salary) throws JSONException {
+		JSONObject obj = new JSONObject();
+		obj.put("name", name);
+		obj.put("age", age);
+		obj.put("value", salary);
+		return obj;
+	}
+
+	private JSONObject getJSONObjectException(String name, String age, String salary) throws JSONException {
+		JSONObject obj = new JSONObject();
+		obj.put("name", name);
+		obj.put("age", age);
+		obj.put("valueExcption", salary);
+		return obj;
+	}
+
+	private JSONArray getJSONArrayException(String name, int age, int salary) throws JSONException {
+		JSONArray obj = new JSONArray();
+		obj.put(name);
+		obj.put(age);
+		obj.put(salary);
+		return obj;
+	}
+
+	private JSONArray getJSONArray() throws JSONException {
+		JSONArray arr = new JSONArray();
+		arr.put(getJSONObject("name1", "27", "77777"));
+		arr.put(getJSONObject("name2", "37", "77777"));
+		return arr;
+	}
+
+	private Iterable<BasicComponent> getComponents() {
+		List<BasicComponent> componentList = Arrays.asList(new BasicComponent(null, 1234567L, "Component", "desc"));
+		Iterable<BasicComponent> components = new Iterable<BasicComponent>() {
+			@Override
+			public Iterator<BasicComponent> iterator() {
+				return componentList.iterator();
+			}
+		};
+		return components;
 	}
 }
