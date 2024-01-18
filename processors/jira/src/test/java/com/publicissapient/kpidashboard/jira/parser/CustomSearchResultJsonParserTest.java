78b71aeb737b5a749ef3ab2918d5dbf8ff7c5420
hargupta15 <harsh.gupta@publicissapient.com>
2024-01-18 13:22:06 +0530

    correcting tc

--- processors/jira/src/test/java/com/publicissapient/kpidashboard/jira/parser/CustomSearchResultJsonParserTest.java
+++ processors/jira/src/test/java/com/publicissapient/kpidashboard/jira/parser/CustomSearchResultJsonParserTest.java
@@ -1,114 +1,114 @@
-//package com.publicissapient.kpidashboard.jira.parser;
-//
-//import static org.junit.Assert.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import org.codehaus.jackson.map.ObjectMapper;
-//import org.codehaus.jettison.json.JSONArray;
-//import org.codehaus.jettison.json.JSONException;
-//import org.codehaus.jettison.json.JSONObject;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//import com.atlassian.jira.rest.client.api.domain.Issue;
-//import com.atlassian.jira.rest.client.api.domain.SearchResult;
-//import com.publicissapient.kpidashboard.jira.dataFactories.IssueDataFactory;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class CustomSearchResultJsonParserTest {
-//
-//	@InjectMocks
-//	CustomSearchResultJsonParser customSearchResultJsonParser;
-//
-//	private List<Issue> issues;
-//
-//	private List<Object> objects;
-//
-//	@Before
-//	public void setUp() {
-//		objects = getMockIssues();
-//	}
-//
-//	@Test
-//	public void testParse() throws JSONException {
-//		// Mock data for your JSONObject
-//		JSONObject json = createMockJson();
-//
-//		// when(customSearchResultJsonParser.parse(json)).thenReturn(createMockIssueIterable());
-//
-//		// Set up the parser with the mocked issueParser
-//		// customSearchResultJsonParser.setIssueParser(issueParser);
-//
-//		// Call the method to test
-//		SearchResult result = customSearchResultJsonParser.parse(json);
-//
-//		// Assert the result
-//		assertNotNull(result);
-//		assertEquals(1, result.getStartIndex());
-//		assertEquals(5, result.getMaxResults());
-//		assertEquals(2, result.getTotal());
-//		assertNotNull(result.getIssues());
-//	}
-//
-//	private JSONObject createMockJson() throws JSONException {
-//		JSONObject json = new JSONObject();
-//		json.put("startAt", 1);
-//		json.put("maxResults", 5);
-//		json.put("total", 2);
-//
-//		JSONArray issuesArray = new JSONArray();
-//		issuesArray = convertObjectListToJsonArray(objects);
-//		// Add more issues as needed
-//
-//		json.put("issues", issuesArray);
-//
-//		JSONObject namesObject = new JSONObject();
-//		namesObject.put("key", "Issue Key");
-//		namesObject.put("summary", "Summary");
-//
-//		json.put("names", namesObject);
-//
-//		JSONObject schemaObject = new JSONObject();
-//		schemaObject.put("fields", createMockFieldJson("summary", "Summary", "main"));
-//
-//		json.put("schema", schemaObject);
-//
-//		return json;
-//	}
-//
-//	public static JSONArray convertObjectListToJsonArray(List<Object> objectList) throws JSONException {
-//		ObjectMapper mapper = new ObjectMapper();
-//
-//		// Convert List<Object> to JSON string
-//		String jsonString;
-//		try {
-//			jsonString = mapper.writeValueAsString(objectList);
-//		} catch (IOException e) {
-//			throw new RuntimeException("Error converting List<Object> to JSON string", e);
-//		}
-//
-//		// Convert JSON string to JSONArray
-//		return new JSONArray(jsonString);
-//	}
-//
-//	private JSONObject createMockFieldJson(String id, String name, String type) throws JSONException {
-//		JSONObject fieldJson = new JSONObject();
-//		fieldJson.put("id", id);
-//		fieldJson.put("name", name);
-//
-//		fieldJson.put("type", type);
-//		// Add more properties as needed
-//		return fieldJson;
-//	}
-//
-//	private List<Object> getMockIssues() {
-//		IssueDataFactory issueDataFactory = IssueDataFactory.newInstance("/json/default/issues.json");
-//		return issueDataFactory.getIssues();
-//	}
-//}
+package com.publicissapient.kpidashboard.jira.parser;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.SearchResult;
+import com.publicissapient.kpidashboard.jira.dataFactories.IssueDataFactory;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomSearchResultJsonParserTest {
+
+	@InjectMocks
+	CustomSearchResultJsonParser customSearchResultJsonParser;
+
+	private List<Issue> issues;
+
+	private List<Object> objects;
+
+	@Before
+	public void setUp() {
+		objects = getMockIssues();
+	}
+
+	@Test
+	public void testParse() throws JSONException {
+		// Mock data for your JSONObject
+		JSONObject json = createMockJson();
+
+		// when(customSearchResultJsonParser.parse(json)).thenReturn(createMockIssueIterable());
+
+		// Set up the parser with the mocked issueParser
+		// customSearchResultJsonParser.setIssueParser(issueParser);
+
+		// Call the method to test
+		SearchResult result = customSearchResultJsonParser.parse(json);
+
+		// Assert the result
+		assertNotNull(result);
+		assertEquals(1, result.getStartIndex());
+		assertEquals(5, result.getMaxResults());
+		assertEquals(2, result.getTotal());
+		assertNotNull(result.getIssues());
+	}
+
+	private JSONObject createMockJson() throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("startAt", 1);
+		json.put("maxResults", 5);
+		json.put("total", 2);
+
+		JSONArray issuesArray = new JSONArray();
+		issuesArray = convertObjectListToJsonArray(objects);
+		// Add more issues as needed
+
+		json.put("issues", issuesArray);
+
+		JSONObject namesObject = new JSONObject();
+		namesObject.put("key", "Issue Key");
+		namesObject.put("summary", "Summary");
+
+		json.put("names", namesObject);
+
+		JSONObject schemaObject = new JSONObject();
+		schemaObject.put("fields", createMockFieldJson("summary", "Summary", "main"));
+
+		json.put("schema", schemaObject);
+
+		return json;
+	}
+
+	public static JSONArray convertObjectListToJsonArray(List<Object> objectList) throws JSONException {
+		ObjectMapper mapper = new ObjectMapper();
+
+		// Convert List<Object> to JSON string
+		String jsonString;
+		try {
+			jsonString = mapper.writeValueAsString(objectList);
+		} catch (IOException e) {
+			throw new RuntimeException("Error converting List<Object> to JSON string", e);
+		}
+
+		// Convert JSON string to JSONArray
+		return new JSONArray(jsonString);
+	}
+
+	private JSONObject createMockFieldJson(String id, String name, String type) throws JSONException {
+		JSONObject fieldJson = new JSONObject();
+		fieldJson.put("id", id);
+		fieldJson.put("name", name);
+
+		fieldJson.put("type", type);
+		// Add more properties as needed
+		return fieldJson;
+	}
+
+	private List<Object> getMockIssues() {
+		IssueDataFactory issueDataFactory = IssueDataFactory.newInstance("/json/default/issues.json");
+		return issueDataFactory.getIssues();
+	}
+}
--- processors/jira/src/test/java/com/publicissapient/kpidashboard/jira/parser/JsonWeakParserForStringTest.java
+++ processors/jira/src/test/java/com/publicissapient/kpidashboard/jira/parser/JsonWeakParserForStringTest.java
@@ -1,35 +1,35 @@
-//package com.publicissapient.kpidashboard.jira.parser;
-//
-//import org.codehaus.jettison.json.JSONException;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class JsonWeakParserForStringTest {
-//
-//	@InjectMocks
-//	JsonWeakParserForString jsonWeakParserForString;
-//
-//	@Test
-//	public void testParseWithStringInput() throws JSONException {
-//		String inputString = "TestString";
-//
-//		try {
-//			String result = jsonWeakParserForString.parse(inputString);
-//			Assert.assertEquals("Parsed string should be equal to input string", inputString, result);
-//		} catch (JSONException e) {
-//			Assert.fail("Unexpected JSONException: " + e.getMessage());
-//		}
-//	}
-//
-//	@Test(expected = JSONException.class)
-//	public void testParseWithNonStringInput() throws JSONException {
-//		Integer nonStringInput = 123;
-//
-//		jsonWeakParserForString.parse(nonStringInput);
-//		// If the code reaches this point, the test should fail because it should throw a JSONException.
-//	}
-//}
+package com.publicissapient.kpidashboard.jira.parser;
+
+import org.codehaus.jettison.json.JSONException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JsonWeakParserForStringTest {
+
+	@InjectMocks
+	JsonWeakParserForString jsonWeakParserForString;
+
+	@Test
+	public void testParseWithStringInput() throws JSONException {
+		String inputString = "TestString";
+
+		try {
+			String result = jsonWeakParserForString.parse(inputString);
+			Assert.assertEquals("Parsed string should be equal to input string", inputString, result);
+		} catch (JSONException e) {
+			Assert.fail("Unexpected JSONException: " + e.getMessage());
+		}
+	}
+
+	@Test(expected = JSONException.class)
+	public void testParseWithNonStringInput() throws JSONException {
+		Integer nonStringInput = 123;
+
+		jsonWeakParserForString.parse(nonStringInput);
+		// If the code reaches this point, the test should fail because it should throw a JSONException.
+	}
+}
