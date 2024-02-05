d2d537b3f9a32aa63a2727fb5bf3ce0b003d1f43
rendk <renukaprasad.dk@publicissapient.com>
2024-02-05 17:54:43 +0530

    customapi JunitTests

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/appsetting/service/ConfigHelperServiceTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/appsetting/service/ConfigHelperServiceTest.java
@@ -23,6 +23,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.publicissapient.kpidashboard.apis.projectconfig.basic.service.ProjectBasicConfigService;
+import com.publicissapient.kpidashboard.common.model.application.*;
+import com.publicissapient.kpidashboard.common.model.userboardconfig.UserBoardConfig;
+import com.publicissapient.kpidashboard.common.repository.application.*;
+import com.publicissapient.kpidashboard.common.repository.userboardconfig.UserBoardConfigRepository;
 import org.bson.types.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,11 +42,6 @@ import com.publicissapient.kpidashboard.apis.common.service.CacheService;
 import com.publicissapient.kpidashboard.apis.data.FieldMappingDataFactory;
 import com.publicissapient.kpidashboard.apis.data.ProjectBasicConfigDataFactory;
 import com.publicissapient.kpidashboard.common.constant.CommonConstant;
-import com.publicissapient.kpidashboard.common.model.application.FieldMapping;
-import com.publicissapient.kpidashboard.common.model.application.ProjectBasicConfig;
-import com.publicissapient.kpidashboard.common.model.application.Tool;
-import com.publicissapient.kpidashboard.common.repository.application.FieldMappingRepository;
-import com.publicissapient.kpidashboard.common.repository.application.ProjectBasicConfigRepository;
 import com.publicissapient.kpidashboard.common.repository.application.impl.ProjectToolConfigRepositoryCustom;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -57,6 +57,19 @@ public class ConfigHelperServiceTest {
 	private FieldMappingRepository fieldMappingRepository;
 	@Mock
 	private ProjectToolConfigRepositoryCustom toolConfigRepository;
+	@Mock
+	UserBoardConfigRepository userBoardConfigRepository;
+	@Mock
+	FieldMappingStructureRepository fieldMappingStructureRepository;
+
+	@Mock
+	ProjectToolConfigRepository projectToolConfigRepository;
+	@Mock
+	HierarchyLevelSuggestionRepository hierarchyLevelSuggestionRepository;
+	@Mock
+	ProjectBasicConfigService projectBasicConfigService;
+	@Mock
+	KpiMasterRepository kpiMasterRepository;
 	@InjectMocks
 	private ConfigHelperService configHelperService;
 
@@ -91,5 +104,87 @@ public class ConfigHelperServiceTest {
 		Assertions.assertTrue(
 				((Map<ObjectId, Tool>) configHelperService.getConfigMapData(CommonConstant.CACHE_TOOL_CONFIG_MAP))
 						.size() > 0);
+	}
+
+	@Test
+	public void loadUserBoardConfig()
+	{
+		List<UserBoardConfig> userBoardConfigs = new ArrayList<>();
+		UserBoardConfig userBoardConfig = new UserBoardConfig();
+		userBoardConfig.setId(new ObjectId("5fd9ab0995fe13000165d0ba"));
+		userBoardConfig.setUsername("PSK");
+		userBoardConfigs.add(userBoardConfig);
+		Mockito.when(userBoardConfigRepository.findAll()).thenReturn(userBoardConfigs);
+		Assertions.assertTrue(configHelperService.loadUserBoardConfig().get(0).getUsername().equals("PSK"));
+	}
+	@Test
+	public void loadFieldMappingStructure()
+	{
+		Mockito.when(fieldMappingStructureRepository.findAll()).thenReturn(new ArrayList<>());
+		Assertions.assertTrue(configHelperService.loadFieldMappingStructure().getClass()!=null);
+	}
+	@Test
+	public void loadAllProjectToolConfig()
+	{
+		Mockito.when(projectToolConfigRepository.findAll()).thenReturn(new ArrayList<>());
+		Assertions.assertTrue(configHelperService.loadAllProjectToolConfig().getClass()!=null);
+	}
+	@Test
+	public void loadHierarchyLevelSuggestion()
+	{
+		Mockito.when(hierarchyLevelSuggestionRepository.findAll()).thenReturn(new ArrayList<>());
+		Assertions.assertTrue(configHelperService.loadHierarchyLevelSuggestion().getClass()!=null);
+	}
+	@Test
+	public void loadProjectBasicTree()
+	{
+		configHelperService.loadProjectBasicTree();
+	}
+	@Test
+	public void calculateCriteriaForCircleKPI()
+	{
+		List<KpiMaster> kpiMasters = new ArrayList<>();
+		KpiMaster kpiMaster = new KpiMaster();
+		kpiMaster.setKpiId("5fd9ab0995fe13000165d0ba");
+		kpiMaster.setAggregationCircleCriteria("criteriaX");
+		kpiMasters.add(kpiMaster);
+		Mockito.when(kpiMasterRepository.findAll()).thenReturn(kpiMasters);
+		Assertions.assertTrue(configHelperService.calculateCriteriaForCircleKPI().get("5fd9ab0995fe13000165d0ba").equals("criteriaX"));
+	}
+	@Test
+	public void calculateCriteria()
+	{
+		List<KpiMaster> kpiMasters = new ArrayList<>();
+		KpiMaster kpiMaster = new KpiMaster();
+		kpiMaster.setKpiId("5fd9ab0995fe13000165d0ba");
+		kpiMaster.setAggregationCriteria("criteriaX");
+		kpiMasters.add(kpiMaster);
+		Mockito.when(kpiMasterRepository.findAll()).thenReturn(kpiMasters);
+		Assertions.assertTrue(configHelperService.calculateCriteria().get("5fd9ab0995fe13000165d0ba").equals("criteriaX"));
+	}
+	@Test
+	public void calculateMaturity()
+	{
+		List<KpiMaster> kpiMasters = new ArrayList<>();
+		KpiMaster kpiMaster = new KpiMaster();
+		kpiMaster.setKpiId("5fd9ab0995fe13000165d0ba");
+		List<String> maturityRange = new ArrayList<>();
+				maturityRange.add("criteriaX");
+		kpiMaster.setMaturityRange(maturityRange);
+		kpiMasters.add(kpiMaster);
+		Mockito.when(kpiMasterRepository.findAll()).thenReturn(kpiMasters);
+		Assertions.assertTrue(configHelperService.calculateMaturity().get("5fd9ab0995fe13000165d0ba").get(0).equals("criteriaX"));
+	}
+	@Test
+	public void loadProjectToolConfigTest()
+	{
+		List<ProjectToolConfig> projectToolConfigs = new ArrayList<>();
+		ProjectToolConfig projectToolConfig = new ProjectToolConfig();
+		projectToolConfig.setBasicProjectConfigId(new ObjectId("5fd9ab0995fe13000165d0ba"));
+		projectToolConfig.setToolName("PSKnowHow");
+		projectToolConfig.setProjectKey("123");
+		projectToolConfigs.add(projectToolConfig);
+		Mockito.when(projectToolConfigRepository.findAll()).thenReturn(projectToolConfigs);
+		configHelperService.loadProjectToolConfig();
 	}
 }
