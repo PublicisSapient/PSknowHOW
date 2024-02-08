52d77f987b7392a7e84106b722cc76911375f726
hirbabar <hirbabar@publicisgroupe.net>
2024-02-08 11:50:35 +0530

    remove ad details code for sonar issue

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/auth/service/AuthTypesConfigServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/auth/service/AuthTypesConfigServiceImpl.java
@@ -47,32 +47,6 @@ public class AuthTypesConfigServiceImpl implements AuthTypesConfigService {
 	@Autowired
 	private UserInfoService userInfoService;
 
-	@Override
-	public AuthTypeConfig saveAuthTypeConfig(AuthTypeConfig authTypeConfig) {
-
-		ValidationMessage validationMessage = authTypeConfigValidator.validateConfig(authTypeConfig);
-		if (validationMessage.isValid()) {
-
-			AuthTypeStatus authTypeStatus = authTypeConfig.getAuthTypeStatus();
-
-			List<GlobalConfig> globalConfigs = globalConfigRepository.findAll();
-			globalConfigs.get(0).setAuthTypeStatus(authTypeStatus);
-			if (authTypeConfig.getAuthTypeStatus().isAdLogin()) {
-				ADServerDetail adServerDetail = authTypeConfig.getAdServerDetail();
-				String passEncrypt = encryptStringForDb(adServerDetail.getPassword());
-				adServerDetail.setPassword(passEncrypt);
-				globalConfigs.get(0).setAdServerDetail(adServerDetail);
-			}
-
-			globalConfigRepository.saveAll(globalConfigs);
-			invalidateUsersAuthToken(authTypeStatus);
-		} else {
-			throw new InvalidAuthTypeConfigException(validationMessage.getMessage());
-		}
-
-		return authTypeConfig;
-	}
-
 	private void invalidateUsersAuthToken(AuthTypeStatus authTypeStatus) {
 		List<String> usernames = new ArrayList<>();
 		// find users
