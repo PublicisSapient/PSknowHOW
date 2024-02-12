63516a543dc5996ea64ce09fcb51d540e58b5ea6
hinshah <hinal.shah@publicissapient.com>
2024-02-12 15:03:38 +0530

    changes

--- UI/src/app/dashboard/filter/filter.component.ts
+++ UI/src/app/dashboard/filter/filter.component.ts
@@ -1461,7 +1461,6 @@ export class FilterComponent implements OnInit, OnDestroy {
         this.service.setSelectedProject(null);
         this.service.setCurrentUserDetails({});
         this.service.setVisibleSideBar(false);
-         console.log('Success clear local storage :', responseData);
         if(!environment['AUTHENTICATION_SERVICE']){
           this.router.navigate(['./authentication/login']);
         } else{
@@ -1470,10 +1469,7 @@ export class FilterComponent implements OnInit, OnDestroy {
           };
           this.httpService.getUserValidation(obj).toPromise()
           .then((response) => {
-            if (response && response['success']) {
-              console.log("cookie not clear due to some reason");
-            } else {
-              console.log("cookie clear");
+            if (response && !response['success']) {
               let redirect_uri = window.location.href;
               window.location.href = environment.CENTRAL_LOGIN_URL + '?redirect_uri=' + redirect_uri;
             }
