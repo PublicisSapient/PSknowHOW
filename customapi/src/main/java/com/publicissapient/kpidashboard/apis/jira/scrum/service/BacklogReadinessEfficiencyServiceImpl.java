71225813ec594ee795fdaa92894abec7295c75fa
rendk <renukaprasad.dk@publicissapient.com>
2024-02-20 19:03:02 +0530

    DTS-31999 Client_master Review comments

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/BacklogReadinessEfficiencyServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/BacklogReadinessEfficiencyServiceImpl.java
@@ -222,7 +222,7 @@ public class BacklogReadinessEfficiencyServiceImpl extends JiraKPIService<Intege
 						Map<String,JiraIssueCustomHistory> jiraIssueCustomHistoryMap = historyForIssues.stream().collect(Collectors.toMap(JiraIssueCustomHistory::getStoryID,jiraIssueCustomHistory->jiraIssueCustomHistory));
 						List<IterationKpiModalValue> modalValues = new ArrayList<>();
 						for (JiraIssue jiraIssue : issues) {
-							KPIExcelUtility.populateBackLogData(overAllmodalValues, modalValues, jiraIssue,jiraIssueCustomHistoryMap.get(jiraIssue.getNumber()),fieldMapping.getReadyForDevelopmentStatusKPI138());
+							KPIExcelUtility.populateBackLogData(overAllmodalValues, modalValues, jiraIssue,jiraIssueCustomHistoryMap.getOrDefault(jiraIssue.getNumber(),new JiraIssueCustomHistory()),fieldMapping.getReadyForDevelopmentStatusKPI138());
 							issueCount = issueCount + 1;
 							overAllIssueCount.set(0, overAllIssueCount.get(0) + 1);
 							AtomicLong difference = getActivityCycleTimeForAnIssue(
