03b62b8a48f324a7d6f7f33c2fb9b11f2b14c171
hirbabar <hirbabar@publicisgroupe.net>
2024-03-05 13:26:52 +0530

    comment remove

--- central-auth-service/authapi/src/main/java/com/publicissapient/kpidashboard/apis/controller/APITokenController.java
+++ central-auth-service/authapi/src/main/java/com/publicissapient/kpidashboard/apis/controller/APITokenController.java
@@ -52,9 +52,7 @@ public class APITokenController {
 	 * @return
 	 */
 	@GetMapping(value = "/generateAPIKey/{resource}") // NOSONAR
-	public ResponseEntity<ServiceResponse> generateAndSaveToken(@PathVariable String resource,
-			HttpServletRequest request) {
-		// Cookie authCookie = cookieUtil.getAuthCookie(request);
+	public ResponseEntity<ServiceResponse> generateAndSaveToken(@PathVariable String resource) {
 		return ResponseEntity.status(HttpStatus.OK).body(tokenAuthenticationService.generateAndSaveToken(resource));
 	}
 }
