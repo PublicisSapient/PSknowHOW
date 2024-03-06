b67e32330b4ac01d3e66b1b2bf40fb712ade04e6
hirbabar <hirbabar@publicisgroupe.net>
2024-03-06 16:12:13 +0530

    thymeleaf version updated and kafka issue fix

--- kafka-consumers/notification-consumer/src/main/java/com/publicissapient/kpidashboard/notification/service/NotificationServiceImpl.java
+++ kafka-consumers/notification-consumer/src/main/java/com/publicissapient/kpidashboard/notification/service/NotificationServiceImpl.java
@@ -42,7 +42,7 @@ import org.springframework.web.client.RestTemplate;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.exceptions.TemplateInputException;
 import org.thymeleaf.exceptions.TemplateProcessingException;
-import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
