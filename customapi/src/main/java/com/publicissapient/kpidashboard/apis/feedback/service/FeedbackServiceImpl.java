513849f3d8c1d465d43e7178170f19da4e17c42d
hirbabar <hirbabar@publicisgroupe.net>
2024-03-07 17:03:17 +0530

    feedback issue fix

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/feedback/service/FeedbackServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/feedback/service/FeedbackServiceImpl.java
@@ -5,20 +5,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.publicissapient.kpidashboard.common.service.NotificationService;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import com.publicissapient.kpidashboard.apis.auth.repository.AuthenticationRepository;
 import com.publicissapient.kpidashboard.apis.common.service.CommonService;
+import com.publicissapient.kpidashboard.apis.common.service.UserInfoService;
 import com.publicissapient.kpidashboard.apis.config.CustomApiConfig;
 import com.publicissapient.kpidashboard.apis.enums.NotificationCustomDataEnum;
 import com.publicissapient.kpidashboard.apis.model.FeedbackSubmitDTO;
 import com.publicissapient.kpidashboard.common.model.application.EmailServerDetail;
 import com.publicissapient.kpidashboard.common.model.application.GlobalConfig;
+import com.publicissapient.kpidashboard.common.model.rbac.UserInfo;
 import com.publicissapient.kpidashboard.common.repository.application.GlobalConfigRepository;
+import com.publicissapient.kpidashboard.common.service.NotificationService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -39,7 +40,7 @@ public class FeedbackServiceImpl implements FeedbackService {
 	private CommonService commonService;
 
 	@Autowired
-	private AuthenticationRepository authenticationRepository;
+	private UserInfoService userInfoService;
 
 	@Autowired
 	private GlobalConfigRepository globalConfigRepository;
@@ -51,13 +52,13 @@ public class FeedbackServiceImpl implements FeedbackService {
 	private KafkaTemplate<String, Object> kafkaTemplate;
 
 	@Override
-	public boolean submitFeedback(FeedbackSubmitDTO feedback) {
+	public boolean submitFeedback(FeedbackSubmitDTO feedback, String loggedUserName) {
 		boolean status = true;
 		List<String> emailAddresses = null;
 
 		List<GlobalConfig> globalConfigs = globalConfigRepository.findAll();
 		GlobalConfig globalConfig = CollectionUtils.isEmpty(globalConfigs) ? null : globalConfigs.get(0);
-		EmailServerDetail emailServerDetail =  globalConfig == null ? null : globalConfig.getEmailServerDetail();
+		EmailServerDetail emailServerDetail = globalConfig == null ? null : globalConfig.getEmailServerDetail();
 		if (emailServerDetail != null) {
 			emailAddresses = emailServerDetail.getFeedbackEmailIds();
 		} else {
@@ -73,11 +74,12 @@ public class FeedbackServiceImpl implements FeedbackService {
 				status = false;
 				log.error("SubmitFeedbackController: Server Host name is not bind with submit feedback Request mail ");
 			}
-			Map<String, String> customData = createCustomData(feedback, serverPath);
+			Map<String, String> customData = createCustomData(feedback, serverPath, loggedUserName);
 			log.info("Notification message sent to kafka with key : {}", NOTIFICATION_KEY);
-			String templateKey = customApiConfig.getMailTemplate().getOrDefault(NOTIFICATION_KEY,"");
+			String templateKey = customApiConfig.getMailTemplate().getOrDefault(NOTIFICATION_KEY, "");
 			notificationService.sendNotificationEvent(emailAddresses, customData, feedbackNotificationSubjects,
-					NOTIFICATION_KEY, customApiConfig.getKafkaMailTopic(),customApiConfig.isNotificationSwitch(),kafkaTemplate,templateKey,customApiConfig.isMailWithoutKafka());
+					NOTIFICATION_KEY, customApiConfig.getKafkaMailTopic(), customApiConfig.isNotificationSwitch(),
+					kafkaTemplate, templateKey, customApiConfig.isMailWithoutKafka());
 		} else {
 			status = false;
 			log.error("Notification Event not sent : No email address "
@@ -87,22 +89,15 @@ public class FeedbackServiceImpl implements FeedbackService {
 		return status;
 	}
 
-	private Map<String, String> createCustomData(FeedbackSubmitDTO feedback, String serverPath) {
+	private Map<String, String> createCustomData(FeedbackSubmitDTO feedback, String serverPath, String loggedUserName) {
 		Map<String, String> customData = new HashMap<>();
-		String email = "";
-		com.publicissapient.kpidashboard.apis.auth.model.Authentication authentication = authenticationRepository
-				.findByUsername(feedback.getUsername());
-		if (null == authentication) {
-			log.error("User {} Does not Exist in Authentication Collection", feedback.getUsername());
-		} else {
-			email = authentication.getEmail();
+		UserInfo userInfo = userInfoService.getUserInfo(loggedUserName);
+		if (userInfo != null) {
+			customData.put(NotificationCustomDataEnum.USER_NAME.getValue(), loggedUserName);
+			customData.put(NotificationCustomDataEnum.USER_EMAIL.getValue(), userInfo.getEmailAddress());
 		}
-		customData.put(NotificationCustomDataEnum.USER_NAME.getValue(), feedback.getUsername());
-		customData.put(NotificationCustomDataEnum.USER_EMAIL.getValue(), email);
 		customData.put(NotificationCustomDataEnum.SERVER_HOST.getValue(), serverPath);
 		customData.put(NotificationCustomDataEnum.FEEDBACK_CONTENT.getValue(), feedback.getFeedback());
-		customData.put(NotificationCustomDataEnum.FEEDBACK_CATEGORY.getValue(), feedback.getCategory());
-		customData.put(NotificationCustomDataEnum.FEEDBACK_TYPE.getValue(), feedback.getFeedbackType());
 		return customData;
 	}
 
