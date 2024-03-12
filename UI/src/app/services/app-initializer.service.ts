7f4ea804ad079ca620dba59d6e14692b4432d3a2
hinshah <hinal.shah@publicissapient.com>
2024-03-12 11:24:43 +0530

    added alerts

--- UI/src/app/services/app-initializer.service.ts
+++ UI/src/app/services/app-initializer.service.ts
@@ -17,42 +17,46 @@ export class AppInitializerService {
 
   validateToken() {
     return new Promise<void>((resolve, reject) => {
-      if (environment['AUTHENTICATION_SERVICE']) {
-        let url = window.location.href;
-        let authToken = url.split("authToken=")?.[1]?.split("&")?.[0];
-        if (authToken) {
-          this.sharedService.setAuthToken(authToken);
-        }
-        let obj = {
-          'resource': environment.RESOURCE,
-          'authToken': authToken
-        };
-        // Make API call or initialization logic here...
-        this.httpService.getUserValidation(obj).subscribe((response) => {
-          if (response?.['success']) {
-            this.sharedService.setCurrentUserDetails(response?.['data']);
-            localStorage.setItem("user_name", response?.['data']?.user_name);
-            localStorage.setItem("user_email", response?.['data']?.user_email);
-            const redirect_uri = localStorage.getItem('redirect_uri');
-            if (authToken) {
-              this.ga.setLoginMethod(response?.['data'], response?.['data']?.authType);
-            }
-            if (redirect_uri) {
-              if (redirect_uri.startsWith('#')) {
-                this.router.navigate([redirect_uri.split('#')[1]])
-              } else {
-                this.router.navigate([redirect_uri]);
-              }
-              localStorage.removeItem('redirect_uri');
-            } else {
-              this.router.navigate(['/dashboard/iteration'], { queryParamsHandling: 'merge' });
-            }
+      console.log(environment['AUTHENTICATION_SERVICE']);
+      setTimeout(() => {
+        if (environment['AUTHENTICATION_SERVICE']) {
+          let url = window.location.href;
+          let authToken = url.split("authToken=")?.[1]?.split("&")?.[0];
+          if (authToken) {
+            this.sharedService.setAuthToken(authToken);
           }
-        }, error => {
-          console.log(error);
-        })
-      }
-      resolve();
+          let obj = {
+            'resource': environment.RESOURCE,
+            'authToken': authToken
+          };
+          // Make API call or initialization logic here...
+          this.httpService.getUserValidation(obj).subscribe((response) => {
+            if (response?.['success']) {
+              this.sharedService.setCurrentUserDetails(response?.['data']);
+              localStorage.setItem("user_name", response?.['data']?.user_name);
+              localStorage.setItem("user_email", response?.['data']?.user_email);
+              const redirect_uri = localStorage.getItem('redirect_uri');
+              if (authToken) {
+                this.ga.setLoginMethod(response?.['data'], response?.['data']?.authType);
+              }
+              if (redirect_uri) {
+                if (redirect_uri.startsWith('#')) {
+                  this.router.navigate([redirect_uri.split('#')[1]])
+                } else {
+                  this.router.navigate([redirect_uri]);
+                }
+                localStorage.removeItem('redirect_uri');
+              } else {
+                this.router.navigate(['/dashboard/iteration'], { queryParamsHandling: 'merge' });
+              }
+            }
+          }, error => {
+            console.log(error);
+          })
+        }
+        resolve();
+      }, 5000)
+      
     })
 
   }
@@ -64,14 +68,15 @@ export class AppInitializerService {
       } else {
         const env$ = this.http.get('assets/env.json').pipe(
           tap(env => {
+            alert("inside app initializer " + env['AUTHENTICATION_SERVICE'])
+            alert("inside app initializer " + env['CENTRAL_LOGIN_URL'])
             environment['baseUrl'] = env['baseUrl'] || '';
             environment['SSO_LOGIN'] = env['SSO_LOGIN'] || false;
-            environment['CENTRAL_LOGIN_URL'] = env['CENTRAL_LOGIN_URL'] || '';
             environment['AUTHENTICATION_SERVICE'] = env['AUTHENTICATION_SERVICE'] || false;
+            environment['CENTRAL_LOGIN_URL'] = env['CENTRAL_LOGIN_URL'] || '';
             environment['MAP_URL'] = env['MAP_URL'] || '';
             environment['RETROS_URL'] = env['RETROS_URL'] || '';
           }));
-
         env$.toPromise().then(async res => {
           this.featureToggleService.config = this.featureToggleService.loadConfig().then((res) => res);
         });
