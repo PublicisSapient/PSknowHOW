a3ff0c9659f2f25f93805f546f4014074cca6095
kunkambl <kunkambl@publicisgroupe.net>
2024-04-22 15:31:12 +0530

    removed unused imports

--- common/src/main/java/com/publicissapient/kpidashboard/common/repository/application/impl/ProjectToolConfigRepositoryImpl.java
+++ common/src/main/java/com/publicissapient/kpidashboard/common/repository/application/impl/ProjectToolConfigRepositoryImpl.java
@@ -21,8 +21,6 @@ package com.publicissapient.kpidashboard.common.repository.application.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
