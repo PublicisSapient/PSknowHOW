72f0d0786bf8bb11122ba7d4164d0968dbe588fd
hinshah <hinal.shah@publicissapient.com>
2024-04-29 19:02:26 +0530

    added flag for speed suite

---
+++ central-auth-service/central-login-ui-react/src/components/SuiteLogos/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import BgItem from "../../components/BgItem";
+
+const bg = ['KnowHOWGroup', 'RetrosGroup', 'APGroup'];
+
+const SuiteLogos = () => {
+    const isSpeedSuite = process.env['REACT_APP_SPEED_SUITE'] ? process.env?.['REACT_APP_SPEED_SUITE'] : false;
+    
+    return (
+        <div className="w-3/5 h-screen gradient-container">
+            <div className="h-3/4 w-1/2 mt-36 m-auto">
+            {
+                !isSpeedSuite && 
+                <BgItem key={0} item={bg[0]} />
+            }
+            {
+                isSpeedSuite && 
+                bg.map((item, index) => (
+                    <BgItem key={index} item={item} />
+                ))
+            }
+                
+            </div>
+            <div className="bg-image absolute h-[150px] w-[150px] bottom-0 left-0"></div>
+        </div>
+    )
+}
+
+export default SuiteLogos;
