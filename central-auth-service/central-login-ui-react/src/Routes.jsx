160198a5bae527fabecbf7f3c44eae47efbf7cba
hinshah <hinal.shah@publicissapient.com>
2024-05-03 16:23:54 +0530

    changes to saving redirect url

--- central-auth-service/central-login-ui-react/src/Routes.jsx
+++ central-auth-service/central-login-ui-react/src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Img } from "./components/Img";
 import StatusPage from './pages/status';
 const LoginPage = React.lazy(() => import("./pages/login"));
@@ -7,8 +7,7 @@ const ForgotPasswordPage = React.lazy(() => import('./pages/forgot-password'));
 const ResetPasswordPage = React.lazy(() => import('./pages/reset-password'));
 const RegisterPage = React.lazy(() => import('./pages/register'));
 const ProjectRoutes = () => {
-  const location = useLocation();
-  const {search} = location;
+  const search = window.location.href;
   return (
     <React.Suspense fallback={<div className="w-full h-screen text-center"><Img
       src={`${process.env.PUBLIC_URL}/images/spinner-black.png`} height='20'
