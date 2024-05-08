5efaab6fd04e79d8f48d818b62efa4d22e7abc90
Girish <girish.pathak@publicissapient.com>
2024-05-08 18:34:52 +0530

    UI-8.0.0

--- database-scripts/archive/scripts/remove_backlog_collections.js
+++
@@ -1,4 +0,0 @@
-print("Start: Script to Removing Backlog Collection")
-db.issue_backlog.drop();
-db.issue_backlog_custom_history.drop();
-print("End: Script to Removing Backlog Collection")
--- kafka-consumers/notification-consumer/src/main/java/com/publicissapient/kpidashboard/notification/service/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.publicissapient.kpidashboard.notification.service;
-
-import com.publicissapient.kpidashboard.notification.model.EmailEvent;
-
-public interface NotificationService {
-	
-	public void sendMail(String key,EmailEvent emailEvent);
-
-}
