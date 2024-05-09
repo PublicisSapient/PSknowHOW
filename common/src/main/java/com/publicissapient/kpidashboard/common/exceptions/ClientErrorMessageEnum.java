8048f12c561f7e1fc67009a78ae7061a16cef8d3
aksshriv1 <akshat.shrivastav@publicissapient.com>
2024-05-10 00:46:59 +0530

    Broken connection defect fix

--- common/src/main/java/com/publicissapient/kpidashboard/common/exceptions/ClientErrorMessageEnum.java
+++ common/src/main/java/com/publicissapient/kpidashboard/common/exceptions/ClientErrorMessageEnum.java
@@ -23,19 +23,30 @@ package com.publicissapient.kpidashboard.common.exceptions;
 
 public enum ClientErrorMessageEnum {
 
+	UNAUTHORIZED(401, "Sorry, you are not authorized to access the requested resource." +
+			" Please check your credentials/token and try again. \n\n" + noteMessage()),
 
-	UNAUTHORIZED(401, "Sorry, you are not authorized to access the requested resource. Please check your credentials/token and try again."),
-	FORBIDDEN(403, "Oops! You don't have permission to access the requested resource/tool."),
-	NOT_FOUND(404, "We couldn't find the requested resource. Please check the base URL, resource version and try again."),
-	METHOD_NOT_ALLOWED(405, "Oops! The action you're trying to perform is not allowed for this resource. Please check your request and try again."),
-	REQUEST_TIMEOUT(408, "Oops! The server timed out while waiting for the request. Please try again later."),
-	TOO_MANY_REQUESTS (429, "Oops! Too many requests have been sent in a short amount of time. Please try again later."),
-	OTHER_CLIENT_ERRORS(-1,"Please check logs and contact the KnowHow support team for assistance or clarification");
+	FORBIDDEN(403, "Oops! You don't have permission to access the requested resource/tool. \n\n" + noteMessage()),
+
+	NOT_FOUND(404, "We couldn't find the requested resource. Please check the base URL, resource version and try again. \n\n" + noteMessage()),
+	METHOD_NOT_ALLOWED(405, "Oops! The action you're trying to perform is not allowed for this resource." +
+			" Please check your request and try again. \n\n" + noteMessage()),
+	REQUEST_TIMEOUT(408, "Oops! The server timed out while waiting for the request. Please try again later. \n\n" + noteMessage()),
+	TOO_MANY_REQUESTS (429, "Oops! Too many requests have been sent in a short amount of time. Please try again later. \n\n" + noteMessage()),
+	OTHER_CLIENT_ERRORS(-1,"Please check logs and contact the KnowHow support team for assistance or clarification \n\n" + noteMessage());
 
 
 	private final int value;
 	private final String reasonPhrase;
 
+	/**
+	 * Common note message for all client errors.
+	 */
+	private static String noteMessage() {
+		return "Note:- One or more projects using this connection are impacted. " +
+				"Please ignore this message if the concerned processors are running successfully " +
+				"and your KPI dashboards remain unaffected for your project.";
+	}
 	/**
 	 *
 	 * @param value
