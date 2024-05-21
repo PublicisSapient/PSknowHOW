30c458759d540a86893d60699288e00e07891b93
pawkandp <pawkandp@publicisgroupe.net>
2024-05-21 12:16:40 +0530

    DTS-36327- changing the username pattern with minimum 3 char

--- central-auth-service/authapi/src/main/java/com/publicissapient/kpidashboard/apis/constant/CommonConstant.java
+++ central-auth-service/authapi/src/main/java/com/publicissapient/kpidashboard/apis/constant/CommonConstant.java
@@ -48,5 +48,5 @@ public final class CommonConstant {
 
 	public static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&]).{8,20})";
 	public static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-	public static final String USERNAME_PATTERN = "^[a-zA-Z0-9]{4,30}$";//Username can only contain letters and numbers Maximum length is 30 and min is 4 characters
+	public static final String USERNAME_PATTERN = "^[a-zA-Z0-9]{3,30}$";//Username can only contain letters and numbers Maximum length is 30 and min is 3 characters
 }
