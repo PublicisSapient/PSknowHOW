f0595032c5bc25f68a77bc3e0f2d4475b117c610
hinshah <hinal.shah@publicissapient.com>
2024-05-28 09:43:02 +0530

    removed unwanted function from recommendations component

--- UI/src/app/component/recommendations/recommendations.component.ts
+++ UI/src/app/component/recommendations/recommendations.component.ts
@@ -36,57 +36,6 @@ export class RecommendationsComponent implements OnInit {
       console.log("response", response);
   
       if(response?.['recommendations']?.length > 0){
-        // let res = {
-        //   "projectId": "AA Data and Reporting_649c00cd1734471c30843d2d",
-        //   "lastSprintId": "284d41ca-0ed6-470e-a664-66983a28eeb4_AA Data and Reporting_649c00cd1734471c30843d2d",
-        //   "recommendations": [
-        //       {
-        //           "kpiId": "kpi14",
-        //           "kpiName": "KPI name for kpi14",
-        //           "maturity": 3,
-        //           "recommendationSummary": "The project quality can be improved!",
-        //           "recommendationDetails": "The last data has showed a decrease in the quality of the project for the last sprints!",
-        //           "recommendationType": "Warnings",
-        //           "filter": "Overall"
-        //       },
-        //       {
-        //           "kpiId": "kpi35",
-        //           "kpiName": "KPI name for kpi35",
-        //           "maturity": 5,
-        //           "recommendationSummary": "Nice job!",
-        //           "recommendationDetails": "The team did a great job during the last sprints!",
-        //           "recommendationType": "Good Practices",
-        //           "filter": "Overall"
-        //       },
-        //       {
-        //         "kpiId": "kpi11",
-        //         "kpiName": "KPI name for kpi11",
-        //         "maturity": 5,
-        //         "recommendationSummary": "Nice job!",
-        //         "recommendationDetails": "The team did a great job during the last sprints!",
-        //         "recommendationType": "Good Practices",
-        //         "filter": "Overall"
-        //       },
-        //       {
-        //         "kpiId": "kpi22",
-        //         "kpiName": "KPI name for kpi22",
-        //         "maturity": 4,
-        //         "recommendationSummary": "Nice job!",
-        //         "recommendationDetails": "The team did a great job during the last sprints!",
-        //         "recommendationType": "Good Practices",
-        //         "filter": "Overall"
-        //       },
-        //       {
-        //         "kpiId": "kpi5",
-        //         "kpiName": "KPI name for kpi5",
-        //         "maturity": 1,
-        //         "recommendationSummary": "Nice job!",
-        //         "recommendationDetails": "The team did a great job during the last sprints!",
-        //         "recommendationType": "Good Practices",
-        //         "filter": "Overall"
-        //       }
-        //     ] 
-        // }
         this.recommendationsData = response['recommendations'];
         this.recommendationsData.forEach((item) => {
           this.maturities = this.maturities?.findIndex((x) => x['value'] == item['maturity']) == -1 ? [...this.maturities, {name: 'M'+item['maturity'], value:item['maturity']}] : [...this.maturities];
@@ -112,10 +61,5 @@ export class RecommendationsComponent implements OnInit {
       this.messageService.add({ severity: 'error', summary: 'Error in Kpi Column Configurations. Please try after sometime!' });
       
     })
-  }
-  
-  checkSelected(e){
-    console.log(e);
-    
   }
 }
