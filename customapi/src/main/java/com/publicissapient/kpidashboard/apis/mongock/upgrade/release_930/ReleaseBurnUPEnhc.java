4920001d91077bf1da2a700f101a3595aa8ce547
aksshriv1 <akshat.shrivastav@publicissapient.com>
2024-05-30 18:03:28 +0530

    FieldMapping changes and DB script

---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_930/ReleaseBurnUPEnhc.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_930;
+
+import java.util.Arrays;
+
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.client.MongoCollection;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+
+/**
+ * add release field mapping for burnup kpi
+ *
+ * @author aksshriv1
+ */
+@ChangeUnit(id = "release_burnup", order = "9301", author = "aksshriv1", systemVersion = "9.3.0")
+public class ReleaseBurnUPEnhc {
+
+	private final MongoTemplate mongoTemplate;
+	private static final String FIELD_NAME = "fieldName";
+	private static final String DEFINITION = "definition";
+
+	public ReleaseBurnUPEnhc(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@Execution
+	public void execution() {
+		addFieldToFieldMappingStructure();
+	}
+
+	public void addFieldToFieldMappingStructure() {
+
+		Document thresholdValueMapping = new Document(FIELD_NAME, "releaseListKPI150")
+				.append("fieldLabel", "Select releases to define team velocity").append("fieldType", "chips")
+				.append("section", "Custom Fields Mapping").append("tooltip", new Document(DEFINITION,
+						"Select release to predict the release prediction end date of active release on the basis of average velocity"));
+
+		mongoTemplate.getCollection("field_mapping_structure").insertOne(thresholdValueMapping);
+
+	}
+
+	@RollbackExecution
+	public void rollback() {
+		deleteFieldMappingStructure();
+	}
+
+	public void deleteFieldMappingStructure() {
+		MongoCollection<Document> fieldMappingStructure = mongoTemplate.getCollection("field_mapping_structure");
+		fieldMappingStructure
+				.deleteMany(new Document(FIELD_NAME, new Document("$in", Arrays.asList("releaseListKPI150"))));
+	}
+}
