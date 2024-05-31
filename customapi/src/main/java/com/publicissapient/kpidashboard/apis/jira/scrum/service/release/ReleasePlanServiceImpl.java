ba5167e80d3130ca809d76e5c9062fc5720d5d81
purushottam08 <purushottam.gupta@publicissapient.com>
2024-05-31 18:04:50 +0530

    cr added

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/release/ReleasePlanServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/release/ReleasePlanServiceImpl.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 package com.publicissapient.kpidashboard.apis.jira.scrum.service.release;
 
 import java.time.DayOfWeek;
@@ -46,6 +63,13 @@ import com.publicissapient.kpidashboard.common.util.DateUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * This service for managing Release plan Kpi on Release Board. Gives analysis
+ * of release scope vs plan.
+ * {@link JiraReleaseKPIService}
+ *
+ * @author purgupta2
+ */
 @Slf4j
 @Component
 public class ReleasePlanServiceImpl extends JiraReleaseKPIService {
--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_920/ReleasePlanKpi.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_920/ReleasePlanKpi.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 package com.publicissapient.kpidashboard.apis.mongock.rollback.release_920;
 
 import java.util.Collections;
@@ -9,6 +26,9 @@ import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 
+/**
+ * @author purgupta2
+ */
 @ChangeUnit(id = "r_release_plan_kpi", order = "09204", author = "purgupta2", systemVersion = "9.2.0")
 public class ReleasePlanKpi {
 
