321c533d72dbc272cb79550d85d37ff7ff362c06
Rishabh Shukla <risshukl0@publicisgroupe.net>
2024-06-19 21:05:31 +0530

    Issue fix on multiline v2

--- UI/src/app/component/multiline-v2/multiline-v2.component.ts
+++ UI/src/app/component/multiline-v2/multiline-v2.component.ts
@@ -264,7 +264,7 @@ export class MultilineV2Component implements OnChanges {
         .domain([0, maxYValue])
         .range([height - margin, 0]);
 
-      if (selectedProjectCount === 1 && (board === 'executive' || board === 'developer' || board === 'backlog' || board === 'dora')) {
+      if (selectedProjectCount === 1 && (board === 'mydashboard' || board === 'developer' || board === 'backlog' || board === 'dora')) {
         d3.select(this.elem).select('#horizontalSVG').select('div').remove();
         d3.select(this.elem).select('#horizontalSVG').select('tooltip-container').remove();
         /** Adding tooltip container */
