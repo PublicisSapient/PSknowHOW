1643dbd26d57e253bbdcb37f7f930bca03f978ef
Shivani <shi6@publicisgroupe.net>
2024-06-24 16:10:49 +0530

    Merge branch 'develop' of https://github.com/PublicisSapient/PSknowHOW into DTS-36462_SquadCapacity
     Conflicts:
    	UI/src/app/services/shared.service.ts

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/iterationdashboard/JiraIterationServiceR.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/iterationdashboard/JiraIterationServiceR.java
@@ -169,7 +169,7 @@ public class JiraIterationServiceR implements JiraNonTrendKPIServiceR {
 							try {
 								calculateAllKPIAggregatedMetrics(kpiRequest, responseList, kpiEle, filteredNode);
 							} catch (Exception e) {
-								log.error("Error while KPI calculation for data {}", kpiRequest.getKpiList(), e);
+								log.error("Error while KPI calculation for data +" , e);
 							}
 						}, executorService);
 						futures.add(future);
