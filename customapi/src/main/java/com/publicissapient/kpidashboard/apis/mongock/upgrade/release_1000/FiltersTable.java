fdf5281646ca77e1e109b7a9e431bb370bf8e4c6
purushottam08 <purushottam.gupta@publicissapient.com>
2024-06-28 12:25:26 +0530

    New GetDashboardConfig API

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1000/FiltersTable.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1000/FiltersTable.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_940;
+package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1000;
 
 import com.publicissapient.kpidashboard.apis.mongock.data.FiltersDataFactory;
 import com.publicissapient.kpidashboard.apis.util.MongockUtil;
@@ -34,7 +34,7 @@ import java.util.List;
  * @author purgupta2
  */
 @Slf4j
-@ChangeUnit(id = "new_filters", order = "9401", author = "purgupta2", systemVersion = "9.4.0")
+@ChangeUnit(id = "new_filters", order = "10001", author = "purgupta2", systemVersion = "10.0.0")
 public class FiltersTable {
 
 	private final MongoTemplate mongoTemplate;
