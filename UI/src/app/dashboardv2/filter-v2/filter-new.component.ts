1a2035898e84f33507d2751da6692ddcfe9e2839
Rishabh Shukla <risshukl0@publicisgroupe.net>
2024-07-04 18:30:18 +0530

    Test cases for Show/Hide

--- UI/src/app/dashboardv2/filter-v2/filter-new.component.ts
+++ UI/src/app/dashboardv2/filter-v2/filter-new.component.ts
@@ -144,7 +144,7 @@ export class FilterNewComponent implements OnInit, OnDestroy {
     this.boardData = boardData;
     let selectedBoard = boardData[this.selectedType ? this.selectedType : 'scrum'].filter((board => board.boardSlug.toLowerCase() === this.selectedTab.toLowerCase()))[0];
     if (!selectedBoard) {
-      selectedBoard = boardData['others'].filter((board => board.boardSlug.toLowerCase() === this.selectedTab.toLowerCase()))[0];
+      selectedBoard = boardData['others']?.filter((board => board.boardSlug.toLowerCase() === this.selectedTab.toLowerCase()))[0];
     }
 
     if (selectedBoard) {
@@ -613,7 +613,6 @@ export class FilterNewComponent implements OnInit, OnDestroy {
   }
 
   toggleShowHideMenu(event) {
-    console.log(this.masterData['kpiList']);
     if (this.showHideDdn?.overlayVisible) {
       this.showHideDdn.close(event);
     } else {
@@ -621,17 +620,12 @@ export class FilterNewComponent implements OnInit, OnDestroy {
     }
   }
 
-  showHideKPIs(e) {
+  showHideKPIs() {
     const kpiArray = this.dashConfigData[this.kanban ? 'kanban' : 'scrum'];
     this.assignUserNameForKpiData();
     for (let i = 0; i < kpiArray.length; i++) {
       if (kpiArray[i].boardSlug.toLowerCase() == this.selectedTab.toLowerCase()) {
-        if (this.selectedTab.toLowerCase() === 'iteration') {
-          this.dashConfigData[this.kanban ? 'kanban' : 'scrum'][i]['kpis'] = [this.dashConfigData[this.kanban ? 'kanban'
-            : 'scrum'][i]['kpis'].find((kpi) => kpi.kpiId === 'kpi121'), ...this.masterData['kpiList']];
-        } else {
           this.dashConfigData[this.kanban ? 'kanban' : 'scrum'][i]['kpis'] = this.masterData['kpiList'];
-        }
       }
     }
 
@@ -669,7 +663,7 @@ export class FilterNewComponent implements OnInit, OnDestroy {
   assignUserNameForKpiData() {
     delete this.masterData['kpiList'].id;
     this.masterData['kpiList'] = this.masterData['kpiList'].map(element => {
-      delete element.kpiDetail.id;
+      delete element?.kpiDetail?.id;
       return {
         kpiId: element.kpiId,
         kpiName: element.kpiName,
@@ -693,17 +687,5 @@ export class FilterNewComponent implements OnInit, OnDestroy {
         this.selectedShowHideKPIs.push(element);
       }
     });
-  }
-
-  fillselectedShowHideKPIs(event, opt) {
-    // if(event.checked) {
-    //   // opt.isEnabled = true;
-    //   this.selectedShowHideKPIs = this.masterData['kpiList'].filter(kpi => kpi.kpiId !== opt.kpiId);
-    //   this.selectedShowHideKPIs.push(opt);
-    // } else {
-    //   // opt.isEnabled = false;
-    //   this.selectedShowHideKPIs = this.masterData['kpiList'].filter(kpi => kpi.kpiId !== opt.kpiId);
-    //   this.selectedShowHideKPIs.push(opt);
-    // }
   }
 }
