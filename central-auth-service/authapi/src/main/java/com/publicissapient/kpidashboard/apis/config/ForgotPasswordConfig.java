49dff52aff7d6ebb623a5c5444b339aa725485d0
tibiorda <tiberiu.iordache@publicissapient.com>
2024-07-04 15:37:32 +0300

    DTS-34986-AuthNAuth-upgrade-final Revert to using .properties config file

--- central-auth-service/authapi/src/main/java/com/publicissapient/kpidashboard/apis/config/ForgotPasswordConfig.java
+++ central-auth-service/authapi/src/main/java/com/publicissapient/kpidashboard/apis/config/ForgotPasswordConfig.java
@@ -2,26 +2,29 @@ package com.publicissapient.kpidashboard.apis.config;
 
 import lombok.Data;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
 @EnableConfigurationProperties
-@ConfigurationProperties(prefix = "auth.forgot-password")
 public class ForgotPasswordConfig {
 
+	@Value("${auth.forgotPassword.expiryInterval}")
 	private Integer expiryInterval;
 
 	// forgot password server host only for server where nginex is not
 	// setup
+	@Value("${auth.forgotPassword.serverPort}")
 	private String serverPort;
 
 	// forgot password server host only for server where nginex is not
 	// setup
+	@Value("${auth.forgotPassword.uiHost}")
 	private String uiHost;
 
 	// forgot password UI port only for server where nginex is not setup
+	@Value("${auth.forgotPassword.uiPort}")
 	private String uiPort;
 }
