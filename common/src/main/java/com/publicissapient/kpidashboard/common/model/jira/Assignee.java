25bf5168f92e3a201566c2e304891c3d6280169c
kunkambl <kunkambl@publicisgroupe.net>
2024-07-08 11:29:30 +0530

    build fix

--- common/src/main/java/com/publicissapient/kpidashboard/common/model/jira/Assignee.java
+++ common/src/main/java/com/publicissapient/kpidashboard/common/model/jira/Assignee.java
@@ -19,6 +19,7 @@
 package com.publicissapient.kpidashboard.common.model.jira;
 
 import java.util.Objects;
+import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,7 +42,7 @@ public class Assignee {
 
 	private String assigneeId;
 	private String assigneeName;
-	private String email;
+	private Set<String> email;
 
 	public Assignee(String assigneeId, String assigneeName) {
 		this.assigneeId = assigneeId;
