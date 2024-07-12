0c65fa964d2c0d5910b494b63db9cd4da57a56f8
Girish <girish.pathak@publicissapient.com>
2024-07-12 18:46:32 +0530

    ui-image-8.3.0: UI changes

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/auth/exceptions/InvalidAuthTypeConfigException.java
+++
@@ -1,8 +0,0 @@
-package com.publicissapient.kpidashboard.apis.auth.exceptions;
-
-public class InvalidAuthTypeConfigException extends RuntimeException {
-
-	public InvalidAuthTypeConfigException(String message) {
-		super(message);
-	}
-}
