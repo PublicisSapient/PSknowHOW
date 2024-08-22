a346d4010da3b79c1dd1c3fd60d15fb53fedf607
kunkambl <kunkambl@publicisgroupe.net>
2024-08-22 09:25:09 +0530

    DTS-39248: repotool tool tracelog

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1010/RemoveRepoToolTraceLogs.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1010/RemoveRepoToolTraceLogs.java
@@ -17,10 +17,15 @@
 
 package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1010;
 
+import com.mongodb.client.MongoCollection;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
 import org.bson.Document;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Removing repotool trace logs as a part of scm-processor refactoring
@@ -31,15 +36,31 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class RemoveRepoToolTraceLogs {
 
 	private final MongoTemplate mongoTemplate;
+	private List<Document> deletedDocuments;
 
 	public RemoveRepoToolTraceLogs(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
+		this.deletedDocuments = new ArrayList<>();
 	}
 
 	@Execution
 	public void execution() {
-		mongoTemplate.getCollection("processor_execution_trace_log")
-				.deleteMany(new Document("processorName", "RepoTool"));
+
+		MongoCollection<Document> processorExecutionTraceLog = mongoTemplate
+				.getCollection("processor_execution_trace_log");
+		// Find and store the documents to be deleted
+		deletedDocuments = processorExecutionTraceLog.find(new Document("processorName", "RepoTool"))
+				.into(new ArrayList<>());
+
+		// Delete the documents
+		processorExecutionTraceLog.deleteMany(new Document("processorName", "RepoTool"));
 	}
 
+	@RollbackExecution
+	public void rollback() {
+		// Restore the deleted documents
+		if (deletedDocuments != null && !deletedDocuments.isEmpty()) {
+			mongoTemplate.getCollection("processor_execution_trace_log").insertMany(deletedDocuments);
+		}
+	}
 }
