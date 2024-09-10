5b8960337c55f49afc1798609cc2a1882487f14c
Ananth Pal <ananth.pal@publicissapient.com>
2024-09-10 18:10:34 +0530

    Revert "Merge branch 'central-hierarchy-id-changes' of https://github.com/PublicisSapient/PSknowHOW into central-hierarchy-id-changes"
    This reverts commit 43c442f63925ebbbd21fa04df89d2a7cb60c495d, reversing
    changes made to b6b1640a07c082f5f59810f62ca17b9487f63598.

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/common/service/impl/CacheServiceImpl.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/common/service/impl/CacheServiceImpl.java
@@ -279,4 +279,10 @@ public class CacheServiceImpl implements CacheService {
 
 	}
 
+	@Cacheable(CommonConstant.CACHE_ORGANIZATION_HIERARCHY_DATA)
+	@Override
+	public List<OrganizationHierarchy> getCacheOrganizationHierarchies() {
+		return organizationHierarchyService.findAllOrganizationHierarchiesFromDB();
+	}
+
 }
