5a7a66371d1e104892d37b25cc7a94b3751315e9
Rishabh Shukla <risshukl0@publicisgroupe.net>
2024-09-11 12:36:55 +0530

    branch filter fix

--- UI/src/app/dashboardv2/filter-v2/additional-filter/additional-filter.component.ts
+++ UI/src/app/dashboardv2/filter-v2/additional-filter/additional-filter.component.ts
@@ -53,6 +53,8 @@ export class AdditionalFilterComponent implements OnChanges {
                   }
                   if (this.filterData[index]?.length && correctLevelMapping[this.additionalFilterConfig[index].defaultLevel.labelName] === this.filterData[index][0].labelName) {
                     this.filterData[index].push(element);
+                  } else {
+                    this.filterData[index] = data[f];
                   }
                 }
               });
