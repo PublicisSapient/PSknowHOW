1b65ddb4ce49ea89237bd8690da664a01bf9ea34
purushottam08 <purushottam.gupta@publicissapient.com>
2024-10-11 16:04:58 +0530

    DTS-40072_iteration thread changes

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/iterationdashboard/JiraIterationServiceR.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/iterationdashboard/JiraIterationServiceR.java
@@ -157,7 +157,7 @@ public class JiraIterationServiceR implements JiraNonTrendKPIServiceR {
 					kpiRequest.setFilterToShowOnTrend(groupName);
 
 					ExecutorService executorService = Executors
-							.newFixedThreadPool(4);
+							.newFixedThreadPool(8);
 
 					List<CompletableFuture<Void>> futures = new ArrayList<>();
 
