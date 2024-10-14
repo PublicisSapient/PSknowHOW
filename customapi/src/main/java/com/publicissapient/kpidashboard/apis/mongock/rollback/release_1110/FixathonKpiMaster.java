b2e36679b5306df74b4d0277e9ce43c18671c22e
chittauri <shivani1@publicissapient.com>
2024-10-14 13:10:38 +0530

    update duplicate info

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1110/FixathonKpiMaster.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1110/FixathonKpiMaster.java
@@ -35,13 +35,10 @@ import io.mongock.api.annotations.RollbackExecution;
 public class FixathonKpiMaster {
 
 	public static final String FIELD_TYPE = "fieldType";
-	public static final String CHIPS = "chips";
 	public static final String FIELD_NAME = "fieldName";
-	public static final String JIRA_DEFECT_REJECTION_STATUS_KPI_151 = "jiraDefectRejectionStatusKPI151";
-	public static final String TEXT = "text";
-	public static final String JIRA_STATUS_FOR_REFINED_KPI_161 = "jiraStatusForRefinedKPI161";
 	public static final String FIELD_LABEL = "fieldLabel";
-	public static final String STATUS_TO_IDENTIFY_IN_READY_FOR_DEV = "Status to identify In Ready For Dev";
+	private static final String KPIID = "kpiId";
+	private static final String KPIINFO_DETAILS = "kpiInfo.details";
 	private final MongoTemplate mongoTemplate;
 
 	public FixathonKpiMaster(MongoTemplate mongoTemplate) {
@@ -53,23 +50,30 @@ public class FixathonKpiMaster {
 
 	}
 
-	private void updateLeadTimeForChange() {
+	private void updateDuplicateInfo() {
 		MongoCollection<Document> kpiMaster = mongoTemplate.getCollection("kpi_master");
-		kpiMaster.updateMany(new Document(), new Document("$unset", new Document("kpiInfo.details", "")));
+		kpiMaster.updateMany(new Document(KPIID, "kpi156"), new Document("$unset", new Document(KPIINFO_DETAILS, "")));
+		kpiMaster.updateMany(new Document(KPIID, "kpi150"), new Document("$unset", new Document(KPIINFO_DETAILS, "")));
 
 		// Step 2: Set new details
-		kpiMaster.updateMany(new Document(), new Document("$set", new Document("kpiInfo.details", List.of(
+		kpiMaster.updateMany(new Document(KPIID, "kpi156"), new Document("$set", new Document(KPIINFO_DETAILS, List.of(
 				new Document("type", "paragraph").append("value",
 						"LEAD TIME FOR CHANGE Captures the time between a code change to commit and deployed to production."),
 				new Document("type", "link").append("kpiLinkDetail",
 						new Document("text", "Detailed Information at").append("link",
 								"https://psknowhow.atlassian.net/wiki/spaces/PSKNOWHOW/pages/71663772/DORA+Lead+time+for+changes"))))));
 
+		// Step 2: Set new details
+		kpiMaster.updateMany(new Document(KPIID, "kpi150"),
+				new Document("$set", new Document(KPIINFO_DETAILS, List.of(new Document("type", "link")
+						.append("kpiLinkDetail", new Document("text", "Detailed Information at").append("link",
+								"https://psknowhow.atlassian.net/wiki/spaces/PSKNOWHOW/pages/70484023/Release+Release+Burnup"))))));
+
 	}
 
 	@RollbackExecution
 	public void rollBack() {
-		updateLeadTimeForChange();
+		updateDuplicateInfo();
 	}
 
 }
