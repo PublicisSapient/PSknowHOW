ea3f43be5325ba08e9f6f435f3496a4ec0431be6
brahmanand1 <44412993+brahmanand1@users.noreply.github.com>
2024-11-13 18:56:45 +0530

    DTS-40992: cosmatic changes

--- UI/src/app/config/advanced-settings/advanced-settings.component.ts
+++ UI/src/app/config/advanced-settings/advanced-settings.component.ts
@@ -51,7 +51,7 @@ export class AdvancedSettingsComponent implements OnInit {
   dataMismatchObj: object = {};
   pid: string;
   configuredToolList: any;
-  azureSnapshotToggleTooltip = "Click to capture the initial scope of your active sprint after sprint planning. Subsequent changes will be tracked as scope changes. Applies to active sprints only."; 
+  azureSnapshotToggleTooltip = 'Enable and click on "Run Now" to capture the initial scope of your active sprint after sprint planning. Subsequent changes will be tracked as scope changes. Applies to active sprints only.'; 
 
   constructor(
     private httpService: HttpService,
