24c444121df80712afd94481f983afb6f4bf96da
Rishabh Shukla <risshukl0@publicisgroupe.net>
2024-11-19 10:34:37 +0530

    Quality Gates fix

--- UI/src/app/dashboardv2/executive-v2/executive-v2.component.ts
+++ UI/src/app/dashboardv2/executive-v2/executive-v2.component.ts
@@ -144,7 +144,7 @@ export class ExecutiveV2Component implements OnInit, OnDestroy {
     this.subscriptions.push(this.service.globalDashConfigData.subscribe((globalConfig) => {
       this.globalConfig = JSON.parse(JSON.stringify(globalConfig));
       this.setGlobalConfigData(globalConfig);
-      let enabledKPIs = globalConfig['enabledKPIs'] ? globalConfig['enabledKPIs'] : [];
+      let enabledKPIs = globalConfig['enabledKPIs'] || [];
       setTimeout(() => {
         this.processKpiConfigData();
         this.setUpTabs();
