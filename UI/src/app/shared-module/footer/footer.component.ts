8731ba4552e1fa49878cc94a59357e998b6338f5
Rishabh Shukla <risshukl0@publicisgroupe.net>
2024-11-22 09:58:33 +0530

    Minor fix

--- UI/src/app/shared-module/footer/footer.component.ts
+++ UI/src/app/shared-module/footer/footer.component.ts
@@ -46,15 +46,4 @@ export class FooterComponent implements OnInit {
       }
     });
   }
-
-  styleObj(): object {
-    let marginLeft = '0';
-    // const urlArray = this.router.url.split('/');
-    // if (urlArray.includes('Help') || urlArray.includes('Config') || urlArray.includes('Error') || urlArray[urlArray.length - 1].includes('login') || urlArray.includes('register') || localStorage.getItem('newUI')) {
-    //   marginLeft = '0rem'
-    // }
-    return { 'margin-left': marginLeft }
-
-  }
-
 }
