4159c3c14e28a346d03c93a61cc8bb6153ac118f
Rishabh Shukla <risshukl0@publicisgroupe.net>
2024-11-26 11:01:31 +0530

    Fix for DTS-41934

--- UI/src/app/dashboardv2/filter-v2/filter-new.component.ts
+++ UI/src/app/dashboardv2/filter-v2/filter-new.component.ts
@@ -275,6 +275,13 @@ export class FilterNewComponent implements OnInit, OnDestroy {
 
       this.masterDataCopy['kpiList'] = JSON.parse(JSON.stringify(this.masterData['kpiList']));
 
+      let visibleKPIs = this.masterDataCopy['kpiList'].filter(kpi => kpi.isEnabled);
+      if (visibleKPIs.length < this.masterDataCopy['kpiList'].length) {
+        this.showHideSelectAll = false;
+      } else if (visibleKPIs.length === this.masterDataCopy['kpiList'].length) {
+        this.showHideSelectAll = true;
+      }
+      this.cdr.detectChanges();
       this.parentFilterConfig = { ...this.selectedBoard.filters.parentFilter };
       if (!this.parentFilterConfig || !Object.keys(this.parentFilterConfig).length) {
         this.selectedLevel = null;
@@ -1294,6 +1301,16 @@ export class FilterNewComponent implements OnInit, OnDestroy {
       }
     });
     this.dashConfigData['username'] = this.service.getCurrentUserDetails('user_name');
+  }
+
+
+  setSelectAll() {
+    let visibleKPIs = this.masterDataCopy['kpiList'].filter(kpi => kpi.isEnabled);
+    if (visibleKPIs.length < this.masterDataCopy['kpiList'].length) {
+      this.showHideSelectAll = false;
+    } else if (visibleKPIs.length === this.masterDataCopy['kpiList'].length) {
+      this.showHideSelectAll = true;
+    }
   }
 
   /**
