dd09fa9bdad86fbade8258fe37269b7a084c5e48
chittauri <shivani1@publicissapient.com>
2024-12-05 15:09:04 +0530

    set organization hierarchy and project basic

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/datamigration/service/DataMigrationService.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/datamigration/service/DataMigrationService.java
@@ -16,7 +16,7 @@
  *
  ******************************************************************************/
 
-package com.publicissapient.kpidashboard.apis.datamigration;
+package com.publicissapient.kpidashboard.apis.datamigration.service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -26,10 +26,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import com.publicissapient.kpidashboard.apis.datamigration.model.MigrateData;
-import com.publicissapient.kpidashboard.apis.datamigration.util.InconsistentDataException;
-import com.publicissapient.kpidashboard.common.model.application.HierarchyValue;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -38,16 +34,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
-
-import com.publicissapient.kpidashboard.apis.datamigration.model.HierarchyValueDup;
-import com.publicissapient.kpidashboard.apis.datamigration.model.ProjectBasicDup;
-import com.publicissapient.kpidashboard.common.model.application.OrganizationHierarchy;
-import com.publicissapient.kpidashboard.common.model.application.ProjectBasicConfig;
-import com.publicissapient.kpidashboard.common.repository.application.OrganizationHierarchyRepository;
-import com.publicissapient.kpidashboard.common.repository.application.ProjectBasicConfigRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
+import com.publicissapient.kpidashboard.apis.datamigration.model.HierarchyValueDup;
+import com.publicissapient.kpidashboard.apis.datamigration.model.MigrateData;
+import com.publicissapient.kpidashboard.apis.datamigration.model.ProjectBasicDup;
+import com.publicissapient.kpidashboard.apis.datamigration.util.InconsistentDataException;
+import com.publicissapient.kpidashboard.common.model.application.OrganizationHierarchy;
+import com.publicissapient.kpidashboard.common.model.application.ProjectBasicConfig;
+import com.publicissapient.kpidashboard.common.repository.application.AccountHierarchyRepository;
+import com.publicissapient.kpidashboard.common.repository.application.AdditionalFilterCategoryRepository;
+import com.publicissapient.kpidashboard.common.repository.application.KanbanAccountHierarchyRepository;
+import com.publicissapient.kpidashboard.common.repository.application.OrganizationHierarchyRepository;
+import com.publicissapient.kpidashboard.common.repository.application.ProjectBasicConfigRepository;
+import com.publicissapient.kpidashboard.common.repository.jira.SprintRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
@@ -57,6 +59,18 @@ public class DataMigrationService {
 	private ProjectBasicConfigRepository basicConfigRepository;
 	@Autowired
 	private OrganizationHierarchyRepository organizationHierarchyRepository;
+	@Autowired
+	public SaveService saveService;
+
+	@Autowired
+	private AccountHierarchyRepository accountHierarchyRepository;
+	@Autowired
+	private KanbanAccountHierarchyRepository kanbanAccountHierarchyRepository;
+	@Autowired
+	private SprintRepository sprintRepository;
+
+	@Autowired
+	private AdditionalFilterCategoryRepository additionalFilterCategoryRepository;
 
 	protected Map<String, OrganizationHierarchy> nodeWiseOrganizationHierarchy;
 	protected List<ProjectBasicConfig> projectBasicConfigList;
@@ -141,10 +155,12 @@ public class DataMigrationService {
 		for (ProjectBasicConfig projectBasicConfig : projectBasicConfigList) {
 			ProjectBasicDup projectBasicDup = mapper.map(projectBasicConfig, ProjectBasicDup.class);
 			List<HierarchyValueDup> hierarchyValueDupList = projectBasicDup.getHierarchy();
-			for (HierarchyValueDup values : hierarchyValueDupList) {
-				values.setCustomizedValue(values.getCustomizedValue());
+			if (CollectionUtils.isNotEmpty(hierarchyValueDupList)) {
+				for (HierarchyValueDup values : hierarchyValueDupList) {
+					values.setCustomizedValue(values.getCustomizedValue());
+				}
+				projectBasicDupList.add(projectBasicDup);
 			}
-			projectBasicDupList.add(projectBasicDup);
 		}
 
 		return projectBasicDupList;
@@ -194,4 +210,67 @@ public class DataMigrationService {
 
 		return organizationHierarchy.getNodeId();
 	}
+
+	/*
+	 * create organization hierarchy and update project basic hierarchy
+	 */
+	public void populateOrganizationHierarchy() {
+		if (MapUtils.isEmpty(nodeWiseOrganizationHierarchy)) {
+			log.info("Calling the validation process");
+			List<MigrateData> failureData = dataMigration();
+			// Proceed only if there are no failures and the map is populated
+			if (CollectionUtils.isEmpty(failureData) && MapUtils.isNotEmpty(nodeWiseOrganizationHierarchy)) {
+				saveOrganizationHierarchyAndUpdateProjectBasic();
+			}
+
+		} else {
+			saveOrganizationHierarchyAndUpdateProjectBasic();
+		}
+	}
+
+	@Transactional
+	public void saveOrganizationHierarchyAndUpdateProjectBasic() {
+		log.info("Start - Updating Node Names to Original Values");
+
+		try {
+			// Update node names to match their display names
+			nodeWiseOrganizationHierarchy.values()
+					.forEach(orgHierarchy -> orgHierarchy.setNodeName(orgHierarchy.getNodeDisplayName()));
+
+			// Map project names to their unique node IDs
+			Map<String, String> projectNameWiseUniqueId = nodeWiseOrganizationHierarchy.values().stream()
+					.filter(orgHierarchy -> "project".equalsIgnoreCase(orgHierarchy.getHierarchyLevelId()))
+					.collect(Collectors.toMap(OrganizationHierarchy::getNodeDisplayName,
+							OrganizationHierarchy::getNodeId));
+
+			// Update project basic config list with unique projectNodeId
+			projectBasicConfigList.forEach(projectBasicConfig -> {
+				projectBasicConfig.setProjectNodeId(projectNameWiseUniqueId.get(projectBasicConfig.getProjectName()));
+				projectBasicConfig.setProjectDisplayName(projectBasicConfig.getProjectName());
+
+			});
+
+			// Save all data to the repository
+			saveService.saveToOrganizationHierarchy(new ArrayList<>(nodeWiseOrganizationHierarchy.values()));
+			saveService.saveToBasicConfig(projectBasicConfigList);
+			log.info("Data successfully saved to the database.");
+
+		} catch (DuplicateKeyException ex) {
+			log.error("Duplicate project name found in organization hierarchy: {}", ex.getMessage());
+			throw new DuplicateKeyException(
+					"Duplicate project name found in organization hierarchy: " + ex.getMessage(), ex);
+
+		} catch (DataIntegrityViolationException ex) {
+			log.error("Data integrity violation occurred: {}", ex.getMessage());
+			throw new DataIntegrityViolationException(
+					"Data integrity violation while saving organization hierarchy or project basic config: "
+							+ ex.getMessage(),
+					ex);
+
+		} catch (Exception ex) {
+			log.error("An unexpected error occurred: {}", ex.getMessage());
+			throw new IllegalStateException("An unexpected error occurred while saving data: " + ex.getMessage(), ex);
+		}
+	}
+
 }
