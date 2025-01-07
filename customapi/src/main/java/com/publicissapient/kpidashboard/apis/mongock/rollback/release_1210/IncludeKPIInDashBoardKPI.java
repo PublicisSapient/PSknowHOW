3d3309480aa891410448b82b20ba3772c8db8715
aksshriv1 <akshat.shrivastav@publicissapient.com>
2025-01-07 15:10:24 +0530

    DTS-40347: Include Left Out KPI
    Change log: DB Script

---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1210/IncludeKPIInDashBoardKPI.java
@@ -0,0 +1,29 @@
+package com.publicissapient.kpidashboard.apis.mongock.rollback.release_1210;
+
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@ChangeUnit(id = "r_add_kpi_category_mapping", order = "012105", author = "aksshriv1", systemVersion = "12.1.0")
+public class IncludeKPIInDashBoardKPI {
+
+    private final MongoTemplate mongoTemplate;
+
+    public IncludeKPIInDashBoardKPI(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Execution
+    public void rollback() {
+        mongoTemplate.getCollection("kpi_category_mapping").deleteOne(new Document("kpiId", "kpi149"));
+    }
+
+    @RollbackExecution
+    public void execution() {
+        mongoTemplate.getCollection("kpi_category_mapping").insertOne(new Document().append("kpiId", "kpi149")
+                .append("categoryId", "value").append("kpiOrder", 4.0).append("kanban", false));
+    }
+}
---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1210/IncludeKPIInDashBoardKPI.java
@@ -0,0 +1,28 @@
+package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1210;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@ChangeUnit(id = "add_kpi_category_mapping", order = "12105", author = "aksshriv1", systemVersion = "12.1.0")
+public class IncludeKPIInDashBoardKPI {
+
+	private final MongoTemplate mongoTemplate;
+
+	public IncludeKPIInDashBoardKPI(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@Execution
+	public void execution() {
+		mongoTemplate.getCollection("kpi_category_mapping").insertOne(new Document().append("kpiId", "kpi149")
+				.append("categoryId", "value").append("kpiOrder", 4.0).append("kanban", false));
+	}
+
+	@RollbackExecution
+	public void rollback() {
+		mongoTemplate.getCollection("kpi_category_mapping").deleteOne(new Document("kpiId", "kpi149"));
+	}
+}
