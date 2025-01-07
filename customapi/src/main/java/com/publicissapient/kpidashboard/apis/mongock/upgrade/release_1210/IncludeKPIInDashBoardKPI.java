06150d06905ec5c4f9a650f81983db0cf5c97f3a
aksshriv1 <akshat.shrivastav@publicissapient.com>
2025-01-07 16:21:58 +0530

    DTS-40347: Include Left Out KPI
    Change log: DB Script

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1210/IncludeKPIInDashBoardKPI.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1210/IncludeKPIInDashBoardKPI.java
@@ -1,3 +1,19 @@
+/*
+ *   Copyright 2014 CapitalOne, LLC.
+ *   Further development Copyright 2022 Sapient Corporation.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1210;
 
 import io.mongock.api.annotations.ChangeUnit;
@@ -6,6 +22,11 @@ import io.mongock.api.annotations.RollbackExecution;
 import org.bson.Document;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+/**
+ * Include left out KPI in dashboardConfig
+ *
+ * @author aksshriv1
+ */
 @ChangeUnit(id = "add_kpi_category_mapping", order = "12107", author = "aksshriv1", systemVersion = "12.1.0")
 public class IncludeKPIInDashBoardKPI {
 
