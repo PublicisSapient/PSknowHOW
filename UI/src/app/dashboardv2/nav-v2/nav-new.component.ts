7613eb86eb6c5ad62f5f0e9edf6cfc87ee1ba11a
Rishabh Shukla <risshukl0@publicisgroupe.net>
2025-01-16 17:50:22 +0530

    Fix

--- UI/src/app/dashboardv2/nav-v2/nav-new.component.ts
+++ UI/src/app/dashboardv2/nav-v2/nav-new.component.ts
@@ -29,20 +29,18 @@ export class NavNewComponent implements OnInit, OnDestroy {
     this.selectedType = this.sharedService.getSelectedType() ? this.sharedService.getSelectedType() : 'scrum';
     this.sharedService.setScrumKanban(this.selectedType);
 
-    if (this.sharedService.getSelectedTrends() && this.sharedService.getSelectedTrends()[0]) {
-      this.getBoardConfig([...this.sharedService.getSelectedTrends().map(proj => proj['basicProjectConfigId'])]);
-    } else {
-      this.getBoardConfig([]);
-    }
-
     this.sharedService.onTabSwitch
       .subscribe(data => {
         this.selectedTab = data.selectedBoard;
       });
 
-    // this.sharedService.primaryFilterChangeSubject.subscribe(x => {
-    //   this.getBoardConfig([...this.sharedService.getSelectedTrends().map(proj => proj['basicProjectConfigId'])]);
-    // })
+    this.sharedService.primaryFilterChangeSubject.subscribe(x => {
+      if (this.sharedService.getSelectedTrends() && this.sharedService.getSelectedTrends()[0]) {
+        this.getBoardConfig([...this.sharedService.getSelectedTrends().map(proj => proj['basicProjectConfigId'])]);
+      } else {
+        this.getBoardConfig([]);
+      }
+    })
 
     this.sharedService.onScrumKanbanSwitch.subscribe(type => {
       this.selectedType = type.selectedType
