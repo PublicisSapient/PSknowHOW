b54c8f7ffa450ebf8fdc3a0f2a1fdff9eccb6aee
chittauri <shivani1@publicissapient.com>
2025-01-17 13:01:04 +0530

    backend test case fix

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/DefectCountByServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/DefectCountByServiceImplTest.java
@@ -125,7 +125,7 @@ public class DefectCountByServiceImplTest {
 		TreeAggregatorDetail treeAggregatorDetail = KPIHelperUtil.getTreeLeafNodesGroupedByFilter(kpiRequest,
 				accountHierarchyDataList, new ArrayList<>(), "hierarchyLevelOne", 5);
 		List<Node> leafNodeList = new ArrayList<>();
-		leafNodeList = KPIHelperUtil.getLeafNodes(treeAggregatorDetail.getRoot(), leafNodeList);
+		leafNodeList = KPIHelperUtil.getLeafNodes(treeAggregatorDetail.getRoot(), leafNodeList, true);
 		String startDate = leafNodeList.get(0).getSprintFilter().getStartDate();
 		String endDate = leafNodeList.get(leafNodeList.size() - 1).getSprintFilter().getEndDate();
 		when(jiraServiceR.getCurrentSprintDetails()).thenReturn(sprintDetails);
