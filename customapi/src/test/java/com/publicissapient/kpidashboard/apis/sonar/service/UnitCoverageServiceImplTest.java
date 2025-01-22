e911935150f704815c9e1edf5da7d9beaf9cbd64
aksshriv1 <akshat.shrivastav@publicissapient.com>
2025-01-22 13:56:45 +0530

    DTS-42446: Central hierarchy code quality gate fix
    Change log: Sonar Fix

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/sonar/service/UnitCoverageServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/sonar/service/UnitCoverageServiceImplTest.java
@@ -27,6 +27,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -36,6 +37,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.publicissapient.kpidashboard.apis.common.service.CacheService;
+import com.publicissapient.kpidashboard.apis.common.service.CommonService;
 import com.publicissapient.kpidashboard.apis.enums.KPISource;
 import org.bson.types.ObjectId;
 import org.junit.After;
@@ -88,6 +90,8 @@ public class UnitCoverageServiceImplTest {
 	CacheService cacheService;
 	@Mock
 	private CustomApiConfig customApiConfig;
+	@Mock
+	private CommonService commonService;
 	private List<AccountHierarchyData> ahdList = new ArrayList<>();
 	private Map<String, Object> filterLevelMap;
 	private List<ProjectBasicConfig> projectConfigList = new ArrayList<>();
@@ -98,8 +102,9 @@ public class UnitCoverageServiceImplTest {
 	private KpiRequest kpiRequest;
 	private KpiElement kpiElement;
 	private List<AccountHierarchyData> accountHierarchyDataList = new ArrayList<>();
-
 	private List<SonarHistory> sonarHistoryData = new ArrayList<>();
+	private Map<String, List<DataCount>> trendValueMap = new HashMap<>();
+	private List<DataCount> trendValues = new ArrayList<>();
 
 	@Before
 	public void setup() {
@@ -128,6 +133,29 @@ public class UnitCoverageServiceImplTest {
 		fieldMappingList.forEach(fieldMapping -> {
 			fieldMappingMap.put(fieldMapping.getBasicProjectConfigId(), fieldMapping);
 		});
+
+		projectConfigList.add(projectBasicConfig);
+
+		projectConfigList.forEach(projectConfigs -> {
+			projectConfigMap.put(projectConfigs.getProjectName(), projectConfigs);
+		});
+		Mockito.when(cacheService.cacheProjectConfigMapData()).thenReturn(projectConfigMap);
+
+		List<DataCount> dataCountList = new ArrayList<>();
+		dataCountList.add(createDataCount("2022-07-26", 0l));
+		dataCountList.add(createDataCount("2022-07-27", 35l));
+		dataCountList.add(createDataCount("2022-07-28", 44l));
+		dataCountList.add(createDataCount("2022-07-29", 0l));
+		dataCountList.add(createDataCount("2022-07-30", 0l));
+		dataCountList.add(createDataCount("2022-07-31", 12l));
+		dataCountList.add(createDataCount("2022-08-01", 0l));
+		DataCount dataCount = createDataCount(null, 0l);
+		dataCount.setData("");
+		dataCount.setValue(dataCountList);
+		trendValues.add(dataCount);
+		trendValueMap.put("Overall", trendValues);
+		trendValueMap.put("BRANCH1->PR_10304", trendValues);
+		when(commonService.sortTrendValueMap(anyMap())).thenReturn(trendValueMap);
 
 		setToolMap();
 	}
@@ -338,4 +366,13 @@ public class UnitCoverageServiceImplTest {
 		assertEquals(null, ucServiceImpl.calculateKPIMetrics(new HashMap<>()));
 	}
 
+	private DataCount createDataCount(String date, Long data) {
+		DataCount dataCount = new DataCount();
+		dataCount.setData(data.toString());
+		dataCount.setSProjectName("PR_10304");
+		dataCount.setDate(date);
+		dataCount.setHoverValue(new HashMap<>());
+		dataCount.setValue(Long.valueOf(data));
+		return dataCount;
+	}
 }
