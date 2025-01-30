7bd6cdba36e8d4fb0508b74f9c6286325ad063e3
Sudhansh Pal <sudpal1@WKMZTD3D66BA.global.publicisgroupe.net>
2025-01-30 15:23:26 +0530

    added environment files for different env

--- UI/src/environments/environment.production.ts
+++ UI/src/environments/environment.production.ts
@@ -17,7 +17,7 @@
  ******************************************************************************/
 
 export const environment = {
-    production: false,
+    production: true,
     baseUrl: '',
     SSO_LOGIN: false,
     CENTRAL_LOGIN_URL: '',
