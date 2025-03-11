4669e2120567768b41f9c9c1788c5dc35cfffa10
Sudhansh Pal <sudpal1@WKMZTD3D66BA.global.publicisgroupe.net>
2025-03-11 16:27:45 +0530

    revet back changes

--- UI/src/app/shared-module/name-pipe.pipe.spec.ts
+++ UI/src/app/shared-module/name-pipe.pipe.spec.ts
@@ -12,7 +12,7 @@ describe('Pipe: Default', () => {
       {
         level: 1,
         hierarchyLevelId: 'hierarchyLevelOne',
-        hierarchyLevelIdName: 'Level One',
+        hierarchyLevelName: 'Level One',
         suggestions: [
           { name: 'Sample One', code: 'Sample One' },
           { name: 'Sample Two', code: 'Sample Two' },
@@ -24,7 +24,7 @@ describe('Pipe: Default', () => {
       {
         level: 2,
         hierarchyLevelId: 'hierarchyLevelTwo',
-        hierarchyLevelIdName: 'Level Two',
+        hierarchyLevelName: 'Level Two',
         suggestions: [
           { name: 'Sample Four', code: 'Sample Four' },
           { name: 'Sample Three', code: 'Sample Three' },
@@ -36,7 +36,7 @@ describe('Pipe: Default', () => {
       {
         level: 3,
         hierarchyLevelId: 'hierarchyLevelThree',
-        hierarchyLevelIdName: 'Level Three',
+        hierarchyLevelName: 'Level Three',
         suggestions: [
           { name: 'Sample Five', code: 'Sample Five' },
           { name: 'Sample Six', code: 'Sample Six' },
