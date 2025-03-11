3bd8a485f226d62855e3c7fb1492cfd3d0940655
Sudhansh Pal <sudpal1@WKMZTD3D66BA.global.publicisgroupe.net>
2025-03-11 14:40:47 +0530

    regration issue fix for mang. access page

--- UI/src/app/shared-module/name-pipe.pipe.ts
+++ UI/src/app/shared-module/name-pipe.pipe.ts
@@ -8,7 +8,7 @@ export class NamePipePipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): unknown {
     if(value.toLowerCase() !== 'project') {
     const hierarchyData = JSON.parse(localStorage.getItem('hierarchyData')) || JSON.parse(localStorage.getItem('completeHierarchyData'))?.['scrum'];
-    value = hierarchyData?.filter(h => h.hierarchyLevelId === value).length ? hierarchyData.filter(h => h.hierarchyLevelId === value)[0].hierarchyLevelName : value;
+    value = hierarchyData?.filter(h => h.hierarchyLevelId === value).length ? hierarchyData.filter(h => h.hierarchyLevelId === value)[0].hierarchyLevelIdName : value;
     } else {
       value = 'Project';
     }
