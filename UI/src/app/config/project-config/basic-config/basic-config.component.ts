c90ed9ec8306f334991e592a7fcb4ebf728cb6b5
Sudhansh Pal <sudpal1@WKMZTD3D66BA.global.publicisgroupe.net>
2025-03-12 15:53:33 +0530

    disable loader

--- UI/src/app/config/project-config/basic-config/basic-config.component.ts
+++ UI/src/app/config/project-config/basic-config/basic-config.component.ts
@@ -407,8 +407,9 @@ export class BasicConfigComponent implements OnInit {
       hierarchyMap['project'] = 'Project';
     }
     this.http.getOrganizationHierarchy()?.subscribe(formFieldData => {
-    if(formFieldData.success === false){
+    if(formFieldData?.success === false){
       this.messenger.add({ severity: 'error', summary: formFieldData.message });
+      this.blocked = false;
     }else{
       const flatData = formFieldData?.data;
 
