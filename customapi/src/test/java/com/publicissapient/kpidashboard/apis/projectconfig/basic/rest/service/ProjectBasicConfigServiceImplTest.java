00de48fbc05802ccdc615f0e6b34eb8b59765637
kunkambl <kunkambl@publicisgroupe.net>
2025-03-18 12:37:50 +0530

    DTS-44666: fixed test cases

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/projectconfig/basic/rest/service/ProjectBasicConfigServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/projectconfig/basic/rest/service/ProjectBasicConfigServiceImplTest.java
@@ -791,9 +791,9 @@ public class ProjectBasicConfigServiceImplTest {
 
 		// Create sample HierarchyResponseDTOs
 		HierarchyResponseDTO dto1 = new HierarchyResponseDTO();
-		dto1.setProjectId(projectId1.toString());
+		dto1.setProjectBasicId(projectId1.toString());
 		HierarchyResponseDTO dto2 = new HierarchyResponseDTO();
-		dto2.setProjectId(projectId2.toString());
+		dto2.setProjectBasicId(projectId2.toString());
 
 		List<HierarchyResponseDTO> hierarchyResponseDTOS = Arrays.asList(dto1, dto2);
 
