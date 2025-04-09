82b032df6f4225e2f9aa205f260164358b363b9c
chittauri <shivani1@publicissapient.com>
2025-04-09 18:14:29 +0530

    DTS-45066: QG_fix_SC

--- customapi/src/main/java/com/publicissapient/kpidashboard/apis/hierarchy/dto/CreateHierarchyRequest.java
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/hierarchy/dto/CreateHierarchyRequest.java
@@ -23,7 +23,6 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
