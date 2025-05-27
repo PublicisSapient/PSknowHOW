3cdd07c0af3c7892c1ec96042206fbb0b2bd3ec5
chittauri <shivani1@publicissapient.com>
2025-05-27 19:42:04 +0530

    DTS-46346:dora mongock changes

---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1100/LabelChangeDoraRollback.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.publicissapient.kpidashboard.apis.mongock.rollback.release_1100;
+
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.client.MongoCollection;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+
+@ChangeUnit(id = "r_label_change_dora", order = "011005", author = "shi6", systemVersion = "11.0.0")
+public class LabelChangeDoraRollback {
+	private final MongoTemplate mongoTemplate;
+
+	public LabelChangeDoraRollback(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@Execution
+	public void execution() {
+		MongoCollection<Document> collection = mongoTemplate.getCollection("kpi_master");
+		updateLabel(collection, "kpi116", null, null);
+		updateLabel(collection, "kpi166", null, null);
+		updateLabel(collection, "kpi116", "Weeks", "Percentage");
+		updateLabel(collection, "kpi166", "Weeks", "Hours");
+	}
+
+	@RollbackExecution
+	public void rollback() {
+		MongoCollection<Document> collection = mongoTemplate.getCollection("kpi_master");
+		updateLabel(collection, "kpi116", "Weeks", "Percentage");
+		updateLabel(collection, "kpi166", "Weeks", "Hours");
+	}
+
+	public void updateLabel(MongoCollection<Document> kpiMaster, String kpiId, String xAxisLabel,
+			String yAxisLabel) {
+		kpiMaster.updateOne(new Document("kpiId", kpiId),
+				new Document("$set", new Document("xAxisLabel", xAxisLabel).append("yAxisLabel", yAxisLabel)));
+	}
+
+}
